cef2d1ed51d4d2f36bd9cd102dabfa16420f0c05
David Fugate <dfugate@microsoft.com>
2011-02-02 09:44:09 -0800

    Added Microsoft's Test262 contributions stemming from IE9 RC1: - 12.14-7.js:  cleaned up - 15.2.3.3:  89 test case additions - 15.2.3.4:  29 test case additions - 15.2.3.6:  1,076 test case additions and 15 minor test case fixes - 15.2.3.9-1.js:  cleaned up - 15.3.4.5-2-8.js:  cleaned up - 15.4.4.14:  14 test case additions and 3 minor test case fixes - 15.4.4.15:  9 test case additions and 3 minor test case fixes - 15.4.4.17:  1 test case additions and 1 minor test case fix - 15.4.4.18:  1 minor test case fix - 15.4.4.19:  1 test case addition and 2 minor test case fixes - 15.4.4.20:  3 minor test case fixes - 15.4.4.21:  1 test case addition and 1 minor test case fix - 15.4.4.22: 2 minor test case fixes - 15.9.4.4: 2 minor test case fixes - 15.9.5.43-0-2.js: minor test case fix

---
+++ external/contributions/Microsoft/ietestcenter_ie9_rc1/chapter15/15.2/15.2.3/15.2.3.6/15.2.3.6-4-354-5.js
@@ -0,0 +1,52 @@
+﻿/// Copyright (c) 2009 Microsoft Corporation 
+/// 
+/// Redistribution and use in source and binary forms, with or without modification, are permitted provided
+/// that the following conditions are met: 
+///    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+///      the following disclaimer. 
+///    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+///      the following disclaimer in the documentation and/or other materials provided with the distribution.  
+///    * Neither the name of Microsoft nor the names of its contributors may be used to
+///      endorse or promote products derived from this software without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+/// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+/// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+/// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ES5Harness.registerTest({
+    id: "15.2.3.6-4-354-5",
+
+    path: "TestCases/chapter15/15.2/15.2.3/15.2.3.6/15.2.3.6-4-354-5.js",
+
+    description: "Object.defineProperty will update [[Value]] attribute successfully when [[Configurable]] attribute is true and [[Writable]] attribute is false, 'O' is the document host object (8.12.9 - step Note)",
+
+    test: function testcase() {
+
+        var obj = document;
+
+        try {
+            Object.defineProperty(obj, "property", {
+                value: 1001,
+                writable: false,
+                configurable: true
+            });
+
+            Object.defineProperty(obj, "property", {
+                value: 1002
+            });
+
+            return dataPropertyAttributesAreCorrect(obj, "property", 1002, false, false, true);
+        } finally {
+            delete obj.property;
+        }
+    },
+
+    precondition: function prereq() {
+        return fnExists(Object.defineProperty);
+    }
+});
