413c693d4a8f8f00ddc4d6413f63c30ea1e873d6
David Fugate <dfugate@microsoft.com>
2011-02-25 10:23:38 -0800

    Added fresher bits from external\contributions\Microsoft\ietestcenter_ie9_rc1_refresh1.

---
+++ test/suite/ietestcenter/chapter10/10.6/10.6-11-b-1.js
@@ -0,0 +1,74 @@
+﻿/// Copyright (c) 2009 Microsoft Corporation 
+/// 
+/// Redistribution and use in source and binary forms, with or without modification, are permitted provided
+/// that the following conditions are met: 
+///    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+///      the following disclaimer. 
+///    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+///      the following disclaimer in the documentation and/or other materials provided with the distribution.  
+///    * Neither the name of Microsoft nor the names of its contributors may be used to
+///      endorse or promote products derived from this software without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+/// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+/// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+/// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ES5Harness.registerTest({
+    id: "10.6-11-b-1",
+
+    path: "TestCases/chapter10/10.6/10.6-11-b-1.js",
+
+    description: "Arguments Object has index property '0' as its own property, it shoulde be writable, enumerable, configurable and does not invoke the setter defined on Object.prototype[0] (Step 11.b)",
+
+    test: function testcase() {
+        try {
+            var data = "data";
+            var getFunc = function () {
+                return data;
+            };
+
+            var setFunc = function (value) {
+                data = value;
+            };
+
+            Object.defineProperty(Object.prototype, "0", {
+                get: getFunc,
+                set: setFunc,
+                configurable: true
+            });
+
+            var argObj = (function () { return arguments })(1);
+
+            var verifyValue = false;
+            verifyValue = (argObj[0] === 1);
+
+            var verifyEnumerable = false;
+            for (var p in argObj) {
+                if (p === "0" && argObj.hasOwnProperty("0")) {
+                    verifyEnumerable = true;
+                }
+            }
+
+            var verifyWritable = false;
+            argObj[0] = 1001;
+            verifyWritable = (argObj[0] === 1001);
+
+            var verifyConfigurable = false;
+            delete argObj[0];
+            verifyConfigurable = argObj.hasOwnProperty("0");
+
+            return verifyValue && verifyWritable && verifyEnumerable && !verifyConfigurable && data === "data";
+        } finally {
+            delete Object.prototype[0];
+        }
+    },
+
+    precondition: function prereq() {
+        return fnExists(Object.defineProperty);
+    }
+});
---
+++ test/suite/ietestcenter/chapter15/15.2/15.2.3/15.2.3.7/15.2.3.7-6-a-285.js
@@ -0,0 +1,60 @@
+/// Copyright (c) 2009 Microsoft Corporation 
+/// 
+/// Redistribution and use in source and binary forms, with or without modification, are permitted provided
+/// that the following conditions are met: 
+///    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+///      the following disclaimer. 
+///    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and 
+///      the following disclaimer in the documentation and/or other materials provided with the distribution.  
+///    * Neither the name of Microsoft nor the names of its contributors may be used to
+///      endorse or promote products derived from this software without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+/// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+/// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+/// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+ES5Harness.registerTest({
+    id: "15.2.3.7-6-a-285",
+
+    path: "TestCases/chapter15/15.2/15.2.3/15.2.3.7/15.2.3.7-6-a-285.js",
+
+    description: "Object.defineProperties - 'O' is an Arguments object, 'P' is own data property of 'O' which is also defined in [[ParameterMap]] of 'O', test TypeError is thrown when updating the [[Configurable]] attribute value of 'P' which is defined as non-configurable (10.6 [[DefineOwnProperty]] step 4)",
+
+    test: function testcase() {
+
+        var arg;
+
+        (function fun(a, b, c) {
+            arg = arguments;
+        }(0, 1, 2));
+
+        Object.defineProperty(arg, "0", {
+            value: 0,
+            writable: false,
+            enumerable: false,
+            configurable: false
+        });
+
+        try {
+            Object.defineProperties(arg, {
+                "0": {
+                    configurable: true
+                }
+            });
+
+            return false; 
+        } catch (e) {
+            return (e instanceof TypeError) && dataPropertyAttributesAreCorrect(arg, "0", 0, false, false, false);
+        }
+    },
+
+    precondition: function prereq() {
+        return fnExists(Object.defineProperties) && fnExists(Object.defineProperty);
+    }
+});
