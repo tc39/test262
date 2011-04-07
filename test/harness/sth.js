08a7f7433a2db1e0329752d16561a745c2805b20
David Fugate <dfugate@microsoft.com>
2011-04-07 10:12:45 -0700

    Few minor changes to sta.js and sth.js to support Strict Mode. Renamed move_test.py to FixTestCasePlacement.py. Still a work in progress.

--- test/harness/sth.js
+++ test/harness/sth.js
@@ -129,7 +129,8 @@ function BrowserRunner() {
                     "  if(test.precondition && !test.precondition()) {" +
                     "    testRun(test.id, test.path, test.description, test.test.toString(),typeof test.precondition !== 'undefined' ? test.precondition.toString() : '', 'fail', 'Precondition Failed');" +
                     "  } else {" +
-                    "    try { var res = test.test.call(window); } catch(e) { res = 'fail'; error = e; }" +
+                    "    var testThis = test.strict===undefined ? window : undefined;" +
+                    "    try { var res = test.test.call(testThis); } catch(e) { res = 'fail'; error = e; }" +
                     "    var retVal = /^s/i.test(test.id) ? (res === true || typeof res === 'undefined' ? 'pass' : 'fail') : (res === true ? 'pass' : 'fail');" +
                     "    testRun(test.id, test.path, test.description, test.test.toString(), typeof test.precondition !== 'undefined' ? test.precondition.toString() : '', retVal, error);" +
                     "  }" +
