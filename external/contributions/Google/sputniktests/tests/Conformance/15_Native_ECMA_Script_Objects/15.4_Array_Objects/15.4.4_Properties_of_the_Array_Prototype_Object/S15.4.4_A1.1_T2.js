bcc15fd553f036ddf1d8746975d85a1d0e2340ae
Mark Miller <erights@gmail.com>
2011-06-23 15:37:18 -0700

    Initial contribution of r88 of Google's Sputnik tests

---
+++ external/contributions/Google/sputniktests/tests/Conformance/15_Native_ECMA_Script_Objects/15.4_Array_Objects/15.4.4_Properties_of_the_Array_Prototype_Object/S15.4.4_A1.1_T2.js
@@ -0,0 +1,17 @@
+// Copyright 2009 the Sputnik authors.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/**
+ * @name: S15.4.4_A1.1_T2;
+ * @section: 15.4.4, 11.4.1;
+ * @assertion: The value of the internal [[Prototype]] property of 
+ * the Array prototype object is the Object prototype object;
+ * @description: delete Array.prototype.toString;
+ * @strict_mode_negative
+*/
+
+//CHECK#1
+delete Array.prototype.toString;
+if (Array.prototype.toString() !== "[object " + "Array" + "]") {
+  $ERROR('#1: delete Array.prototype.toString; var Array.prototype = Object(); Array.prototype.toString() === "[object " + "Array" + "]". Actual: ' + (Array.prototype.toString()));
+}
