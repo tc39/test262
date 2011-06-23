55855b285e67bc29e567c515459726cb1a110163
David Fugate <dfugate@microsoft.com>
2011-06-23 15:04:00 -0700

    Updated test262 with test case additions and edits from the PPB2 release of IE10.  This included: - 22 test case additions (mostly strict mode) - 4 test case edits

--- test/suite/ietestcenter/chapter15/15.2/15.2.3/15.2.3.6/15.2.3.6-4-153.js
+++ test/suite/ietestcenter/chapter15/15.2/15.2.3/15.2.3.6/15.2.3.6-4-153.js
@@ -25,7 +25,7 @@ ES5Harness.registerTest({
 
     path: "TestCases/chapter15/15.2/15.2.3/15.2.3.6/15.2.3.6-4-153.js",
 
-    description: "Object.defineProperty - 'O' is an Array, 'name' is the length prosperty of 'O',  test RangeError is thrown when the [[Value]] field of 'desc' is a negative non-integer values (15.4.5.1 step 3.c)",
+    description: "Object.defineProperty - 'O' is an Array, 'name' is the length property of 'O',  test RangeError is thrown when the [[Value]] field of 'desc' is a negative non-integer values (15.4.5.1 step 3.c)",
 
     test: function testcase() {
 
