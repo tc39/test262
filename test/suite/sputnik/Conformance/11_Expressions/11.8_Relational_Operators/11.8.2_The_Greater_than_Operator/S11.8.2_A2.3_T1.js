45f7cee0194cfec7186f5b272a449357c8801864
Mark Miller <erights@gmail.com>
2011-09-25 11:21:54 -0700

    Fixes https://bugs.ecmascript.org/show_bug.cgi?id=15

--- test/suite/sputnik/Conformance/11_Expressions/11.8_Relational_Operators/11.8.2_The_Greater_than_Operator/S11.8.2_A2.3_T1.js
+++ test/suite/sputnik/Conformance/11_Expressions/11.8_Relational_Operators/11.8.2_The_Greater_than_Operator/S11.8.2_A2.3_T1.js
@@ -2,24 +2,23 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
- * @name: S11.8.2_A2.3_T1;
- * @section: 11.8.2;
- * @assertion: ToNumber(second expression) is called first, and then ToNumber(first expression);
+ * In ES5, First expression should be evaluated first.
+ *
  * @description: Checking with "throw";
-*/
+ */
 
 //CHECK#1
 var x = { valueOf: function () { throw "x"; } };
 var y = { valueOf: function () { throw "y"; } };
 try {
    x > y;
-   $ERROR('#1.1: var x = { valueOf: function () { throw "x"; } }; var y = { valueOf: function () { throw "y"; } }; x > y throw "y". Actual: ' + (x > y));
+   $ERROR('#1.1: Should have thrown');
 } catch (e) {
-   if (e === "x") {
-     $ERROR('#1.2: ToNumber(second expression) is called first, and then ToNumber(first expression)');
+   if (e === "y") {
+     $ERROR('#1.2: First expression should be evaluated first');
    } else {
-     if (e !== "y") {
-       $ERROR('#1.3: var x = { valueOf: function () { throw "x"; } }; var y = { valueOf: function () { throw "y"; } }; x > y throw "y". Actual: ' + (e));
+     if (e !== "x") {
+       $ERROR('#1.3: Failed with: ' + e);
      }
    }
 }
