71b447b8b469fc71989fabc47f6a3fcea478a127
David Fugate <dfugate@microsoft.com>
2011-09-25 12:03:31 -0700

    Removed sputnik_converted.  We now take what's under external/contributions/Google/* directly.

--- test/suite/sputnik_converted/12_Statement/12.5_The_if_Statement/S12.5_A2.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2009 the Sputnik authors.  All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-
-// Converted for Test262 from original Sputnik source
-
- if (eval("true")) $FAIL('#1: In the "if" Statement eval as Expression is admitted');
- 
-
