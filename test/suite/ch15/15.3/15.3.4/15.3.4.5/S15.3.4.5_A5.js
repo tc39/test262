5a935ed229102851707f09b15cf959f03d2a0e9f
Mark Miller <erights@gmail.com>
2011-11-09 12:02:28 -0800

    Revise opera crash exclusions to exclude only the test that causes the crash

--- test/suite/ch15/15.3/15.3.4/15.3.4.5/S15.3.4.5_A5.js
+++ test/suite/ch15/15.3/15.3.4/15.3.4.5/S15.3.4.5_A5.js
@@ -11,7 +11,7 @@ function construct(f, args) {
   var bound = Function.prototype.bind.apply(f, [null].concat(args));
   return new bound();
 }
-var d = construct(Date, [1957, 5, 27]);
+var d = construct(Date, [1957, 4, 27]);
 if (Object.prototype.toString.call(d) !== '[object Date]') {
   $ERROR('Using the Date constructor via .bind did not create a Date.');
 }
