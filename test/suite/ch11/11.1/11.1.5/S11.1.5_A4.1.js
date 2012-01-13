d8add6e8c680858a54facf2cafdde6718225de5f
David Fugate <dfugate@microsoft.com>
2012-01-13 09:51:10 -0800

    https://bugs.ecmascript.org/show_bug.cgi?id=14 fixed.

--- test/suite/ch11/11.1/11.1.5/S11.1.5_A4.1.js
+++ test/suite/ch11/11.1/11.1.5/S11.1.5_A4.1.js
@@ -2,11 +2,10 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
- * The PropertyName is not BooleanLiteral
+ * The PropertyName is not really a BooleanLiteral
  *
  * @path ch11/11.1/11.1.5/S11.1.5_A4.1.js
- * @description Checking if execution of "var object = {true : 1}" fails
- * @negative
+ * @description Checking if execution of "var object = {true : 1}" does not fail
  */
 
 //CHECK#1
--- test/suite/ch11/11.1/11.1.5/S11.1.5_A4.2.js
+++ test/suite/ch11/11.1/11.1.5/S11.1.5_A4.2.js
@@ -2,13 +2,11 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
- * The PropertyName is not nullLiteral
+ * The PropertyName is not really a nullLiteral
  *
  * @path ch11/11.1/11.1.5/S11.1.5_A4.2.js
- * @description Checking if execution of "var object = {null : true}" fails
- * @negative
+ * @description Checking if execution of "var object = {null : true}" does not fail
  */
 
 //CHECK#1
 var object = {null : true};
-
