e94f47829ee1df734ec3aa463da317af6f67bd1e
Hank Yates <hankyates@gmail.com>
2014-01-26 16:48:37 -0800

    Adding Array.prototype#fill tests

---
+++ test/suite/es6/ch22/22.1/22.1.3/S22.1.3.6_T1.js
@@ -0,0 +1,28 @@
+/**
+ * @description Testing Array#fill
+ * @author Hank Yates (hankyates@gmail.com)
+ */
+
+runTestCase(function () {
+  var testArr = new Array('testString', 'anotherTestString', 3),
+      updatedArr = testArr.fill('newValue', 1, 3);
+
+  if (updatedArr[3] !== 'newValue') {
+    return false;
+  }
+
+  if (updatedArr[2] !== 'newValue') {
+    return false;
+  }
+
+  if (updatedArr[0] !== 'testString') {
+    return false;
+  }
+
+  if (updatedArr.length !== 4) {
+    return false;
+  }
+
+  return true;
+
+});
