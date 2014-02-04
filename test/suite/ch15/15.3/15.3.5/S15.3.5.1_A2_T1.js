81de0d328546eba2f4228cb7ae754100c753363f
André Bargull <andre.bargull@gmail.com>
2014-02-04 18:25:47 +0100

    'length' property of user-defined functions is configurable

--- test/suite/ch15/15.3/15.3.5/S15.3.5.1_A2_T1.js
+++ test/suite/ch15/15.3/15.3.5/S15.3.5.1_A2_T1.js
@@ -2,10 +2,10 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
- * the length property has the attributes { DontDelete }
+ * the length property does not have the attributes { DontDelete }
  *
  * @path ch15/15.3/15.3.5/S15.3.5.1_A2_T1.js
- * @description Checking if deleting the length property of Function("arg1,arg2,arg3", null) fails
+ * @description Checking if deleting the length property of Function("arg1,arg2,arg3", null) succeeds
  */
 
 f = new Function("arg1,arg2,arg3", null);
@@ -16,17 +16,17 @@ if (!(f.hasOwnProperty('length'))) {
 }
 
 //CHECK#2
-if(delete f.length){
-  $ERROR('#2: the function.length property has the attributes DontDelete.');
+if(!delete f.length){
+  $ERROR('#2: the function.length property does not have the attributes DontDelete.');
 }
 
 //CHECK#3
-if (!(f.hasOwnProperty('length'))) {
-  $ERROR('#3: the function.length property has the attributes DontDelete.');
+if (f.hasOwnProperty('length')) {
+  $ERROR('#3: the function.length property does not have the attributes DontDelete.');
 }
 
 //CHECK#4
-if (f.length !== 3) {
-  $ERROR('#4: the length property has the attributes { DontDelete }');
+if (f.length === 3) {
+  $ERROR('#4: the length property does not have the attributes { DontDelete }');
 }
 
