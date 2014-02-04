2a58081ff3a1b8cf54b46247e91eaf212415750e
André Bargull <andre.bargull@gmail.com>
2014-02-04 13:45:14 +0100

    Add semicolon to ensure code is not parsed as ObjectLiteral with shorthand initialiser

--- test/suite/ch12/12.1/S12.1_A4_T2.js
+++ test/suite/ch12/12.1/S12.1_A4_T2.js
@@ -5,7 +5,7 @@
  * The production Block can't be inside of expression
  *
  * @path ch12/12.1/S12.1_A4_T2.js
- * @description Checking if execution of "y={x}" fails
+ * @description Checking if execution of "y={x;}" fails
  * @negative
  */
 
@@ -13,7 +13,7 @@ x=1;
 
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#1
-y={x};
+y={x;};
 //
 //////////////////////////////////////////////////////////////////////////////
 
