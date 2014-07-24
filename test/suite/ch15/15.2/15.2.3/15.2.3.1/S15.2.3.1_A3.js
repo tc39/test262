509472bb74ae1f6b523f1e40dc6f4f1a0c8545e7
Brian Terlson <brian.terlson@microsoft.com>
2014-07-24 15:29:21 -0700

    Fix suspect formatting in S15.2.3.1_A3

--- test/suite/ch15/15.2/15.2.3/15.2.3.1/S15.2.3.1_A3.js
+++ test/suite/ch15/15.2/15.2.3/15.2.3.1/S15.2.3.1_A3.js
@@ -2,11 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /**
- * @path ch15/15.2/15.2.3/15.2.3.1/S15.2.3.1_A3.js
- */
-
-
-/**
+* @path ch15/15.2/15.2.3/15.2.3.1/S15.2.3.1_A3.js
 * @name: S15.2.3.1_A3;
 * @section: 15.2.3.1, 15.2.4;
 * @assertion: The Object.prototype property has the attribute DontDelete;
