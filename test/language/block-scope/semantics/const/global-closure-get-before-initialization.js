3f9587a8ee98c73dc39f28978da936ed8213aa63
Rick Waldron <waldron.rick@gmail.com>
2015-02-26 14:13:00 -0500

    Import tests from Google V8 (Block Scoping)
    These tests are derived from the following files within the Google V8 project:
    
    	test/mjsunit/harmony/block-conflicts.js
    	test/mjsunit/harmony/block-for.js
    	test/mjsunit/harmony/block-leave.js
    	test/mjsunit/harmony/block-let-declaration.js
    	test/mjsunit/harmony/block-let-semantics.js
    	test/mjsunit/harmony/block-scoping.js

---
+++ test/language/block-scope/semantics/const/global-closure-get-before-initialization.js
@@ -0,0 +1,12 @@
+// Copyright (C) Copyright 2011 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 13.1
+description: >
+    const: global closure [[Get]] before initialization.
+    (TDZ, Temporal Dead Zone)
+negative: ReferenceError
+---*/
+function f() { return x + 1; }
+f();
+const x = 1;
---
+++ test/language/block-scope/syntax/for-loop/const-invalid-assignment-next-expression.js
@@ -0,0 +1,10 @@
+// Copyright (C) Copyright 2015 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 13.1
+description: >
+    const: invalid assignment in next expression
+negative: TypeError
+---*/
+
+for (const i = 0; i < 1; i++) { }
