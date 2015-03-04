4c49047f20b4674f69fa47a54bd8337fe7a7aca2
Rick Waldron <waldron.rick@gmail.com>
2015-03-04 13:48:05 -0500

    Import tests from Google V8 (Object Literal Concise Method and Property Initialization)
    These tests are derived from the following files within the Google V8 project:
    
            test/mjsunit/harmony/method-name-eval-arguments.js
            test/mjsunit/harmony/object-literals-method.js
            test/mjsunit/harmony/object-literals-property-shorthand.js
            test/mjsunit/harmony/object-literals-super.js

---
+++ test/language/object-literal/not-defined.js
@@ -0,0 +1,9 @@
+// Copyright (C) Copyright 2015 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+es6id: 12.2.5
+description: >
+    Throws when IdentifierReference is undefined
+negative: ReferenceError
+---*/
+var o = {notDefined};
