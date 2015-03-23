98627cddfa3dde35ae95a5d8cb0542d5954fc8aa
André Bargull <andre.bargull@gmail.com>
2015-03-23 17:34:52 +0100

    Strict eval variable declarations are lexically scoped, assign expected result value to a previously declared global (issue #35)

--- test/language/statements/break/S12.8_A2.js
+++ test/language/statements/break/S12.8_A2.js
@@ -11,11 +11,13 @@ description: >
     Identifier
 ---*/
 
+var result;
+
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#1
 try{
-	eval("FOR1 : for(var i=1;i<2;i++){ LABEL1 : do {var x =1;break\u000AFOR1;var y=2;} while(0);}");
-	if (i!==2) {
+	eval("FOR1 : for(var i=1;i<2;i++){ LABEL1 : do {var x =1;break\u000AFOR1;var y=2;} while(0);} result = i;");
+	if (result!==2) {
 		$ERROR('#1: Since LineTerminator(U-000A) between break and Identifier not allowed break evaluates without label');
 	}
 } catch(e){
@@ -27,8 +29,8 @@ try{
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#2
 try{
-	eval("FOR2 : for(var i=1;i<2;i++){ LABEL2 : do {var x =1;break\u000DFOR2;var y=2;} while(0);}");
-	if (i!==2) {
+	eval("FOR2 : for(var i=1;i<2;i++){ LABEL2 : do {var x =1;break\u000DFOR2;var y=2;} while(0);} result = i;");
+	if (result!==2) {
 		$ERROR('#2: Since LineTerminator(U-000D) between break and Identifier not allowed break evaluates without label');
 	}
 } catch(e){
@@ -40,8 +42,8 @@ try{
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#3
 try{
-	eval("FOR3 : for(var i=1;i<2;i++){ LABEL3 : do {var x =1;break\u2028FOR3;var y=2;} while(0);}");
-	if (i!==2) {
+	eval("FOR3 : for(var i=1;i<2;i++){ LABEL3 : do {var x =1;break\u2028FOR3;var y=2;} while(0);} result = i;");
+	if (result!==2) {
 		$ERROR('#3: Since LineTerminator(U-2028) between break and Identifier not allowed break evaluates without label');
 	}
 } catch(e){
@@ -53,8 +55,8 @@ try{
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#4
 try{
-	eval("FOR4 : for(var i=1;i<2;i++){ LABEL4 : do {var x =1;break\u2029FOR4;var y=2;} while(0);}");
-	if (i!==2) {
+	eval("FOR4 : for(var i=1;i<2;i++){ LABEL4 : do {var x =1;break\u2029FOR4;var y=2;} while(0);} result = i;");
+	if (result!==2) {
 		$ERROR('#4: Since LineTerminator(U-2029) between break and Identifier not allowed break evaluates without label');
 	}
 } catch(e){
