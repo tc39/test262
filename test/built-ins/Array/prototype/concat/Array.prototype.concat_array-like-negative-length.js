19481d380193c7935cad01515f25bac38fa3d2d8
Brian Terlson <brian.terlson@microsoft.com>
2015-05-15 15:22:12 -0700

    Use a very negative number so ES5 implementations don't loop forever

--- test/built-ins/Array/prototype/concat/Array.prototype.concat_array-like-negative-length.js
+++ test/built-ins/Array/prototype/concat/Array.prototype.concat_array-like-negative-length.js
@@ -9,14 +9,14 @@ description: Array.prototype.concat array like negative length
 includes: [compareArray.js]
 ---*/
 var obj = {
-  "length": -6,
+  "length": -4294967294,
   "1": "A",
   "3": "B",
   "5": "C"
 };
 obj[Symbol.isConcatSpreadable] = true;
 assert(compareArray([].concat(obj), []));
-obj.length = -6.7;
+obj.length = -4294967294;
 assert(compareArray([].concat(obj), []));
-obj.length = "-6";
+obj.length = "-4294967294";
 assert(compareArray([].concat(obj), []));
