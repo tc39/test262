e0387bcee86f844300dd92b8abf3c2f5a95e57d9
Mike Pennisi <mike@mikepennisi.com>
2015-05-26 11:05:12 -0400

    Refactor tests for built-in @@iterator methods
    Assert the value of the properties and update the test frontmatter to
    include the `Symbol.iterator` feature.

--- test/language/arguments-object/mapped/Symbol.iterator.js
+++ test/language/arguments-object/mapped/Symbol.iterator.js
@@ -1,16 +1,16 @@
 // Copyright (C) Copyright 2014 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-es6id: 9.4.4.6 S7
+es6id: 9.4.4.7 S22
 description: >
-    Unmapped arguments exotic objects should implement the Array iterator
+    Mapped arguments exotic objects should implement the Array iterator
     protocol.
 includes: [propertyHelper.js]
 flags: [noStrict]
+features: [Symbol.iterator]
 ---*/
 
 (function() {
-  'use strict';
   var descriptor = Object.getOwnPropertyDescriptor(arguments, Symbol.iterator);
 
   assert.sameValue(arguments[Symbol.iterator], [][Symbol.iterator]);
