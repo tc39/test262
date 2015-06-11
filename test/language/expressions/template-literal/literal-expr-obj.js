bd3d160ba1e52b66e352c90633bf7f42139e02a6
Mike Pennisi <mike@mikepennisi.com>
2015-06-11 12:00:29 -0400

    Reorganize existing tests

--- test/language/expressions/template-literal/literal-expr-obj.js
+++ test/language/expressions/template-literal/literal-expr-obj.js
@@ -1,11 +1,16 @@
 // Copyright (C) Copyright 2014 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-es6id: 12.2.8
-description: >
-    Expressions should be evaluated and converted to Strings according to the
-    ToString abstract operation.
+es6id: 12.2.8.5
+description: Object reference in expression position of TemplateLiteral
+info: >
+    TemplateLiteral : TemplateHead Expression TemplateSpans
+
+    1. Let head be the TV of TemplateHead as defined in 11.8.6.
+    2. Let sub be the result of evaluating Expression.
+    3. Let middle be ToString(sub).
 ---*/
+
 var plain = {};
 var custom = {
   toString: function() {
