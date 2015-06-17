fabf5f33203ae794c0b0798e1006775b5c4cb55a
Mike Pennisi <mike@mikepennisi.com>
2015-06-17 13:09:37 -0400

    Correct typo in test descriptions

--- test/language/expressions/compound-assignment/11.13.2-47-s.js
+++ test/language/expressions/compound-assignment/11.13.2-47-s.js
@@ -9,7 +9,7 @@ es5id: 11.13.2-47-s
 description: >
     Strict Mode - TypeError is thrown if The LeftHandSide of a
     Compound Assignment operator(%=) is a reference to a non-existent
-    property of an object whose [[Extensible]] internal property if
+    property of an object whose [[Extensible]] internal property is
     false
 flags: [onlyStrict]
 includes: [runTestCase.js]
--- test/language/expressions/compound-assignment/11.13.2-53-s.js
+++ test/language/expressions/compound-assignment/11.13.2-53-s.js
@@ -9,7 +9,7 @@ es5id: 11.13.2-53-s
 description: >
     Strict Mode - TypeError is thrown if The LeftHandSide of a
     Compound Assignment operator(&=) is a reference to a non-existent
-    property of an object whose [[Extensible]] internal property if
+    property of an object whose [[Extensible]] internal property is
     false
 flags: [onlyStrict]
 includes: [runTestCase.js]
