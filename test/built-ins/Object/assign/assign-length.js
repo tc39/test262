07b21a5e448ed9d8dcf611106386dabe9154ba8b
Yanli Xu <v-yanlxu@microsoft.com>
2015-06-18 15:23:15 +0800

    add tests for Object.assign

---
+++ test/built-ins/Object/assign/assign-length.js
@@ -0,0 +1,9 @@
+// Copyright 2015 Microsoft Corporation. All rights reserved.
+// This code is governed by the license found in the LICENSE file.
+
+/*---
+description: The length property of the assign method should be 2
+es6id:  19.1.2.1
+---*/
+
+assert.sameValue(Object.assign.length, 2, "The length property of the assign method should be 2.");
