a330d0d979f6b0458162465e08ed31fa2be06cb8
Leonardo Balter <leonardo.balter@gmail.com>
2015-06-30 17:12:50 -0400

    Improve coverage of SetIteratorPrototype

--- test/built-ins/SetIteratorPrototype/next/iteration-mutable.js
+++ test/built-ins/SetIteratorPrototype/next/iteration-mutable.js
@@ -1,14 +1,14 @@
 // Copyright (C) 2014 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
-
 /*---
-  description: >
-      When an item is added to the set after the iterator is created but before
-      the iterator is "done" (as defined by 23.2.5.2.1), the new item should be
-      accessible via iteration. When an item is added to the set after the
-      iterator is "done", the new item should not be accessible via iteration.
-  es6id: 23.2.3.11
- ---*/
+es6id: 23.2.3.11
+description: >
+  When an item is added to the set after the iterator is created but before
+  the iterator is "done" (as defined by 23.2.5.2.1), the new item should be
+  accessible via iteration. When an item is added to the set after the
+  iterator is "done", the new item should not be accessible via iteration.
+features: [Symbol.iterator]
+---*/
 
 var set = new Set();
 set.add(1);
