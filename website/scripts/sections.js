a85f5039e0a646ba07408fd3b00d1d3950031915
André Bargull <andre.bargull@gmail.com>
2015-07-17 17:42:45 +0200

    Remove links to hg.ecmascript.org

--- website/scripts/sections.js
+++ website/scripts/sections.js
@@ -1,8 +1,5 @@
 /// Copyright (c) 2012 Ecma International.  All rights reserved. 
-/// Ecma International makes this code available under the terms and conditions set
-/// forth on http://hg.ecmascript.org/tests/test262/raw-file/tip/LICENSE (the 
-/// "Use Terms").   Any redistribution of this code must retain the above 
-/// copyright and this notice and otherwise comply with the Use Terms.
+/// This code is governed by the BSD license found in the LICENSE file.
 
 /* A section of the spec. Stores test results and subsections and some rolled up stats on how many tests passed or
  * failed under that section
