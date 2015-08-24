69969e16b1f03fc99343f3051a522b2a5e770081
Kevin Gibbons <bakkot@gmail.com>
2015-08-24 15:24:10 -0700

    Remove extraneous indent in header, for consistency

--- test/language/identifiers/val-yield-strict.js
+++ test/language/identifiers/val-yield-strict.js
@@ -2,12 +2,12 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-  description: >
-      `yield` is a reserved identifier in strict mode code and may not be used
-      as an identifier.
-  es6id: 12.1.1
-  negative: SyntaxError
-  flags: [onlyStrict]
+description: >
+    `yield` is a reserved identifier in strict mode code and may not be used
+    as an identifier.
+es6id: 12.1.1
+negative: SyntaxError
+flags: [onlyStrict]
  ---*/
 
 var yield = 13;
