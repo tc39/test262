4bdd808d048dec99d4f03067ce7d504f7bfc39be
Leonardo Balter <leonardo.balter@gmail.com>
2016-01-22 15:00:44 -0500

    Update %TypedArray% function length

--- test/built-ins/TypedArray/length.js
+++ test/built-ins/TypedArray/length.js
@@ -1,23 +1,22 @@
 // Copyright (C) 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-es6id: 22.2.2
+es7id: pending
 description: >
-  TypedArray has a "length" property whose value is 3.
+  TypedArray has a "length" property whose value is 0.
 info: >
-  22.2.2 Properties of the %TypedArray% Intrinsic Object
+  22.2.1.1 %TypedArray% ()
 
-  Besides a length property whose value is 3 and a name property whose value is
-  "TypedArray", %TypedArray% has the following properties:
+  The length property of the %TypedArray% constructor function is 0.
   ...
 
-  ES6 section 17: Unless otherwise specified, the length property of a built-in
+  ES7 section 17: Unless otherwise specified, the length property of a built-in
   Function object has the attributes { [[Writable]]: false, [[Enumerable]]:
   false, [[Configurable]]: true }.
 includes: [propertyHelper.js, testTypedArray.js]
 ---*/
 
-assert.sameValue(TypedArray.length, 3);
+assert.sameValue(TypedArray.length, 0);
 
 verifyNotEnumerable(TypedArray, 'length');
 verifyNotWritable(TypedArray, 'length');
