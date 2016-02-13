428418d5c7556aa1302ae40ec141f6ccc74bc02b
Gorkem Yakin <goyakin@microsoft.com>
2016-02-12 16:27:10 -0800

    Replace remaining es7id tags with id

--- test/built-ins/TypedArray/length.js
+++ test/built-ins/TypedArray/length.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-es7id: pending
+id: pending
 description: >
   TypedArray has a "length" property whose value is 0.
 info: >
