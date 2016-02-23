2db5766300812589f15cecaee3b8b6fb116556a1
Leonardo Balter <leonardo.balter@gmail.com>
2016-02-23 13:50:13 -0500

    Update descriptor tests for %TypedArray%.prototype properties

--- test/built-ins/TypedArray/prototype/buffer/prop-desc.js
+++ test/built-ins/TypedArray/prototype/buffer/prop-desc.js
@@ -7,11 +7,17 @@ description: >
 info: >
   %TypedArray%.prototype.buffer is an accessor property whose set accessor
   function is undefined.
-includes: [testTypedArray.js]
+
+  Section 17: Every accessor property described in clauses 18 through 26 and in
+  Annex B.2 has the attributes {[[Enumerable]]: false, [[Configurable]]: true }
+includes: [propertyHelper.js, testTypedArray.js]
 ---*/
 
 var TypedArrayPrototype = TypedArray.prototype;
-var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, 'buffer');
+var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, "buffer");
 
 assert.sameValue(desc.set, undefined);
-assert.sameValue(typeof desc.get, 'function');
+assert.sameValue(typeof desc.get, "function");
+
+verifyNotEnumerable(TypedArrayPrototype, "buffer");
+verifyConfigurable(TypedArrayPrototype, "buffer");
--- test/built-ins/TypedArray/prototype/byteOffset/prop-desc.js
+++ test/built-ins/TypedArray/prototype/byteOffset/prop-desc.js
@@ -7,11 +7,17 @@ description: >
 info: >
   %TypedArray%.prototype.byteOffset is an accessor property whose set accessor
   function is undefined.
-includes: [testTypedArray.js]
+
+  Section 17: Every accessor property described in clauses 18 through 26 and in
+  Annex B.2 has the attributes {[[Enumerable]]: false, [[Configurable]]: true }
+includes: [propertyHelper.js, testTypedArray.js]
 ---*/
 
 var TypedArrayPrototype = TypedArray.prototype;
-var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, 'byteOffset');
+var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, "byteOffset");
 
 assert.sameValue(desc.set, undefined);
-assert.sameValue(typeof desc.get, 'function');
+assert.sameValue(typeof desc.get, "function");
+
+verifyNotEnumerable(TypedArrayPrototype, "byteOffset");
+verifyConfigurable(TypedArrayPrototype, "byteOffset");
