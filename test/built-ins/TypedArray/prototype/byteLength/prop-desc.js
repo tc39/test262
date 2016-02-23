7445d77d6d6b81dd92bc0f9a9f0e25a12b511e0e
Leonardo Balter <leonardo.balter@gmail.com>
2016-02-23 13:50:13 -0500

    Update descriptor tests for %TypedArray%.prototype properties

--- test/built-ins/TypedArray/prototype/byteLength/prop-desc.js
+++ test/built-ins/TypedArray/prototype/byteLength/prop-desc.js
@@ -7,11 +7,17 @@ description: >
 info: >
   %TypedArray%.prototype.byteLength is an accessor property whose set accessor
   function is undefined.
-includes: [testTypedArray.js]
+
+  Section 17: Every accessor property described in clauses 18 through 26 and in
+  Annex B.2 has the attributes {[[Enumerable]]: false, [[Configurable]]: true }
+includes: [propertyHelper.js, testTypedArray.js]
 ---*/
 
 var TypedArrayPrototype = TypedArray.prototype;
-var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, 'byteLength');
+var desc = Object.getOwnPropertyDescriptor(TypedArrayPrototype, "byteLength");
 
 assert.sameValue(desc.set, undefined);
-assert.sameValue(typeof desc.get, 'function');
+assert.sameValue(typeof desc.get, "function");
+
+verifyNotEnumerable(TypedArrayPrototype, "byteLength");
+verifyConfigurable(TypedArrayPrototype, "byteLength");
