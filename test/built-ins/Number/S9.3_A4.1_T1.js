f7aa31b41faee25823bb38b7ca53b104cf53debd
Mike Pennisi <mike@mikepennisi.com>
2016-03-11 10:25:06 -0500

    Remove over-specified tests
    Except for a small set of expected input/output pairs, both ES5 and
    ES2015 define the expected return value of these methods in terms of an
    "implementation-dependent approximation." This makes it inappropriate to
    enforce expectations for specific values, even if expressed imprecisely.

--- test/built-ins/Number/S9.3_A4.1_T1.js
+++ test/built-ins/Number/S9.3_A4.1_T1.js
@@ -30,13 +30,3 @@ if (Number(1.3) !== 1.3) {
 if (Number(-1.3) !== -1.3) {
   $ERROR('#4: Number(-1.3) === -1.3. Actual: ' + (Number(-1.3)));
 }
-
-// CHECK#5
-if (Number(Number.MAX_VALUE) !== 1.7976931348623157e308) {
-  $ERROR('#5: Number(Number.MAX_VALUE) === 1.7976931348623157e308. Actual: ' + (Number(Number.MAX_VALUE)));
-}
-
-// CHECK#6
-if (Number(Number.MIN_VALUE) !== 5e-324) {
-  $ERROR('#6: Number(Number.MIN_VALUE) === 5e-324. Actual: ' + (Number(Number.MIN_VALUE)));
-}
