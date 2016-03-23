333cd35025ecf86e8942d327c6fc549e05d4042c
Michael Ficarra <git@michael.ficarra.me>
2016-03-23 10:06:03 -0700

    fix syntax error for function declaration

--- test/built-ins/Function/prototype/toString/class-declaration-explicit-ctor.js
+++ test/built-ins/Function/prototype/toString/class-declaration-explicit-ctor.js
@@ -10,4 +10,4 @@ description: Function.prototype.toString on a class declaration (explicit constr
 
 assert.sameValue(A.toString(), "class /* a */ A /* b */ extends /* c */ B /* d */ { /* e */ constructor /* f */ ( /* g */ ) /* h */ { /* i */ ; /* j */ } /* k */ m /* l */ ( /* m */ ) /* n */ { /* o */ } /* p */ }");
 
-function B{}
+function B(){}
