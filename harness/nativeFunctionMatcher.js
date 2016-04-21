ba3dacbe5aca4a76055cd4f0617b05627024c6c3
jugglinmike <mike@mikepennisi.com>
2016-04-21 15:55:54 -0400

    Correct tests for native functions' toString value (#580)
    Modify the regular expression for native functions' toString value to
    satisfy all test cases. Correct the test file's reference to the harness
    file. Re-format the test file's assertions to aid debugging in the event
    of failure.

--- harness/nativeFunctionMatcher.js
+++ harness/nativeFunctionMatcher.js
@@ -1,1 +1,5 @@
-const NATIVE_FUNCTION_RE = /\bfunction\b[\s\S]+\b\w+\b[\s\S]*\([\s\S]*\)[\s\S]*\{[\s\S]*\[[\s\S]*\bnative\b[\s\S]+\bcode\b[\s\S]*\][\s\S]*\}/;
+/**
+ * This regex makes a best-effort determination that the tested string matches
+ * the NativeFunction grammar production without requiring a correct tokeniser.
+ */
+const NATIVE_FUNCTION_RE = /\bfunction\b[\s\S]*\([\s\S]*\)[\s\S]*\{[\s\S]*\[[\s\S]*\bnative\b[\s\S]+\bcode\b[\s\S]*\][\s\S]*\}/;
