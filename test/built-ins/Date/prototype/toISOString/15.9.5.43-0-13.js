664ec78fccb5dfa7f346f82a7194f0e357416b10
Mythri <mythria@chromium.org>
2016-04-25 16:45:03 +0100

    Moves Date constructor outside of assert, so that it actually tests toISOString

--- test/built-ins/Date/prototype/toISOString/15.9.5.43-0-13.js
+++ test/built-ins/Date/prototype/toISOString/15.9.5.43-0-13.js
@@ -8,11 +8,8 @@ description: >
     date is outside the valid range of time. 
 ---*/
 
-        var date, dateStr;
-
+  // As specified in ES5 15.9.1.14, time > 8.64e15 is not in the valid range.
+  var date = new Date(8.64e15 + 1);
 assert.throws(RangeError, function() {
-                // As specified in ES5 15.9.1.14, time > 8.64e15 is not in the
-                // valid range.
-                date = new Date(8.64e15 + 1);
-                dateStr = date.toISOString();
+                date.toISOString();
 });
