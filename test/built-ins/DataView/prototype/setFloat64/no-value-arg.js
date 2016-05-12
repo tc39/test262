3cf8bd3362323c7c9cbc7ade2743079e657a056b
Leonardo Balter <leonardo.balter@gmail.com>
2016-05-12 18:25:59 -0400

    Add tests for DataView.prototype.setInt32

--- test/built-ins/DataView/prototype/setFloat64/no-value-arg.js
+++ test/built-ins/DataView/prototype/setFloat64/no-value-arg.js
@@ -22,13 +22,12 @@ info: |
   24.1.1.6 SetValueInBuffer ( arrayBuffer, byteIndex, type, value [ , isLittleEndian ] )
 
   ...
-  8. If type is "Float32", then
-    a. Set rawBytes to a List containing the 4 bytes that are the result of
-    converting value to IEEE 754-2008 binary32 format using “Round to nearest,
-    ties to even” rounding mode. If isLittleEndian is false, the bytes are
+  9. Else if type is "Float64", then
+    a. Set rawBytes to a List containing the 8 bytes that are the IEEE 754-2008
+    binary64 format encoding of value. If isLittleEndian is false, the bytes are
     arranged in big endian order. Otherwise, the bytes are arranged in little
     endian order. If value is NaN, rawValue may be set to any implementation
-    chosen IEEE 754-2008 binary32 format Not-a-Number encoding. An
+    chosen IEEE 754-2008 binary64 format Not-a-Number encoding. An
     implementation must always choose the same encoding for each implementation
     distinguishable NaN value.
   ...
--- test/built-ins/DataView/prototype/setInt32/index-to-integer.js
+++ test/built-ins/DataView/prototype/setInt32/index-to-integer.js
@@ -17,7 +17,7 @@ info: >
     ...
 ---*/
 
-var dataView = new DataView(new ArrayBuffer(8));
+var dataView = new DataView(new ArrayBuffer(8), 0);
 
 dataView.setInt32(+0, 1);
 assert.sameValue(dataView.getInt32(0), 1, "setInt32(+0, 1)");
