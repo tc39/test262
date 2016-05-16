5443c2571d594710a683904d838eb67e4fe604d0
Leonardo Balter <leonardo.balter@gmail.com>
2016-05-16 13:41:03 -0400

    fixup! getUint32: distinct values

--- test/built-ins/DataView/prototype/getUint32/return-values-custom-offset.js
+++ test/built-ins/DataView/prototype/getUint32/return-values-custom-offset.js
@@ -36,22 +36,23 @@ sample.setUint8(0, 39);
 sample.setUint8(1, 2);
 sample.setUint8(2, 6);
 sample.setUint8(3, 2);
-sample.setUint8(4, 0);
+sample.setUint8(4, 128);
 sample.setUint8(5, 0);
-sample.setUint8(6, 0);
+sample.setUint8(6, 128);
 sample.setUint8(7, 1);
-sample.setUint8(8, 0);
+sample.setUint8(8, 127);
 sample.setUint8(9, 0);
-sample.setUint8(10, 0);
+sample.setUint8(10, 127);
+sample.setUint8(11, 1);
 
 sample = new DataView(buffer, 4);
 
-assert.sameValue(sample.getUint32(0, false), 1, "0, false");
-assert.sameValue(sample.getUint32(1, false), 256, "1, false");
-assert.sameValue(sample.getUint32(2, false), 65536, "2, false");
-assert.sameValue(sample.getUint32(3, false), 16777216, "3, false");
+assert.sameValue(sample.getUint32(0, false), 2147516417, "0, false");
+assert.sameValue(sample.getUint32(1, false), 8388991, "1, false");
+assert.sameValue(sample.getUint32(2, false), 2147581696, "2, false");
+assert.sameValue(sample.getUint32(3, false), 25100415, "3, false");
 
-assert.sameValue(sample.getUint32(0, true), 16777216, "0, true");
-assert.sameValue(sample.getUint32(1, true), 65536, "1, true");
-assert.sameValue(sample.getUint32(2, true), 256, "2, true");
-assert.sameValue(sample.getUint32(3, true), 1, "3, true");
+assert.sameValue(sample.getUint32(0, true), 25165952, "0, true");
+assert.sameValue(sample.getUint32(1, true), 2130804736, "1, true");
+assert.sameValue(sample.getUint32(2, true), 8323456, "2, true");
+assert.sameValue(sample.getUint32(3, true), 2130738945, "3, true");
