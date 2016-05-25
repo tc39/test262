88879de7359a31f0a9edac5e43505329e1e16e82
Mike Pennisi <mike@mikepennisi.com>
2016-05-25 17:29:27 -0400

    Generate tests

---
+++ test/language/expressions/assignment/dstr-obj-id-init-fn-name-class.js
@@ -0,0 +1,43 @@
+// This file was procedurally generated from the following sources:
+// - src/dstr-assignment/obj-id-init-fn-name-class.case
+// - src/dstr-assignment/default/assignment-expr.template
+/*---
+description: Assignment of function `name` attribute (ClassExpression) (AssignmentExpression)
+esid: sec-variable-statement-runtime-semantics-evaluation
+es6id: 13.3.2.4
+features: [class, destructuring-binding]
+flags: [generated]
+includes: [propertyHelper.js]
+info: |
+    VariableDeclaration : BindingPattern Initializer
+
+    1. Let rhs be the result of evaluating Initializer.
+    2. Let rval be GetValue(rhs).
+    3. ReturnIfAbrupt(rval).
+    4. Return the result of performing BindingInitialization for
+       BindingPattern passing rval and undefined as arguments.
+
+    AssignmentProperty : IdentifierReference Initializeropt
+    [...] 6. If Initializeropt is present and v is undefined, then
+       [...]
+       d. If IsAnonymousFunctionDefinition(Initializer) is true, then
+          i. Let hasNameProperty be HasOwnProperty(v, "name").
+          ii. ReturnIfAbrupt(hasNameProperty).
+          iii. If hasNameProperty is false, perform SetFunctionName(v, P).
+
+---*/
+var xCls, cls;
+
+var result;
+var vals = {};
+
+result = { xCls = class x {}, cls = class {} } = vals;
+
+assert.notSameValue(xCls.name, 'xCls');
+
+assert.sameValue(cls.name, 'cls');
+verifyNotEnumerable(cls, 'name');
+verifyNotWritable(cls, 'name');
+verifyConfigurable(cls, 'name');
+
+assert.sameValue(result, vals);
