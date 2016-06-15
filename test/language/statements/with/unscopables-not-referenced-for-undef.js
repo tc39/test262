6a3837fc0fb23ad02c208ff83e458325b7c1f645
Mike Pennisi <mike@mikepennisi.com>
2016-06-15 13:52:13 -0400

    Add test for abrupt completion from HasBinding
    Ensure that when HasBinding of an Object environment record returns an
    abrupt completion, that same completion is returned to the runtime.
    Update the meta-data of related tests for consistency with this new
    test.

--- test/language/statements/with/unscopables-not-referenced-for-undef.js
+++ test/language/statements/with/unscopables-not-referenced-for-undef.js
@@ -2,21 +2,29 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-es6id: 8.1.1.2.1
+esid: sec-getidentifierreference
+es6id: 8.1.2.1
 description: >
-    `Symbol.unscopables` is not referenced when environment record does not have requested property
-info: >
-    1. Let envRec be the object Environment Record for which the method was
-       invoked.
-    2. Let bindings be the binding object for envRec.
-    3. Let foundBinding be HasProperty(bindings, N)
-    4. ReturnIfAbrupt(foundBinding).
-    5. If foundBinding is false, return false.
+  `Symbol.unscopables` is not referenced when environment record does not have
+  requested property
+info: |
+  [...]
+  2. Let envRec be lex's EnvironmentRecord.
+  3. Let exists be ? envRec.HasBinding(name).
 
-    ES6: 13.11.7 (The `with` Statement) Runtime Semantics: Evaluation
-    [...]
-    6. Set the withEnvironment flag of newEnv’s EnvironmentRecord to true.
-    [...]
+  8.1.1.2.1 HasBinding
+
+  1. Let envRec be the object Environment Record for which the method was
+     invoked.
+  2. Let bindings be the binding object for envRec.
+  3. Let foundBinding be ? HasProperty(bindings, N).
+  4. If foundBinding is false, return false.
+
+  13.11.7 (The `with` Statement) Runtime Semantics: Evaluation
+
+  [...]
+  5. Set the withEnvironment flag of newEnv’s EnvironmentRecord to true.
+  [...]
 flags: [noStrict]
 features: [Symbol.unscopables]
 ---*/
@@ -31,7 +39,7 @@ Object.defineProperty(env, Symbol.unscopables, {
 });
 
 with (env) {
-  void x;
+  x;
 }
 
 assert.sameValue(callCount, 0);
