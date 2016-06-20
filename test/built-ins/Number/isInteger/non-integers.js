c246bc7d09c728e1c5440320f235f05ebdc9ff87
Leonardo Balter <leonardo.balter@gmail.com>
2016-06-20 14:37:27 -0400

    Add tests for Number.isInteger

--- test/built-ins/Number/isInteger/non-integers.js
+++ test/built-ins/Number/isInteger/non-integers.js
@@ -7,4 +7,7 @@ author: Ryan Lewis
 description: Number.isInteger should return false if called with a double.
 ---*/
 
-assert.sameValue(Number.isInteger(6.75), false, 'Number.isInteger(6.75)');
+assert.sameValue(Number.isInteger(6.75), false, '6.75');
+assert.sameValue(Number.isInteger(0.000001), false, '0.000001');
+assert.sameValue(Number.isInteger(-0.000001), false, '-0.000001');
+assert.sameValue(Number.isInteger(11e-1), false, '11e-1');
