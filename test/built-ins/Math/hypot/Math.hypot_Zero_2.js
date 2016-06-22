b5276e144bca4cceed299240cf7a21fe1b868c2c
Leonardo Balter <leonardo.balter@gmail.com>
2016-06-22 18:54:57 -0400

    Update tests for Math.hypot

--- test/built-ins/Math/hypot/Math.hypot_Zero_2.js
+++ test/built-ins/Math/hypot/Math.hypot_Zero_2.js
@@ -4,7 +4,10 @@
 /*---
 es6id: 20.2.2.18
 author: Ryan Lewis
-description: Math.hypot should return 0 if called with all arguments being 0.
+description: Return 0 if all arguments being are 0 or -0.
 ---*/
 
 assert.sameValue(Math.hypot(0, 0), 0, 'Math.hypot(0, 0)');
+assert.sameValue(Math.hypot(0, -0), 0, 'Math.hypot(0, -0)');
+assert.sameValue(Math.hypot(-0, 0), 0, 'Math.hypot(-0, 0)');
+assert.sameValue(Math.hypot(-0, -0), 0, 'Math.hypot(-0, -0)');
