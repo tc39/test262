080afac9060501c1dc5aa4cf78c410c04d858147
Leonardo Balter <leonardo.balter@gmail.com>
2016-06-22 19:31:42 -0400

    Update tests for Math.{sign,sin,trunc}

--- test/built-ins/Math/sin/zero.js
+++ test/built-ins/Math/sin/zero.js
@@ -2,9 +2,9 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-info: If x is -0, Math.sin(x) is -0
 es5id: 15.8.2.16_A3
-description: Checking if Math.sin(-0) equals to -0
+description: Return arg if -0 or +0
 ---*/
 
-assert.sameValue(Math.sin(-0), -0);
+assert.sameValue(Math.sin(0), 0, "+0");
+assert.sameValue(Math.sin(-0), -0, "-0");
