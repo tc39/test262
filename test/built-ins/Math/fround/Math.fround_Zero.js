dfefb0427457423e18b77324b031e45b0304b4a0
Leonardo Balter <leonardo.balter@gmail.com>
2016-06-22 18:48:28 -0400

    Expand tests for Math.fround

--- test/built-ins/Math/fround/Math.fround_Zero.js
+++ test/built-ins/Math/fround/Math.fround_Zero.js
@@ -4,7 +4,8 @@
 /*---
 es6id: 20.2.2.17
 author: Ryan Lewis
-description: Math.fround should return 0 if called with 0.
+description: Math.fround should return arg if called with 0 or -0.
 ---*/
 
 assert.sameValue(Math.fround(0), 0, 'Math.fround(0)');
+assert.sameValue(Math.fround(-0), -0, 'Math.fround(-0)');
