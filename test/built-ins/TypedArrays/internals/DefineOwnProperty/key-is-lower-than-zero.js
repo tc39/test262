d1d91af25698cda3372f5f8d9000262c267ea3dd
Henrique Ferreiro <henrique.ferreiro@gmail.com>
2016-11-11 18:42:01 +0100

    Integer indexed property descriptor must be non-configurable (#786)
    * Integer indexed property descriptor must be non-configurable
    
    * Make the configurable key explicitly 'false'

--- test/built-ins/TypedArrays/internals/DefineOwnProperty/key-is-lower-than-zero.js
+++ test/built-ins/TypedArrays/internals/DefineOwnProperty/key-is-lower-than-zero.js
@@ -24,7 +24,7 @@ testWithTypedArrayConstructors(function(TA) {
   assert.sameValue(
     Reflect.defineProperty(sample, "-1", {
       value: 42,
-      configurable: true,
+      configurable: false,
       enumerable: true,
       writable: true
     }),
