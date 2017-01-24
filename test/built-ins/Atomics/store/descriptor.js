32ba9c05cdb5494f56d052e59de3a9b419308f55
Shu-yu Guo <shu@rfrn.org>
2017-01-24 11:13:08 -0800

    Append Mozilla copyright instead of replacing original copyright in mostly copied tests.

--- test/built-ins/Atomics/store/descriptor.js
+++ test/built-ins/Atomics/store/descriptor.js
@@ -1,4 +1,5 @@
 // Copyright 2015 Microsoft Corporation. All rights reserved.
+// Copyright (C) 2017 Mozilla Corporation. All rights reserved.
 // This code is governed by the license found in the LICENSE file.
 
 /*---
--- test/built-ins/SharedArrayBuffer/prototype/slice/length.js
+++ test/built-ins/SharedArrayBuffer/prototype/slice/length.js
@@ -1,4 +1,5 @@
 // Copyright (C) 2015 André Bargull. All rights reserved.
+// Copyright (C) 2017 Mozilla Corporation. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
