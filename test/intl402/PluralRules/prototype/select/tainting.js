0530222a03ee11856803b663474f9c998d83fbf1
André Bargull <andre.bargull@gmail.com>
2017-02-14 21:15:43 +0100

    Update YAML frontmatter of some tests to be compatible with monkeyYaml

--- test/intl402/PluralRules/prototype/select/tainting.js
+++ test/intl402/PluralRules/prototype/select/tainting.js
@@ -6,7 +6,7 @@ esid: sec-intl-pluralrules-abstracts
 description: >
     Tests that the behavior of a Record is not affected by
     adversarial  changes to Object.prototype.
-info:
+info: |
   1.1.1. InitializePluralRules (pluralRules, locales, options)
   (...)
   1.1.1_6. Let t be ? GetOption(options, "type", "string", « "cardinal", "ordinal" », "cardinal").
