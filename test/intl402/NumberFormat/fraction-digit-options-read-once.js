271c07a8748963beddb742b109649f23c8ceebd1
Daniel Ehrenberg <littledan@chromium.org>
2017-03-23 19:30:03 +0100

    Updates from review

--- test/intl402/NumberFormat/fraction-digit-options-read-once.js
+++ test/intl402/NumberFormat/fraction-digit-options-read-once.js
@@ -7,12 +7,12 @@ description: >
     The maximum and minimum fraction digits properties should be read from
     the options bag exactly once from the NumberFormat constructor.
     Regression test for https://bugs.chromium.org/p/v8/issues/detail?id=6015
-include: [assert.js]
 ---*/
 
-var minCounter = 0;
-var maxCounter = 0;
-new Intl.NumberFormat("en", { get minimumFractionDigits() { minCounter++ },
-                              get maximumFractionDigits() { maxCounter++ } });
-assert.sameValue(1, minCounter);
-assert.sameValue(1, maxCounter);
+var calls = [];
+
+new Intl.NumberFormat("en", { get minimumFractionDigits() { calls.push('minimumFractionDigits') },
+                              get maximumFractionDigits() { calls.push('maximumFractionDigits') } });
+assert.sameValue(calls.length, 2);
+assert.sameValue(calls[0], 'minimumFractionDigits');
+assert.sameValue(calls[1], 'maximumFractionDigits');
