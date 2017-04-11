d10e980278ef4e9135b4deffddaa6c01a51d9b1a
Daniel Ehrenberg <littledan@chromium.org>
2017-04-11 21:45:08 +0200

    Prohibit async methods named 'function'
    Addresses #832

---
+++ test/language/statements/class/syntax/early-errors/async-method-name-function.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2017 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-async-function-definitions-static-semantics-early-errors
+description: async methods may not be named 'function' in a class declaration.
+info: >
+    AsyncMethod : `async` [no LineTerminator here] PropertyName `(` UniqueFormalParameters `)` `{` AsyncFunctionBody `}`
+
+    It is a Syntax Error if the StringValue of |PropertyName| is `"function"`.
+
+negative:
+  phase: early
+  type: SyntaxError
+---*/
+
+class A {
+  async function() {}
+}
