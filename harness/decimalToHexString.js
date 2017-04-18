9efab94047e202edfb32984951c9c71b9737595e
André Bargull <andre.bargull@gmail.com>
2017-04-18 09:46:15 -0700

    Optimize decimalToHexString and support numbers greater than 65535

--- harness/decimalToHexString.js
+++ harness/decimalToHexString.js
@@ -2,25 +2,15 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 function decimalToHexString(n) {
-  n = Number(n);
-  var h = "";
-  for (var i = 3; i >= 0; i--) {
-    if (n >= Math.pow(16, i)) {
-      var t = Math.floor(n / Math.pow(16, i));
-      n -= t * Math.pow(16, i);
-      if ( t >= 10 ) {
-        if ( t == 10 ) { h += "A"; }
-        if ( t == 11 ) { h += "B"; }
-        if ( t == 12 ) { h += "C"; }
-        if ( t == 13 ) { h += "D"; }
-        if ( t == 14 ) { h += "E"; }
-        if ( t == 15 ) { h += "F"; }
-      } else {
-        h += String(t);
-      }
-    } else {
-      h += "0";
-    }
+  var hex = "0123456789ABCDEF";
+  n >>>= 0;
+  var s = "";
+  while (n) {
+    s = hex[n & 0xf] + s;
+    n >>>= 4;
   }
-  return h;
+  while (s.length < 4) {
+    s = "0" + s;
+  }
+  return s;
 }
