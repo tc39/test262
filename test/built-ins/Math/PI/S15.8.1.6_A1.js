8907c38f8c7077e763bc717750dfd8d215c05ad9
Daniel Ehrenberg <littledan@chromium.org>
2017-04-24 20:09:41 +0200

    Test Math constants for exact value

--- test/built-ins/Math/PI/S15.8.1.6_A1.js
+++ test/built-ins/Math/PI/S15.8.1.6_A1.js
@@ -2,15 +2,12 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-info: Math.PI is approximately 3.1415926535897932
+info: Math.PI is 3.1415926535897932
 es5id: 15.8.1.6_A1
 description: Comparing Math.PI with 3.1415926535897932
-includes:
-    - math_precision.js
-    - math_isequal.js
 ---*/
 
 // CHECK#1
-if (!isEqual(Math.PI, 3.1415926535897932)) {
-  $ERROR('#1: \'Math.PI is not approximatley equal to 3.1415926535897932\'');
+if (Math.PI !== 3.1415926535897932) {
+  $ERROR('#1: \'Math.PI is not equal to 3.1415926535897932\'');
 }
--- test/built-ins/Math/SQRT1_2/S15.8.1.7_A1.js
+++ test/built-ins/Math/SQRT1_2/S15.8.1.7_A1.js
@@ -2,15 +2,12 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-info: Math.SQRT1_2 is approximately 0.7071067811865476
+info: Math.SQRT1_2 is 0.7071067811865476
 es5id: 15.8.1.7_A1
 description: Comparing Math.SQRT1_2 with 0.7071067811865476
-includes:
-    - math_precision.js
-    - math_isequal.js
 ---*/
 
 // CHECK#1
-if (!isEqual(Math.SQRT1_2, 0.7071067811865476)) {
-  $ERROR('#1: \'Math.SQRT1_2 is not approximatley equal to  0.7071067811865476\'');
+if (Math.SQRT1_2 !== 0.7071067811865476) {
+  $ERROR('#1: \'Math.SQRT1_2 is not equal to  0.7071067811865476\'');
 }
