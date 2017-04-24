e394e924e837da22bdd188abb2128693ebb90da3
Daniel Ehrenberg <littledan@chromium.org>
2017-04-24 10:19:33 +0200

    Revert "Remove over-specified tests"
    This reverts commit f7aa31b41faee25823bb38b7ca53b104cf53debd.

--- test/language/expressions/addition/S9.3_A4.1_T2.js
+++ test/language/expressions/addition/S9.3_A4.1_T2.js
@@ -30,3 +30,13 @@ if (+(1.3) !== 1.3) {
 if (+(-1.3) !== -1.3) {
   $ERROR('#4: +(-1.3) === -1.3. Actual: ' + (+(-1.3)));
 }
+
+// CHECK#5
+if (+(Number.MAX_VALUE) !== 1.7976931348623157e308) {
+  $ERROR('#5: +(Number.MAX_VALUE) === 1.7976931348623157e308. Actual: ' + (+(Number.MAX_VALUE)));
+}
+
+// CHECK#6
+if (+(Number.MIN_VALUE) !== 5e-324) {
+  $ERROR('#6: +(Number.MIN_VALUE) === 5e-324. Actual: ' + (+(Number.MIN_VALUE)));
+}
