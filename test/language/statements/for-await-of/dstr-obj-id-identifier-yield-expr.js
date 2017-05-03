2a294d9128c7afe4d3a28a6c9e5ae9f5c8acef33
Rick Waldron <waldron.rick@gmail.com>
2017-05-03 16:20:27 -0400

    Generate tests
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ test/language/statements/for-await-of/dstr-obj-id-identifier-yield-expr.js
@@ -0,0 +1,34 @@
+// This file was procedurally generated from the following sources:
+// - src/dstr-assignment/obj-id-identifier-yield-expr.case
+// - src/dstr-assignment/syntax/for-await-of.template
+/*---
+description: yield is not a valid IdentifierReference in an AssignmentProperty within generator function bodies. (for-await-of statement)
+esid: sec-for-in-and-for-of-statements-runtime-semantics-labelledevaluation
+features: [generators, destructuring-binding, async-iteration]
+flags: [generated, noStrict]
+negative:
+  phase: early
+  type: SyntaxError
+info: |
+    IterationStatement :
+      for await ( LeftHandSideExpression of AssignmentExpression ) Statement
+
+    1. Let keyResult be the result of performing
+        ? ForIn/OfHeadEvaluation(« », AssignmentExpression, async-iterate).
+    2. Return ? ForIn/OfBodyEvaluation(LeftHandSideExpression, Statement,
+        keyResult, assignment, labelSet, async).
+
+    13.7.5.13 Runtime Semantics: ForIn/OfBodyEvaluation
+
+    [...]
+    5. If destructuring is true and if lhsKind is assignment, then
+       a. Assert: lhs is a LeftHandSideExpression.
+       b. Let assignmentPattern be the parse of the source text corresponding to
+          lhs using AssignmentPattern as the goal symbol.
+    [...]
+---*/
+(function*() {
+
+for await ({ yield } of [{}]) ;
+
+});
---
+++ test/language/statements/for-await-of/dstr-obj-prop-elem-init-yield-ident-invalid.js
@@ -0,0 +1,31 @@
+// This file was procedurally generated from the following sources:
+// - src/dstr-assignment/obj-prop-elem-init-yield-ident-invalid.case
+// - src/dstr-assignment/syntax/for-await-of.template
+/*---
+description: When a `yield` token appears within the Initializer of an AssignmentElement and outside of a generator function body, it should behave as an IdentifierReference. (for-await-of statement)
+esid: sec-for-in-and-for-of-statements-runtime-semantics-labelledevaluation
+features: [destructuring-binding, async-iteration]
+flags: [generated, onlyStrict]
+negative:
+  phase: early
+  type: SyntaxError
+info: |
+    IterationStatement :
+      for await ( LeftHandSideExpression of AssignmentExpression ) Statement
+
+    1. Let keyResult be the result of performing
+        ? ForIn/OfHeadEvaluation(« », AssignmentExpression, async-iterate).
+    2. Return ? ForIn/OfBodyEvaluation(LeftHandSideExpression, Statement,
+        keyResult, assignment, labelSet, async).
+
+    13.7.5.13 Runtime Semantics: ForIn/OfBodyEvaluation
+
+    [...]
+    5. If destructuring is true and if lhsKind is assignment, then
+       a. Assert: lhs is a LeftHandSideExpression.
+       b. Let assignmentPattern be the parse of the source text corresponding to
+          lhs using AssignmentPattern as the goal symbol.
+    [...]
+---*/
+
+for await ({ x: x = yield } of [{}]) ;
