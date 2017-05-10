5f04d2b1f3d356ad4be9fb237ccb2fa72b2d5e50
Leo Balter <leonardo.balter@gmail.com>
2017-05-10 13:48:36 -0400

    Generate tests

--- test/language/expressions/class/dstr-async-gen-meth-static-dflt-ary-ptrn-rest-obj-prop-id.js
+++ test/language/expressions/class/dstr-async-gen-meth-static-dflt-ary-ptrn-rest-obj-prop-id.js
@@ -53,6 +53,7 @@ info: |
              BindingPattern with A and environment as the arguments.
        [...]
 ---*/
+let length = "outer";
 
 
 var callCount = 0;
@@ -64,9 +65,7 @@ var C = class {
     assert.sameValue(y, undefined);
     assert.sameValue(z, 3);
 
-    assert.throws(ReferenceError, function() {
-      length;
-    });
+    assert.sameValue(length, "outer", "the length prop is not set as a binding name");
     callCount = callCount + 1;
   }
 };
--- test/language/statements/for-await-of/async-gen-dstr-let-ary-ptrn-rest-obj-prop-id.js
+++ test/language/statements/for-await-of/async-gen-dstr-let-ary-ptrn-rest-obj-prop-id.js
@@ -48,6 +48,7 @@ info: |
              BindingPattern with A and environment as the arguments.
        [...]
 ---*/
+let length = "outer";
 
 var iterCount = 0;
 
@@ -59,9 +60,7 @@ async function *fn() {
     assert.sameValue(y, undefined);
     assert.sameValue(z, 3);
 
-    assert.throws(ReferenceError, function() {
-      length;
-    });
+    assert.sameValue(length, "outer", "the length prop is not set as a binding name");
 
     iterCount += 1;
   }
