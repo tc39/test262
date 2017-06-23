b74e66c3cfb8e2ac4cc58f72feb67a1261a2b911
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-06-23 03:45:21 +0300

    add Symbol.name test

---
+++ test/built-ins/Symbol/name.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2017 Aleksey Shvayka. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-symbol-constructor
+description: >
+  Symbol ( [ description ] )
+
+includes: [propertyHelper.js]
+---*/
+
+assert.sameValue(Symbol.name, "Symbol", "The value of `Symbol.name` is `'Symbol'`");
+
+verifyNotEnumerable(Symbol, "name");
+verifyNotWritable(Symbol, "name");
+verifyConfigurable(Symbol, "name");
