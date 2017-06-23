3e85c608daf3b23bfa9b8808f77d3ead0e67a2dd
Rick Waldron <waldron.rick@gmail.com>
2017-06-23 11:08:58 -0400

    harness test: sta.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ test/harness/sta-override-error.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2017 Rick Waldron.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: >
+    Assert that global $ERROR is overridable
+includes: [sta.js]
+---*/
+function BaloneyError() {}
+
+// Override $ERROR
+$ERROR = function() {
+  throw new BaloneyError();
+};
+
+assert.throws(BaloneyError, function() {
+  $ERROR();
+});
