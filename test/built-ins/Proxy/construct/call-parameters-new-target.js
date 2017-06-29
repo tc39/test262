ad8c6bd97ec4eec1fd707e0c40067cb91b079279
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-06-29 13:56:31 +0300

    fix typo in "parameters"

--- test/built-ins/Proxy/construct/call-parameters-new-target.js
+++ test/built-ins/Proxy/construct/call-parameters-new-target.js
@@ -11,26 +11,28 @@ info: >
     [[Construct]] (argumentsList, newTarget)
 
     9. Let newObj be Call(trap, handler, «target, argArray, newTarget»).
+features: [Reflect.construct]
 ---*/
 
-let Target = function() {};
-let NewTarget = function() {};
+function Target() {}
+function NewTarget() {}
 
-let handler = {
+var handler = {
     construct: function(_Target, args, _NewTarget) {
         assert.sameValue(this, handler, "trap context is the handler object");
         assert.sameValue(_Target, Target, "first parameter is the target object");
         assert.sameValue(args.length, 2, "arguments list contains all construct arguments");
 
-        let [a, b] = args;
+        var a = args[0];
+        var b = args[1];
         assert.sameValue(a, 1, "arguments list has first construct argument");
         assert.sameValue(b, 2, "arguments list has second construct argument");
-        assert.sameValue(_NewTarget, NewTarget, "newTarget is sent as the third parameter");
+        assert.sameValue(_NewTarget, NewTarget, "newTarget is passed as the third parameter");
 
         return {sum: a + b};
     },
 };
 
-let P = new Proxy(Target, handler);
-let res = Reflect.construct(P, [1, 2], NewTarget);
+var P = new Proxy(Target, handler);
+var res = Reflect.construct(P, [1, 2], NewTarget);
 assert.sameValue(res.sum, 3);
