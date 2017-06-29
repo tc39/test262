92cbc4975d20b825603f2854aeb724fa295e22af
Lyza Danger Gardner <lyza@lyza.com>
2017-06-29 11:24:53 -0400

    Remove trailing whitespace

--- test/built-ins/parseFloat/S15.1.2.3_A1_T7.js
+++ test/built-ins/parseFloat/S15.1.2.3_A1_T7.js
@@ -17,7 +17,7 @@ assert.sameValue(parseFloat(object), NaN, "{valueOf: function() {return 1}}");
 var object = {valueOf: function() {return 1}, toString: function() {return 0}};
 if (parseFloat(object) !== 0) {
   $ERROR('#2: var object = {valueOf: function() {return 1}, toString: function() {return 0}}; parseFloat(object) === 0. Actual: ' + (parseFloat(object)));
-} 
+}
 
 //CHECK#3
 var object = {valueOf: function() {return 1}, toString: function() {return {}}};
@@ -57,11 +57,11 @@ try {
   var object = {valueOf: function() {return 1}, toString: function() {throw "error"}};
   parseFloat(object);
   $ERROR('#7.1: var object = {valueOf: function() {return 1}, toString: function() {throw "error"}}; parseFloat(object) throw "error". Actual: ' + (parseFloat(object)));
-}  
+}
 catch (e) {
   if (e !== "error") {
     $ERROR('#7.2: var object = {valueOf: function() {return 1}, toString: function() {throw "error"}}; parseFloat(object) throw "error". Actual: ' + (e));
-  } 
+  }
 }
 
 //CHECK#8
@@ -69,9 +69,9 @@ try {
   var object = {valueOf: function() {return {}}, toString: function() {return {}}};
   parseFloat(object);
   $ERROR('#8.1: var object = {valueOf: function() {return {}}, toString: function() {return {}}}; parseFloat(object) throw TypeError. Actual: ' + (parseFloat(object)));
-}  
+}
 catch (e) {
   if ((e instanceof TypeError) !== true) {
     $ERROR('#8.2: var object = {valueOf: function() {return {}}, toString: function() {return {}}}; parseFloat(object) throw TypeError. Actual: ' + (e));
-  } 
+  }
 }
