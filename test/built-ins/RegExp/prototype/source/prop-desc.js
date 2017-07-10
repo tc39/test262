7daa528f43735ea9e6c221c95c324fbf9c8c9dd9
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-10 22:28:47 +0300

    join descriptor tests

--- test/built-ins/RegExp/prototype/source/prop-desc.js
+++ test/built-ins/RegExp/prototype/source/prop-desc.js
@@ -2,16 +2,19 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
+esid: sec-get-regexp.prototype.source
 es5id: 15.10.7.1-2
+es6id: 21.2.5.10
 description: >
     RegExp.prototype.source is an accessor property whose set accessor
     function is undefined
+includes: [propertyHelper.js]
 ---*/
 
-  var d = Object.getOwnPropertyDescriptor(RegExp.prototype, 'source');
-  
+var d = Object.getOwnPropertyDescriptor(RegExp.prototype, 'source');
 
 assert.sameValue(typeof d.get, 'function', 'typeof d.get');
 assert.sameValue(d.set, undefined, 'd.set');
-assert.sameValue(d.enumerable, false, 'd.enumerable');
-assert.sameValue(d.configurable, true, 'd.configurable');
+
+verifyNotEnumerable(RegExp.prototype, 'source');
+verifyConfigurable(RegExp.prototype, 'source');
