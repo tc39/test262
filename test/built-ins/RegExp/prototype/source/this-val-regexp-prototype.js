9176610b773fa93b4cfd911cfaf91bfa37ea56e8
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-07-10 21:52:31 +0300

    simplify `RegExp.prototype` test

--- test/built-ins/RegExp/prototype/source/this-val-regexp-prototype.js
+++ test/built-ins/RegExp/prototype/source/this-val-regexp-prototype.js
@@ -3,8 +3,7 @@
 /*---
 esid: sec-get-regexp.prototype.source
 description: >
-  Return value of `undefined` when the "this" value is the RegExp prototype
-  object
+  Return "(?:)" when the `this` value is the RegExp.prototype object
 info: |
   1. Let R be the this value.
   2. If Type(R) is not Object, throw a TypeError exception.
@@ -12,6 +11,4 @@ info: |
      a. If SameValue(R, %RegExpPrototype%) is true, return "(?:)".
 ---*/
 
-var get = Object.getOwnPropertyDescriptor(RegExp.prototype, 'source').get;
-
-assert.sameValue(get.call(RegExp.prototype), '(?:)');
+assert.sameValue(RegExp.prototype.source, '(?:)');
