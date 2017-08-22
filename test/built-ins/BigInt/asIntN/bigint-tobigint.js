3b776b716a2299618a0402458e743a95f1c6881a
Josh Wolfe <jwolfe@igalia.com>
2017-08-22 15:56:52 -0700

    toString in addition to valueOf

--- test/built-ins/BigInt/asIntN/bigint-tobigint.js
+++ test/built-ins/BigInt/asIntN/bigint-tobigint.js
@@ -15,7 +15,11 @@ function MyError() {}
 
 assert.sameValue(BigInt.asIntN(3, Object(10n)), 2n);
 assert.sameValue(BigInt.asIntN(3, {valueOf:()=>10n}), 2n);
+assert.sameValue(BigInt.asIntN(3, {toString:()=>"10"}), 2n);
+assert.sameValue(BigInt.asIntN(3, {valueOf:()=>10n, toString(){throw new MyError();}}), 2n);
 assert.throws(MyError, () => BigInt.asIntN(0, {valueOf(){throw new MyError();}}));
+assert.throws(MyError, () => BigInt.asIntN(0, {toString(){throw new MyError();}}));
+assert.throws(MyError, () => BigInt.asIntN(0, {valueOf(){throw new MyError();}, toString:()=>"10"}));
 assert.throws(TypeError, () => BigInt.asIntN(0, undefined));
 assert.throws(TypeError, () => BigInt.asIntN(0, null));
 assert.sameValue(BigInt.asIntN(2, true), 1n);
