cf43f93284dccbae7d54c32b12d2ff400e1af357
Leo Balter <leonardo.balter@gmail.com>
2017-09-06 15:52:05 -0400

    Address feedback review

--- test/built-ins/Proxy/apply/trap-is-null.js
+++ test/built-ins/Proxy/apply/trap-is-null.js
@@ -1,27 +1,37 @@
 // Copyright (C) 2015 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
+
 /*---
 es6id: 9.5.13
 esid: sec-proxy-object-internal-methods-and-internal-slots-call-thisargument-argumentslist
 description: >
-    If trap is null or undefined, propagate the call to the target object.
+    If the apply trap value is null, propagate the call to the target object.
 info: >
     [[Call]] (thisArgument, argumentsList)
 
-    7. If trap is undefined, then Return Call(target, thisArgument,
-    argumentsList).
+    ...
+    5. Let trap be ? GetMethod(handler, "apply").
+    6. If trap is undefined, then
+      a. Return ? Call(target, thisArgument, argumentsList).
+    ...
+
+    GetMethod ( V, P )
+
+    ...
+    3. If func is either undefined or null, return undefined.
+    ...
 ---*/
 
+var calls = 0;
+
 function target(a, b) {
-    assert.sameValue(this, ctx);
-    return a + b;
+  assert.sameValue(this, ctx);
+  calls += 1;
+  return a + b;
 }
 
 var ctx = {};
 var p = new Proxy(target, {apply: null});
 var res = p.call(ctx, 1, 2);
 assert.sameValue(res, 3, "`apply` trap is `null`");
-
-p = new Proxy(target, {apply: undefined});
-res = p.call(ctx, 3, 4);
-assert.sameValue(res, 7, "`apply` trap is `undefined`");
+assert.sameValue(calls, 1, "target is called once");
