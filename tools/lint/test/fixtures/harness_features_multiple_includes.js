1df51ee19c7e3e7385e290bbf7578592c0168fe2
Rick Waldron <waldron.rick@gmail.com>
2017-09-08 12:48:06 -0400

    Lint/test: update multiple includes test

--- tools/lint/test/fixtures/harness_features_multiple_includes.js
+++ tools/lint/test/fixtures/harness_features_multiple_includes.js
@@ -1,11 +1,11 @@
-HARNESS_FEATURES - Missing Frontmatter: `features: [TypedArray, template]
+HARNESS_FEATURES - Missing Frontmatter: `features: [Symbol.toPrimitive, BigInt]
 ^ expected errors | v input
 // Copyright (C) 2017 Rick Waldron. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-assignment-operators-static-semantics-early-errors
 description: Minimal test
-includes: [testTypedArray.js, compareArray.js]
+includes: [typeCoercion.js]
 ---*/
 
 // empty
