5f338a30a127df80934f72adf6285ec3b5ed309e
Robin Templeton <robin@igalia.com>
2017-10-02 16:13:09 -0400

    Add tests for BigInt.prototype.valueOf (#1234)
    * BigInt valueOf tests
    
    * add features from typeCoercion.js

---
+++ test/built-ins/BigInt/prototype/valueOf/this-value.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2017 Robin Templeton. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-bigint.prototype.valueof
+description: BigInt.prototype.valueOf this value coercion
+info: >
+  BigInt.prototype.valueOf ( )
+
+  Return ? thisBigIntValue(this value). 
+includes: [typeCoercion.js]
+features: [BigInt, arrow-function, Symbol, Symbol.toPrimitive]
+---*/
+
+testCoercibleToBigIntThisValue(
+  0n,
+  (x) => assert.sameValue(0n, BigInt.prototype.valueOf.call(x))
+);
