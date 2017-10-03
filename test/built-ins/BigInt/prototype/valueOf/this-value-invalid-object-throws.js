b307b80056d17094276050c69bbc9aafa7b02e4e
Leo Balter <leonardo.balter@gmail.com>
2017-10-03 13:38:49 -0400

    fixup! Improve tests for BigInt.prototype.valueOf

--- test/built-ins/BigInt/prototype/valueOf/this-value-invalid-object-throws.js
+++ test/built-ins/BigInt/prototype/valueOf/this-value-invalid-object-throws.js
@@ -16,7 +16,7 @@ info: |
   2. If Type(value) is Object and value has a [[BigIntData]] internal slot, then
     ...
   3. Throw a TypeError exception.
-features: [BigInt, Symbol]
+features: [BigInt]
 ---*/
 
 var valueOf = BigInt.prototype.valueOf;
