11cd9e4c343db99a3422d5e3fe5e43f9e18f8a52
Robin Templeton <robin@igalia.com>
2017-10-16 12:56:49 -0400

    [BigInt] typeof

---
+++ test/language/expressions/typeof/bigint.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2017 Robin Templeton. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-typeof-operator-runtime-semantics-evaluation
+description: typeof of BigInt and BigInt object
+info: >
+  The typeof Operator
+
+  Runtime Semantics: Evaluation
+features: [BigInt]
+---*/
+
+assert.sameValue(typeof 0n, "bigint");
+assert.sameValue(typeof Object(0n), "object");
