8311965251953d4745aeb68c98fb71fab2eac1d0
André Bargull <andre.bargull@gmail.com>
2017-12-01 11:16:43 -0800

    Fix copy-paste bugs in BigInt tests (#1361)

--- test/built-ins/BigInt/constructor-from-string-syntax-errors.js
+++ test/built-ins/BigInt/constructor-from-string-syntax-errors.js
@@ -15,57 +15,56 @@ features: [BigInt]
 
 assert.throws(SyntaxError, function() {
   BigInt("10n");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("10x");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("10b");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("10.5");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("0b");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("-0x1");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("-0XFFab");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("0oa");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("000 12");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("0o");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("0x");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("00o");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("00b");
-}
+});
 
 assert.throws(SyntaxError, function() {
   BigInt("00x");
-}
-
+});
--- test/built-ins/BigInt/value-of-throws.js
+++ test/built-ins/BigInt/value-of-throws.js
@@ -16,11 +16,11 @@ assert.throws(Test262Error, function() {
   BigInt({
     valueOf: function() { throw new Test262Error(); }
   });
-}
+});
 
 assert.throws(Test262Error, function() {
   BigInt({
     toString: function() { throw new Test262Error(); }
   });
-}
+});
 
