e4459f3999418456309dc77413d4212e64764e62
André Bargull <andre.bargull@gmail.com>
2017-12-01 10:39:40 -0800

    Fix copy-paste bugs in BigInt tests

--- test/built-ins/BigInt/constructor-from-binary-string.js
+++ test/built-ins/BigInt/constructor-from-binary-string.js
@@ -16,7 +16,7 @@ features: [BigInt]
 assert.sameValue(BigInt("0b1111"), 15n);
 assert.sameValue(BigInt("0b10"), 2n);
 assert.sameValue(BigInt("0b0"), 0n);
-assert.sameValue(BigInt("0b1"), 0n);
+assert.sameValue(BigInt("0b1"), 1n);
 
 let binaryString = "0b1";
 for (let i = 0; i < 128; i++)
@@ -27,7 +27,7 @@ assert.sameValue(BigInt(binaryString), 340282366920938463463374607431768211456n)
 assert.sameValue(BigInt("0B1111"), 15n);
 assert.sameValue(BigInt("0B10"), 2n);
 assert.sameValue(BigInt("0B0"), 0n);
-assert.sameValue(BigInt("0B1"), 0n);
+assert.sameValue(BigInt("0B1"), 1n);
 
 binaryString = "0B1";
 for (let i = 0; i < 128; i++)
