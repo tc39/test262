6b8bd307d29dcf39f22bd067db513952e68dc811
André Bargull <andre.bargull@gmail.com>
2017-12-08 13:22:26 -0800

    Test other-id-start/continue and vertical tilde in identifiers

---
+++ test/language/identifiers/other_id_start-escaped.js
@@ -0,0 +1,19 @@
+// Copyright (C) 2017 André Bargull. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+id: sec-names-and-keywords
+info: Test grandfathered characters of ID_Start.
+description: >
+  Grandfathered characters (Other_ID_Start)
+---*/
+
+// Other_ID_Start (Unicode 4.0)
+var \u2118;
+var \u212E;
+var \u309B;
+var \u309C;
+
+// Other_ID_Start (Unicode 9.0)
+var \u1885;
+var \u1886;
---
+++ test/language/identifiers/vertical-tilde-start.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2017 André Bargull. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+id: sec-names-and-keywords
+info: Test VERTICAL TILDE (U+2E2F) is not recognized as ID_Start character.
+description: >
+  VERTICAL TILDE is in General Category 'Lm' and [:Pattern_Syntax:].
+negative:
+  type: SyntaxError
+  phase: early
+---*/
+
+throw "Test262: This statement should not be evaluated.";
+
+var ⸯ; // U+2E2F
