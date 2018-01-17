c9e7a8b5af4325d9243e816dfd0f45f4790ff4ab
Viktor <vic99999@yandex.ru>
2018-01-18 00:05:46 +0500

    A fix for the copyright

--- test/built-ins/Date/UTC/non-integer-values.js
+++ test/built-ins/Date/UTC/non-integer-values.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// Copyright (C) 2018 Viktor Mukhachev. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-date.utc
