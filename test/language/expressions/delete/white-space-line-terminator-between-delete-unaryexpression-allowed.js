2cb0028bf1268d6edf731a531ed9a093c92e1cc4
Rick Waldron <waldron.rick@gmail.com>
2018-01-17 12:47:44 -0500

    Rename sec-delete-operator test, mark non-obvious syntax

--- test/language/expressions/delete/white-space-line-terminator-between-delete-unaryexpression-allowed.js
+++ test/language/expressions/delete/white-space-line-terminator-between-delete-unaryexpression-allowed.js
@@ -1,11 +1,14 @@
 // Copyright 2009 the Sputnik authors.  All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
+esid: sec-delete-operator
+description: >
+  White Space and Line Terminator between "delete" and UnaryExpression are allowed
 info: |
-    White Space and Line Terminator between "delete" and UnaryExpression are
-    allowed
-es5id: 11.4.1_A1
-description: Checking by using eval
+
+  UnaryExpression :
+      delete UnaryExpression
+
 ---*/
 
 var result;
@@ -25,11 +28,14 @@ assert.sameValue(result, true, '\\u0020');
 result = delete 0;
 assert.sameValue(result, true, '\\u00A0');
 
+// Line Break is intentional
 result = delete
 0;
 assert.sameValue(result, true, '\\u000A');
 
-result = delete0;
+// Line Break is intentional
+result = delete
+0;
 assert.sameValue(result, true, '\\u000D');
 
 result = delete 0;
@@ -38,6 +44,7 @@ assert.sameValue(result, true, '\\u2028');
 result = delete 0;
 assert.sameValue(result, true, '\\u2029');
 
+// Line Break is intentional
 result = delete	  
-  0;
+  0;
 assert.sameValue(result, true, '\\u0009\\u000B\\u000C\\u0020\\u00A0\\u000A\\u000D\\u2028\\u2029');
