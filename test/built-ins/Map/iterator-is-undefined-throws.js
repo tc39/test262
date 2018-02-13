cde6693ba360f23676edc63d1d5b04241198d7b4
Amal Hussein <amal@bocoup.com>
2018-02-13 11:15:02 -0500

    resolved esid linting error for test/built-ins/Map/iterator-is-undefined-throws.js

--- test/built-ins/Map/iterator-is-undefined-throws.js
+++ test/built-ins/Map/iterator-is-undefined-throws.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2018 Amal Hussein. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: #sec-map-objects
+esid: sec-map-objects
 description: >
   Throws a TypeError if the iterator of the iterable is undefined.
 info: |
