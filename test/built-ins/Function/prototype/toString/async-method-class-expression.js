3010d5660f68f3b85eaf2481b797efb14133e84e
Valerie R Young <valerie@bocoup.com>
2018-02-16 14:33:49 -0500

    Update Function/prototype/toString 'esid: pending' frontmatter

--- test/built-ins/Function/prototype/toString/async-method-class-expression.js
+++ test/built-ins/Function/prototype/toString/async-method-class-expression.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: pending
+esid: sec-function.prototype.tostring
 description: Function.prototype.toString on an async method
 features: [async-functions]
 ---*/
