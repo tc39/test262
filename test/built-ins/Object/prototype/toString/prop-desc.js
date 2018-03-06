cc256f677f7ff352f01954728f93ca139257065e
Rick Waldron <waldron.rick@gmail.com>
2018-03-06 15:21:00 -0500

    fixup! test/built-ins/Object/prototype/toString/*.js: clean up and extended fixes from gh-1473

--- test/built-ins/Object/prototype/toString/prop-desc.js
+++ test/built-ins/Object/prototype/toString/prop-desc.js
@@ -4,21 +4,15 @@
 /*---
 esid: sec-object.prototype.tostring
 description: >
-  Object.prototype.toString.length is 0.
+  Object.prototype.toString property descriptor
 info: |
-  Object.prototype.toString ( )
-
   17 ECMAScript Standard Built-in Objects:
-    Every built-in Function object, including constructors, has a length
-    property whose value is an integer. Unless otherwise specified, this
-    value is equal to the largest number of named arguments shown in the
-    subclause headings for the function description, including optional
-    parameters. However, rest parameters shown using the form “...name”
-    are not included in the default argument count.
 
-    Unless otherwise specified, the length property of a built-in Function
-    object has the attributes { [[Writable]]: false, [[Enumerable]]: false,
-    [[Configurable]]: true }.
+  ...
+  Every other data property described in clauses 18 through 26
+  and in Annex B.2 has the attributes { [[Writable]]: true,
+  [[Enumerable]]: false, [[Configurable]]: true } unless otherwise specified.
+
 includes: [propertyHelper.js]
 ---*/
 
