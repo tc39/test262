44bf3795c988446246780529a1ce47e813d3ea8b
Leo Balter <leonardo.balter@gmail.com>
2018-03-07 16:24:50 -0500

    Fix length prop tests for the TypedArrays ctors family

--- test/built-ins/TypedArrays/BigInt64Array/length.js
+++ test/built-ins/TypedArrays/BigInt64Array/length.js
@@ -5,27 +5,19 @@
 esid: sec-typedarray-constructors
 description: BigInt64Array.length property descriptor
 info: |
-  22.2.4 The TypedArray Constructors
-
-  [...]
+  The TypedArray Constructors
 
   The length property of the TypedArray constructor function is 3.
 
   17 ECMAScript Standard Built-in Objects
 
-  Every built-in function object, including constructors, has a length
-  property whose value is an integer. Unless otherwise specified, this
-  value is equal to the largest number of named arguments shown in the
-  subclause headings for the function description. Optional parameters
-  (which are indicated with brackets: [ ]) or rest parameters (which
-  are shown using the form «...name») are not included in the default
-  argument count.
+  ...
 
-  Unless otherwise specified, the length property of a built-in
-  function object has the attributes { [[Writable]]: false,
-  [[Enumerable]]: false, [[Configurable]]: true }.
+  Unless otherwise specified, the length property of a built-in function
+  object has the attributes { [[Writable]]: false, [[Enumerable]]: false,
+  [[Configurable]]: true }.
 includes: [propertyHelper.js]
-features: [BigInt]
+features: [BigInt, TypedArray]
 ---*/
 
 verifyProperty(BigInt64Array, "length", {
