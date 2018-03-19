431e6cb20ca849a2ee3853f3c1d20457fc8090bd
Leo Balter <leonardo.balter@gmail.com>
2018-03-19 19:50:28 -0400

    Add more tests for Atomics wait (#1495)

--- test/built-ins/Atomics/wait/undefined-index-defaults-to-zero.js
+++ test/built-ins/Atomics/wait/undefined-index-defaults-to-zero.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-atomics.wait
-description:
+description: >
   An undefined index arg should translate to 0
 info: |
   Atomics.wait( typedArray, index, value, timeout )
