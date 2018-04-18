a1a4e2b1b85efb912fe97965553ec5af8f98e7ab
Rick Waldron <waldron.rick@gmail.com>
2018-04-18 16:54:30 -0400

    Corrections to sec-web-compat-evaldeclarationinstantiation and sec-web-compat-functiondeclarationinstantiation tests. Fixes gh-1364 (#1509)

--- test/language/eval-code/direct/block-decl-onlystrict.js
+++ test/language/eval-code/direct/block-decl-onlystrict.js
@@ -1,26 +1,22 @@
 // Copyright (C) 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-description: AnnexB extension not honored in strict mode
-es6id: B.3.3.3
+esid: sec-web-compat-evaldeclarationinstantiation
+description: >
+    AnnexB extension not honored in strict mode, Block statement
+    in eval code containing a function declaration
 info: |
-    Function declaration in the `default` clause of a `switch` statement in
-    eval code
-
     B.3.3.3 Changes to EvalDeclarationInstantiation
 
     1. If strict is false, then
+      ...
+
+flags: [onlyStrict]
 ---*/
 
 var err;
 
-eval('\
-  "use strict";\
-  switch (1) {\
-    default:\
-      function f() {  }\
-  }\
-');
+eval('{ function f() {} }');
 
 try {
   f;
--- test/language/function-code/switch-case-decl-nostrict.js
+++ test/language/function-code/switch-case-decl-nostrict.js
@@ -1,16 +1,17 @@
 // Copyright (C) 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-description: AnnexB extension not honored in strict mode
-es6id: B.3.3.1
-flags: [onlyStrict]
+esid: sec-web-compat-functiondeclarationinstantiation
+description: >
+    AnnexB extension not honored in strict mode, Function declaration
+    in the `case` clause of a `switch` statement in function code
 info: |
-    Function declaration in the `case` clause of a `switch` statement in
-    function code
-
     B.3.3.1 Changes to FunctionDeclarationInstantiation
 
     1. If strict is false, then
+      ...
+
+flags: [noStrict]
 ---*/
 
 var err1, err2;
@@ -34,5 +35,5 @@ var err1, err2;
   }
 }());
 
-assert.sameValue(err1.constructor, ReferenceError);
-assert.sameValue(err2.constructor, ReferenceError);
+assert.sameValue(err1, undefined);
+assert.sameValue(err2, undefined);
