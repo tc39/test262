9a2e4beb50be31191b9317995761c7896138530b
Valerie R Young <valerie@bocoup.com>
2018-05-02 16:14:31 -0400

    Update filename character restriction in documention, linter and a few filenames

---
+++ tools/lint/test/fixtures/filename_forbidden_char_$.js
@@ -0,0 +1,10 @@
+FILENAME
+^ expected errors | v input
+// Copyright (c) 2018 Valerie Young.  All rights reserved.
+// This code is governed by the license found in the LICENSE file.
+/*---
+esid: sec-assignment-operators-static-semantics-early-errors
+description: Minimal test
+---*/
+
+void 0;
