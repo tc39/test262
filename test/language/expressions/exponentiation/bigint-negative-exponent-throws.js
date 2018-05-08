3358fcc2202b7c9a82173b2cbf609bbf435eb46a
Rick Waldron <waldron.rick@gmail.com>
2018-05-08 12:11:57 -0400

    BigInt: cleanup assertion messages

--- test/language/expressions/exponentiation/bigint-negative-exponent-throws.js
+++ test/language/expressions/exponentiation/bigint-negative-exponent-throws.js
@@ -1,6 +1,5 @@
 // Copyright (C) 2017 Robin Templeton. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
-
 /*---
 description: If the BigInt exponent is < 0, throw a RangeError exception
 esid: sec-exp-operator-runtime-semantics-evaluation
@@ -13,24 +12,21 @@ info: |
   BigInt::exponentiate (base, exponent)
 
   1. If exponent < 0, throw a RangeError exception.
-  2. If base is 0n and exponent is 0n, return 1n.
-  3. Return a BigInt representing the mathematical value of base raised to the power exponent.
   ...
 features: [BigInt]
 ---*/
+assert.throws(RangeError, function() {
+  1n ** -1n;
+}, '1n ** -1n throws RangeError');
 
 assert.throws(RangeError, function() {
-  1n ** -1n
-});
+  0n ** -1n;
+}, '0n ** -1n throws RangeError');
 
 assert.throws(RangeError, function() {
-  0n ** -1n
-});
+  (-1n) ** -1n;
+}, '(-1n) ** -1n throws RangeError');
 
 assert.throws(RangeError, function() {
-  (-1n) ** -1n
-});
-
-assert.throws(RangeError, function() {
-  1n ** -100000000000000000n
-});
+  1n ** -100000000000000000n;
+}, '1n ** -100000000000000000n throws RangeError');
--- test/language/expressions/less-than/bigint-and-symbol.js
+++ test/language/expressions/less-than/bigint-and-symbol.js
@@ -1,19 +1,14 @@
 // Copyright (C) 2018 Caio Lima. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
-
 /*---
 description: Relational comparison of BigInt and Symbol values
 esid: sec-abstract-relational-comparison
 features: [BigInt, Symbol]
 ---*/
-
-function MyError() {}
+assert.throws(TypeError, function() {
+  3n < Symbol('2');
+}, '3n < Symbol("2") throws TypeError');
 
 assert.throws(TypeError, function() {
-  3n < Symbol("2");
-}, "ToNumeric(Symbol) on RHS throws.");
-
-assert.throws(TypeError, function() {
-  Symbol("2") < 3n;
-}, "ToNumeric(Symbol) on LHS throws.");
-
+  Symbol('2') < 3n;
+}, 'Symbol("2") < 3n throws TypeError');
