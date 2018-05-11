de53c2bc17162934422212ae03eeccb729442341
Ms2ger <Ms2ger@gmail.com>
2018-05-11 10:00:10 +0200

    Move the empty calendar option to the 'valid' test.
    I misread the ABNF syntax.

--- test/intl402/Locale/constructor-options-calendar-valid.js
+++ test/intl402/Locale/constructor-options-calendar-valid.js
@@ -12,11 +12,31 @@ info: |
     ...
     14. Let calendar be ? GetOption(options, "calendar", "string", undefined, undefined).
     ...
+    30. Let r be ! ApplyUnicodeExtensionToTag(tag, opt, relevantExtensionKeys).
+    ...
 
+    ApplyUnicodeExtensionToTag( tag, options, relevantExtensionKeys )
+
+    ...
+    8. Let locale be the String value that is tag with all Unicode locale extension sequences removed.
+    9. Let newExtension be ! CanonicalizeUnicodeExtension(attributes, keywords).
+    10. If newExtension is not the empty String, then
+        a. Let locale be ! InsertUnicodeExtension(locale, newExtension).
+    ...
+
+    CanonicalizeUnicodeExtension( attributes, keywords )
+    ...
+    4. Repeat for each element entry of keywords in List order,
+        a. Let keyword be entry.[[Key]].
+        b. If entry.[[Value]] is not the empty String, then
+            i. Let keyword be the string-concatenation of keyword, "-", and entry.[[Value]].
+        c. Append keyword to fullKeywords.
+    ...
 features: [Intl.Locale]
 ---*/
 
 const validCalendarOptions = [
+  ["", "en-u-ca"],
   ["abc", "en-u-ca-abc"],
   ["abcd", "en-u-ca-abcd"],
   ["abcde", "en-u-ca-abcde"],
