c686cc60994e41a359c2ff9e6dcf5110ccd15082
Rick Waldron <waldron.rick@gmail.com>
2018-06-07 22:38:36 -0400

    Lint: disallow calls to verifyConfigurable and verifyProperty in same file

---
+++ tools/lint/lib/checks/harness.py
@@ -0,0 +1,14 @@
+import yaml
+
+from ..check import Check
+
+class CheckHarness(Check):
+    '''Ensure tests don't misuse harness APIs.'''
+    ID = 'HARNESS'
+
+    def run(self, name, meta, source):
+        print(source)
+        if 'verifyConfigurable(' in source and 'verifyProperty(' in source:
+            return 'verifyConfigurable & verifyProperty may not be used in the same file'
+        else:
+            return
---
+++ tools/lint/test/fixtures/harness_verifyconfigurableproperty_valid.js
@@ -0,0 +1,10 @@
+^ expected errors | v input
+// Copyright (C) 2017 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-whatever
+description: Minimal test
+includes: [verifyProperty.js]
+---*/
+
+verifyConfigurable(Object, '');
