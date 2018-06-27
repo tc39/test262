a1ff358a064857d07ce962eb2c0c6981bffc0796
Rick Waldron <waldron.rick@gmail.com>
2018-06-27 10:31:13 -0400

    Atomics: fix invalid index; print error

--- test/built-ins/Atomics/wake/out-of-range-index-throws.js
+++ test/built-ins/Atomics/wake/out-of-range-index-throws.js
@@ -30,8 +30,8 @@ assert.throws(RangeError, function() {
   Atomics.wake(i32a, Infinity, poisoned);
 }, '`Atomics.wake(i32a, Infinity, poisoned)` throws RangeError');
 assert.throws(RangeError, function() {
-  Atomics.wake(i32a, 2, poisoned);
-}, '`Atomics.wake(i32a, 2, poisoned)` throws RangeError');
+  Atomics.wake(i32a, 4, poisoned);
+}, '`Atomics.wake(i32a, 4, poisoned)` throws RangeError');
 assert.throws(RangeError, function() {
   Atomics.wake(i32a, 200, poisoned);
 }, '`Atomics.wake(i32a, 200, poisoned)` throws RangeError');
