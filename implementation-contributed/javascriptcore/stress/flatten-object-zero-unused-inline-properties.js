3e02edd20645f7e96f8ac89293382cfcb615d1e6
Rick Waldron <waldron.rick@gmail.com>
2018-07-06 14:36:06 -0400

    Curation: Removed all test files that depend on $vm (implementation-contributed/javascriptcore)
    - Eliminates:
      - controlFlowProfiler/*
      - exceptionFuzz/*

--- implementation-contributed/javascriptcore/stress/flatten-object-zero-unused-inline-properties.js
+++
@@ -1,7 +0,0 @@
-let o = { foo: 1, bar: 2, baz: 3 };
-if ($vm.inlineCapacity(o) <= 3)
-    throw new Error("There should be inline capacity");
-
-delete o.foo;
-$vm.flattenDictionaryObject(o);
-o.foo = 1;
