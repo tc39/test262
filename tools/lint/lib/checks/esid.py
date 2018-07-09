2e1de7984af2e9fe6c256fdd727709e85c699004
jugglinmike <mike@mikepennisi.com>
2018-07-09 12:29:43 -0400

    Relax linting rules for `esid` metadata (#1632)
    The document fragments used by the ECMAScript specification do not
    conform to any particular pattern beyond the grammar defined by the URL
    standard [1]. Relax the linting rule to enforce a simplified version of
    that grammar.
    
    [1] https://url.spec.whatwg.org/#fragment-state

--- tools/lint/lib/checks/esid.py
+++ tools/lint/lib/checks/esid.py
@@ -8,7 +8,12 @@ class CheckEsid(Check):
     def __init__(self):
         #self.es5idRegex = re.compile(r"^S?(B|\d+)(\.\d+)+(-(\d+|[a-z]|i+))*(_A\d(\.\d+)?(_T\d(\.\d+)?)?)?$")
         self.es6idRegex = re.compile(r"^(S?(B|\d+)(\.\d+)+(((_A\d\.\d)?_T?\d)|[ _]S\d+(\.[a-z])*)?(, |$))+")
-        self.esidRegex = re.compile(r"^(pending|(prod|sec|sup)-[-_A-Za-z0-9.%@]+)$")
+        # Simpliiied version of the WhatWG URL specification for fragment
+        # parsing
+        # https://url.spec.whatwg.org/#fragment-state
+        self.esidRegex = re.compile(
+            u"^[a-z0-9!$&'()*+,\-./:;=?@_~\u00a0-\U0010fffd]+$", re.IGNORECASE
+        )
 
     def run(self, name, meta, source):
         if not meta:
