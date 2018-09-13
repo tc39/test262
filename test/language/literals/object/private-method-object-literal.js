aa829bf721c2e8a5f77ec6c602d4b1530418f6dc
jbhoosreddy <jbhoosreddy@bloomberg.net>
2018-09-12 23:49:06 -0400

    test: Add test for private method in object literal

---
+++ test/language/literals/object/private-method-object-literal.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2018 Bloomberg LP. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+name: Private methods in object literal
+description: It's SyntaxError if private method used in Object literal
+features: [class-fields-private]
+esid: prod-FieldDefinition
+negative:
+  phase: runtime
+  type: SyntaxError
+---*/
+
+throw "Test262: This statement should not be evaluated.";
+
+var o = {
+  #m() {}
+};
