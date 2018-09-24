f6467d85a43ad29d38b256dd8ec06affb46ca913
Richard Gibson <richard.gibson@gmail.com>
2018-09-23 22:54:47 -0400

    Remove duplicate test

--- test/built-ins/JSON/stringify/15.12.3_4-1-2.js
+++ test/built-ins/JSON/stringify/15.12.3_4-1-2.js
@@ -3,11 +3,15 @@
 
 /*---
 es5id: 15.12.3_4-1-2
-description: JSON.stringify a circular object throws a TypeError
+description: JSON.stringify a indirectly circular object throws a error
 ---*/
 
-var obj = {};
-obj.prop = obj;
+var obj = {
+  p1: {
+    p2: {}
+  }
+};
+obj.p1.p2.prop = obj;
 
 assert.throws(TypeError, function() {
   JSON.stringify(obj);
