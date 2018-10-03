98b2962d8ffb78632eb7e3f54fb60620256087f2
Leo Balter <leonardo.balter@gmail.com>
2018-10-03 17:52:36 -0400

    fix fixtures folders and nit picking on tests

--- test/language/module-code/dynamic-import/eval-rqstd-once.js
+++ test/language/module-code/dynamic-import/eval-rqstd-once.js
@@ -1,5 +1,6 @@
-// Copyright (C) 2016 the V8 project authors. All rights reserved.
+// Copyright (C) 2018 Rick Waldron. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
+
 /*---
 description: Requested modules are evaluated exactly once
 esid: sec-moduleevaluation
@@ -12,20 +13,19 @@ info: |
        b. Perform ? requiredModule.ModuleEvaluation().
     [...]
 includes: [fnGlobalObject.js]
-flags: [async, module]
+flags: [async]
 features: [dynamic-import]
 ---*/
 
 var global = fnGlobalObject();
 
-
-
 Promise.all([
   import('./eval-rqstd-once_FIXTURE.js'),
   import('./eval-rqstd-once_FIXTURE.js'),
-  import('./eval-rqstd-once_FIXTURE.js'),
-]).then((...importeds) => {
+]).then(async () => {
+  // Use await to serialize imports
+  await import('./eval-rqstd-once_FIXTURE.js');
+  await import('./eval-rqstd-once_FIXTURE.js');
 
   assert.sameValue(global.test262, 262, 'global property was defined');
-
 }).then($DONE, $DONE);
