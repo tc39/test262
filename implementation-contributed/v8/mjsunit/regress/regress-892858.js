1e2181be0229fd8882e5a6b992d5b97b287c5bb8
test262-automation <test262-automation@bocoup.com>
2018-10-08 18:40:25 +0000

    [v8-test262-automation] Changes from https://github.com/v8/v8.git at sha fdfdce1d on Mon Oct 08 2018 18:39:39 GMT+0000 (Coordinated Universal Time)

--- implementation-contributed/v8/mjsunit/regress/regress-892858.js
+++ implementation-contributed/v8/mjsunit/regress/regress-892858.js
@@ -1,4 +1,4 @@
-// Copyright 2013 the V8 project authors. All rights reserved.
+// Copyright 2018 the V8 project authors. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -25,43 +25,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Flags: --allow-natives-syntax --opt --noalways-opt
-
-function mul(a, b) {
-    return a * b;
+async function foo() {
+  await Promise.resolve(42);
 }
 
-
-mul(-1, 2);
-mul(-1, 2);
-%OptimizeFunctionOnNextCall(mul);
-assertEquals(-2, mul(-1, 2));
-assertOptimized(mul);
-
-// Deopt on minus zero.
-assertEquals(-0, mul(-1, 0));
-assertUnoptimized(mul);
-
-
-function mul2(a, b) {
-    return a * b;
-}
-
-mul2(-1, 2);
-mul2(-1, 2);
-%OptimizeFunctionOnNextCall(mul2);
-
-// 2^30 is a smi boundary on arm and ia32.
-var two_30 = 1 << 30;
-// 2^31 is a smi boundary on x64.
-var two_31 = 2 * two_30;
-
-if (%IsValidSmi(two_31)) {
-  // Deopt on two_31 on x64.
-  assertEquals(two_31, mul2(-two_31, -1));
-  assertUnoptimized(mul2);
-} else {
-  // Deopt on two_30 on ia32.
-  assertEquals(two_30, mul2(-two_30, -1));
-  assertUnoptimized(mul2);
-}
+foo();
