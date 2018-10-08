874f50807777344100c6ab3bf1b00f080121d73d
Leo Balter <leonardo.balter@gmail.com>
2018-10-08 11:41:21 -0400

    Generate tests

--- test/language/module-code/dynamic-import/syntax/valid/nested-block-labeled-script-code-valid.js
+++ test/language/module-code/dynamic-import/syntax/valid/nested-block-labeled-script-code-valid.js
@@ -23,7 +23,7 @@ info: |
 ---*/
 // This is still valid in script code, and should not be valid for module code
 // https://tc39.github.io/ecma262/#sec-scripts-static-semantics-lexicallydeclarednames
-var x; function x() {}
+var smoosh; function smoosh() {}
 
 
 label: {
