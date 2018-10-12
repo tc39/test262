b7e0a48725ab6ec181e5ae0660db3d901122c1ee
Leo Balter <leonardo.balter@gmail.com>
2018-10-12 15:32:38 -0400

    Reuse fixture

--- test/language/module-code/dynamic-import/ns/delete-exported-init_FIXTURE.js
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2016 the V8 project authors. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-export var local1 = 333;
-var local2 = 444;
-export { local2 as renamed };
-export { local1 as indirect } from './delete-exported-init_FIXTURE.js';
