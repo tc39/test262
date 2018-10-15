e527498bff1d127e0034988a8f2ed4a9f0ebee43
Ms2ger <Ms2ger@gmail.com>
2018-10-15 17:29:25 +0200

    Intl.Segmenter: Some initial tests. (#1844)

---
+++ test/intl402/Segmenter/constructor/supportedLocalesOf/branding.js
@@ -0,0 +1,29 @@
+// Copyright 2018 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-Intl.Segmenter.supportedLocalesOf
+description: >
+    Verifies there's no branding check for Intl.Segmenter.supportedLocalesOf().
+info: |
+    Intl.Segmenter.supportedLocalesOf ( locales [, options ])
+features: [Intl.Segmenter]
+---*/
+
+const fn = Intl.Segmenter.supportedLocalesOf;
+const thisValues = [
+  undefined,
+  null,
+  true,
+  "",
+  Symbol(),
+  1,
+  {},
+  Intl.Segmenter,
+  Intl.Segmenter.prototype,
+];
+
+for (const thisValue of thisValues) {
+  const result = fn.call(thisValue);
+  assert.sameValue(Array.isArray(result), true);
+}
