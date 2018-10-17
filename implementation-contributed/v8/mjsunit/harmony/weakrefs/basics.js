82955482de3c8c8d54248131710c0497d4ae4168
test262-automation <test262-automation@bocoup.com>
2018-10-17 18:44:44 +0000

    [v8-test262-automation] Changes from https://github.com/v8/v8.git at sha d34cbcd7 on Wed Oct 17 2018 18:44:12 GMT+0000 (Coordinated Universal Time)

--- implementation-contributed/v8/mjsunit/harmony/weakrefs/basics.js
+++ implementation-contributed/v8/mjsunit/harmony/weakrefs/basics.js
@@ -31,8 +31,84 @@
   assertEquals(wc.toString(), "[object WeakCell]");
   assertNotSame(wc.__proto__, Object.prototype);
   assertSame(wc.__proto__.__proto__, Object.prototype);
+  assertEquals(wc.holdings, undefined);
+  let desc = Object.getOwnPropertyDescriptor(wc.__proto__, "holdings");
+  assertEquals(true, desc.configurable);
+  assertEquals(false, desc.enumerable);
+  assertEquals("function", typeof desc.get);
+  assertEquals(undefined, desc.set);
+})();
+
+(function TestMakeCellWithHoldings() {
+  let wf = new WeakFactory();
+  let obj = {a: 1};
+  let holdings = {b: 2};
+  let wc = wf.makeCell(obj, holdings);
+  assertSame(wc.holdings, holdings);
+})();
+
+(function TestMakeCellWithHoldingsSetHoldings() {
+  let wf = new WeakFactory();
+  let obj = {a: 1};
+  let holdings = {b: 2};
+  let wc = wf.makeCell(obj, holdings);
+  assertSame(wc.holdings, holdings);
+  wc.holdings = 5;
+  assertSame(wc.holdings, holdings);
+})();
+
+(function TestMakeCellWithHoldingsSetHoldingsStrict() {
+  "use strict";
+  let wf = new WeakFactory();
+  let obj = {a: 1};
+  let holdings = {b: 2};
+  let wc = wf.makeCell(obj, holdings);
+  assertSame(wc.holdings, holdings);
+  assertThrows(() => { wc.holdings = 5; }, TypeError);
+  assertSame(wc.holdings, holdings);
+})();
+
+(function TestMakeCellWithNonObject() {
+  let wf = new WeakFactory();
+  let message = "WeakFactory.makeCell: target must be an object";
+  assertThrows(() => wf.makeCell(), TypeError, message);
+  assertThrows(() => wf.makeCell(1), TypeError, message);
+  assertThrows(() => wf.makeCell(false), TypeError, message);
+  assertThrows(() => wf.makeCell("foo"), TypeError, message);
+  assertThrows(() => wf.makeCell(Symbol()), TypeError, message);
+})();
+
+(function TestMakeCellWithProxy() {
+  let handler = {};
+  let obj = {};
+  let proxy = new Proxy(obj, handler);
+  let wf = new WeakFactory();
+  let wc = wf.makeCell(proxy);
+})();
+
+(function TestMakeCellTargetAndHoldingsSameValue() {
+  let wf = new WeakFactory();
+  let obj = {a: 1};
+  // SameValue(target, holdings) not ok
+  assertThrows(() => wf.makeCell(obj, obj), TypeError,
+               "WeakFactory.makeCell: target and holdings must not be same");
+  // target == holdings ok
+  let holdings = {a: 1};
+  let wc = wf.makeCell(obj, holdings);
 })();
 
 (function TestMakeCellWithoutWeakFactory() {
   assertThrows(() => WeakFactory.prototype.makeCell.call({}, {}), TypeError);
+  // Does not throw:
+  let wf = new WeakFactory();
+  WeakFactory.prototype.makeCell.call(wf, {});
+})();
+
+(function TestHoldingsWithoutWeakCell() {
+  let wf = new WeakFactory();
+  let wc = wf.makeCell({});
+  let holdings_getter = Object.getOwnPropertyDescriptor(wc.__proto__, "holdings").get;
+  assertThrows(() => holdings_getter.call({}), TypeError);
+  // Does not throw:
+  holdings_getter.call(wc);
 })();
