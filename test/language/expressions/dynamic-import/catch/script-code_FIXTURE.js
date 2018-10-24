1538a93bc1bc8252270cf536191b19eca9073a39
Leo Balter <leonardo.balter@gmail.com>
2018-10-24 18:53:34 -0400

    typo

--- test/language/expressions/dynamic-import/catch/script-code_FIXTURE.js
+++ test/language/expressions/dynamic-import/catch/script-code_FIXTURE.js
@@ -2,6 +2,6 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 // This is still valid in script code and strict and non strict modes
-// This is valid as module code
+// This is invalid as module code
 // https://tc39.github.io/ecma262/#sec-scripts-static-semantics-lexicallydeclarednames
 var smoosh; function smoosh() {}
