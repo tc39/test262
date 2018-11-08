09cdabed733e532ef8fb2bb9b3be7209120d0ae8
Leo Balter <leonardo.balter@gmail.com>
2018-11-08 16:41:02 -0500

    Generate tests

---
+++ test/language/statements/class/syntax/early-errors/delete/field-delete-err-delete-member-expression-privatename.js
@@ -0,0 +1,33 @@
+// This file was procedurally generated from the following sources:
+// - src/class-elements/err-delete-member-expression-privatename.case
+// - src/class-elements/delete-error/cls-decl-field-delete.template
+/*---
+description: It's a SyntaxError if delete operator is applied to MemberExpression.PrivateName private field (in field)
+esid: sec-class-definitions-static-semantics-early-errors
+features: [class, class-fields-private, class-fields-public]
+flags: [generated]
+negative:
+  phase: parse
+  type: SyntaxError
+info: |
+    Static Semantics: Early Errors
+
+      UnaryExpression : delete UnaryExpression
+
+      It is a Syntax Error if the UnaryExpression is contained in strict mode
+      code and the derived UnaryExpression is
+      PrimaryExpression : IdentifierReference ,
+      MemberExpression : MemberExpression.PrivateName , or
+      CallExpression : CallExpression.PrivateName .
+
+---*/
+
+
+$DONOTEVALUATE();
+
+class C {
+  #x;
+  
+  x = delete this.#x;
+  
+}
