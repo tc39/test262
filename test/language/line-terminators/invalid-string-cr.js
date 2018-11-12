f9ffa71c56ac1fcd98af3091db01e437e351999b
Mike Pennisi <mike@mikepennisi.com>
2018-11-12 00:01:49 -0500

    Refactor LT tests for parsers: string
    The tests for the parsing of line terminators were expressed using eval.
    This made the tests more complex than necessary and also prevented the
    tests from providing value to ECMAScript parsers.
    
    Remove the use of eval and instead express the expectations with literal
    source text.

--- test/language/line-terminators/invalid-string-cr.js
+++ test/language/line-terminators/invalid-string-cr.js
@@ -4,9 +4,13 @@
 /*---
 info: CARRIAGE RETURN (U+000D) within strings is not allowed
 es5id: 7.3_A2.2_T1
+esid: sec-line-terminators
 description: Insert CARRIAGE RETURN (\u000D) into string
+negative:
+  phase: parse
+  type: SyntaxError
 ---*/
 
-assert.throws(SyntaxError, function() {
-  eval("'\u000Dstr\u000Ding\u000D'");
-});
+$DONOTEVALUATE();
+
+''
