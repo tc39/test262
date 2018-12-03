d628f9cd1ec8f84af9d410628b1b6b410dac31eb
Bradley Farias <bfarias@godaddy.com>
2018-12-03 15:55:42 -0600

    Add tests for Hashbang comments

---
+++ test/language/comments/hashbang-preceding-hashbang.js
@@ -0,0 +1,14 @@
+#!
+#!
+/*---
+esid: pending
+description: >
+    Hashbang comments should only be allowed at the start of source texts and should not be preceded by Hashbang comments.
+info: |
+    HashbangComment::
+      #! SingleLineCommentChars[opt]
+flags: [raw]
+negative:
+  phase: parse
+  type: SyntaxError
+---*/
---
+++ test/language/comments/hashbang-use-strict.js
@@ -0,0 +1,12 @@
+#!"use strict"
+/*---
+esid: pending
+description: >
+    Hashbang comments should not be interpretted and should not generate DirectivePrologues.
+info: |
+    HashbangComment::
+      #! SingleLineCommentChars[opt]
+flags: [raw]
+---*/
+
+with ({}) {}
