c76f9d9b8a4afe6d280b3057c2a38cb99b9cda9c
Leo Balter <leonardo.balter@gmail.com>
2018-12-18 18:33:56 -0200

    refactor coverage for flatMap

--- test/built-ins/Array/prototype/flatMap/this-value-null-undefined-throws.js
+++ test/built-ins/Array/prototype/flatMap/this-value-null-undefined-throws.js
@@ -3,20 +3,23 @@
 /*---
 esid: sec-array.prototype.flatMap
 description: >
-    null or undefined should throw TypeError Exception
+  Throw a TypeError if this value is null or undefined
+info: |
+  Array.prototype.flatMap ( mapperFunction [ , thisArg ] )
+
+  1. Let O be ? ToObject(this value).
+  ...
 features: [Array.prototype.flatMap]
 ---*/
 
 assert.sameValue(typeof Array.prototype.flatMap, 'function');
 
+var mapperFn = function() {};
+
 assert.throws(TypeError, function() {
-  [].flatMap.call(null);
+  [].flatMap.call(null, mapperFn);
 }, 'null value');
 
 assert.throws(TypeError, function() {
-  [].flatMap.call();
-}, 'missing');
-
-assert.throws(TypeError, function() {
-  [].flatMap.call(void 0);
+  [].flatMap.call(undefined, mapperFn);
 }, 'undefined');
