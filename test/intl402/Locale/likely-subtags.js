0043975043a8249cfb94574872f68d0cc4b7e050
Frank Tang <ftang@chromium.org>
2019-01-08 18:48:18 -0800

    Remove no-longer supported privae tag
    https://github.com/tc39/proposal-intl-locale/pull/66

--- test/intl402/Locale/likely-subtags.js
+++ test/intl402/Locale/likely-subtags.js
@@ -37,9 +37,6 @@ const testDataMaximal = {
     "und-419": "es-Latn-419",
     "und-150": "ru-Cyrl-RU",
     "und-AT": "de-Latn-AT",
-
-    // privateuse only.
-    "x-private": "x-private",
 };
 
 const testDataMinimal = {
@@ -65,9 +62,6 @@ const testDataMinimal = {
     "es-Latn-419": "es-419",
     "ru-Cyrl-RU": "ru",
     "de-Latn-AT": "de-AT",
-
-    // privateuse only.
-    "x-private": "x-private",
 };
 
 // Add variants, extensions, and privateuse subtags and ensure they don't
