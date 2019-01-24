d41e3974094a59aba1aec577f695c31335a98d5c
Frank Tang <ftang@chromium.org>
2019-01-23 22:26:47 -0800

    Change comments

--- test/intl402/Locale/constructor-options-language-valid-undefined.js
+++ test/intl402/Locale/constructor-options-language-valid-undefined.js
@@ -13,15 +13,9 @@ info: |
     12. Set tag to ? ApplyOptionsToTag(tag, options).
 
     ApplyOptionsToTag( tag, options )
+
+    2. If IsStructurallyValidLanguageTag(tag) is false, throw a RangeError exception.
     ...
-    9. Set tag to CanonicalizeLanguageTag(tag).
-
-    CanonicalizeLanguageTag( tag )
-
-    The CanonicalizeLanguageTag abstract operation returns the canonical and
-    case-regularized form of the locale argument (which must be a String value
-    that is a structurally valid Unicode BCP 47 Locale Identifier as verified by
-    the IsStructurallyValidLanguageTag abstract operation).
 
     IsStructurallyValidLanguageTag ( locale )
 
