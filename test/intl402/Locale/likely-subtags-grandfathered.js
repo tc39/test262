9229d09547ccb940a5156b74034aa5ef2a95ac4e
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2019-02-12 18:44:31 -0800

    Remove unreasonable expectation

--- test/intl402/Locale/likely-subtags-grandfathered.js
+++ test/intl402/Locale/likely-subtags-grandfathered.js
@@ -127,7 +127,6 @@ for (const {tag} of regularGrandfathered) {
 
     for (const extra of extras) {
         const loc = new Intl.Locale(tag + "-" + extra);
-        assert.sameValue(loc.toString(), tag + "-" + extra);
 
         assert.sameValue(loc.maximize().toString(), tagMax + "-" + extra);
         assert.sameValue(loc.maximize().maximize().toString(), tagMax + "-" + extra);
