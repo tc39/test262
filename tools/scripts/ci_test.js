69380ef7eded8c70b1a51235532f444d9ccdd01f
Leo Balter <leonardo.balter@gmail.com>
2019-02-28 14:54:24 -0500

    leverage pending stauts

--- tools/scripts/ci_test.js
+++ tools/scripts/ci_test.js
@@ -20,6 +20,8 @@ const {
     TRAVIS_BUILD_WEB_URL,
 } = process.env;
 
+const API_URL = `https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${TRAVIS_PULL_REQUEST_SHA}`;
+
 const paths = execSync(`git diff --diff-filter ACMR --name-only -z ${process.env.TRAVIS_BRANCH} -- test/`)
     .toString().trim();
 
@@ -30,88 +32,82 @@ if (!paths) {
 
 console.log(`New or modified test files: \n${paths.replace(/\u0000/g, '\n')}`);
 
-fetch(`https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${TRAVIS_PULL_REQUEST_SHA}`,
-    {
+const promises = [];
+for (const { hostName, hostType, hostPath } of engines) {
+    const execution = spawn('test262-harness', [
+        `--hostType=${hostType}`,
+        `--hostPath=${hostPath}`,
+        '--',
+        ...paths.split('\u0000'),
+    ], {
+        shell: true,
+        stdio: ['pipe', 'pipe', process.stderr],
+    });
+
+    const output = [];
+
+    execution.stdout.on('data', chunk => {
+        output.push(chunk);
+    })
+
+    promises.push(new Promise((res, rej) => {
+        execution.on('close', code => {
+            if (code === 0) {
+                res(hostName);
+            } else {
+                rej([code, hostName]);
+            }
+        });
+    }).then(name => {
+        console.log(`Completed execution for ${name}`);
+        const results = Buffer.concat(output).toString('utf8')
+            .replace(/^PASS test\/\w.*$\n/gm, '');
+        return [name, results];
+    }));
+}
+
+Promise.all(promises).then((engines) => {
+    const body = engines.map(([engine, results]) =>
+        `Results for ${engine}:\n${results}`).join('\n');
+    console.log(body);
+
+    const faileds = body.match(/^\d* failed$/gm);
+
+    let state = 'success';
+    let description = 'All new or modified tests are passing';
+
+    const total = faileds
+        .map(failed => Number(/\d+/.exec(failed)[0]))
+        .reduce((acc, n) => acc + n);
+
+    if (faileds.some(n => n !== '0 failed')) {
+        // Use pending for yellow, don't break the build
+        state = 'pending';
+        description = `Found ${total} failures executing the tests`;
+    }
+
+    fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify({
-            state: 'pending',
+            state,
             target_url: TRAVIS_BUILD_WEB_URL,
-            description: 'Running new and/or modified tests',
-            context: 'continuous-integration/travis-ci/tests-execution'
+            description,
+            context: 'tests-execution'
         }),
         headers: { 'Authorization': `token ${GITHUB_OAUTH2TOKEN}` }
-    }
-).then(res => {
-    console.log('Sent pending status', res);
-    const promises = [];
-    for (const { hostName, hostType, hostPath } of engines) {
-        const execution = spawn('test262-harness', [
-            `--hostType=${hostType}`,
-            `--hostPath=${hostPath}`,
-            '--',
-            ...paths.split('\u0000'),
-        ], {
-            shell: true,
-            stdio: ['pipe', 'pipe', process.stderr],
-        });
-
-        const output = [];
-
-        execution.stdout.on('data', chunk => {
-            output.push(chunk);
-        })
-
-        promises.push(new Promise((res, rej) => {
-            execution.on('close', code => {
-                if (code === 0) {
-                    res(hostName);
-                } else {
-                    rej([code, hostName]);
-                }
-            });
-        }).then(name => {
-            console.log(`Completed execution for ${name}`);
-            const results = Buffer.concat(output).toString('utf8')
-                .replace(/^PASS test\/\w.*$\n/gm, '');
-            return [name, results];
-        }));
-    }
-
-    return promises;
-}).then(promises => {
-    Promise.all(promises).then((engines) => {
-        const body = engines.map(([engine, results]) =>
-            `Results for ${engine}:\n${results}`).join('\n');
-        console.log(body);
-
-        const faileds = body.match(/^\d* failed$/gm);
-
-        let state = 'success';
-        let description = 'All new or modified tests are passing';
-
-        const total = faileds
-            .map(failed => Number(/\d+/.exec(failed)[0]))
-            .reduce((acc, n) => acc + n);
-
-        if (faileds.some(n => n !== '0 failed')) {
-            state = 'error';
-            description = `Found ${total} failures executing the tests`;
-        }
-
-        fetch(`https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${TRAVIS_PULL_REQUEST_SHA}`,
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    state,
-                    target_url: TRAVIS_BUILD_WEB_URL,
-                    description,
-                    context: 'continuous-integration/travis-ci/tests-execution'
-                }),
-                headers: { 'Authorization': `token ${GITHUB_OAUTH2TOKEN}` }
-            }
-        ).then(res => console.log(res));
-    });
+    }).then(res => console.log(res));
 }).catch((code, hostName) => {
     console.error('Failed to execute the tests!', code, hostName);
-    process.exit(code);
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify({
+            state: 'error',
+            target_url: TRAVIS_BUILD_WEB_URL,
+            description: 'Failed to execute the tests!',
+            context: 'tests-execution'
+        }),
+        headers: { 'Authorization': `token ${GITHUB_OAUTH2TOKEN}` }
+    }).then(res => {
+        console.log(res);
+    }).finally(() => process.exit(code));
 });
