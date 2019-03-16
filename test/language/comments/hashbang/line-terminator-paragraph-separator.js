76f32a6eb52f29c90eb56a47e13e89841ba5db0e
Mike Pennisi <mike@mikepennisi.com>
2019-03-16 18:29:05 -0400

    fixup! Add tests for termination of "hashbang" comment

--- test/language/comments/hashbang/line-terminator-paragraph-separator.js
+++ test/language/comments/hashbang/line-terminator-paragraph-separator.js
@@ -1,4 +1,5 @@
-#! this comment ends with a Paragraph Separator (U+2029) 0++;
+#! this comment ends with a Paragraph Separator (U+2029) {
+}
 
 // Copyright (C) 2019 Mike Pennisi. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
@@ -22,17 +23,6 @@ info: |
       <CR>
       <LS>
       <PS>
-
-    This test relies on the static semantics of UpdateExpression in order to
-    verify the precise location that the source text is interpreted as
-    executable code. Although throwing a runtime exception may be a more
-    explicit mechanism to signal execution, using an early error allows this
-    test to verify correctness of ECMAScript parsers.
 flags: [raw]
 features: [hashbang]
-negative:
-  phase: parse
-  type: ReferenceError
 ---*/
-
-throw "Test262: This statement should not be evaluated.";
