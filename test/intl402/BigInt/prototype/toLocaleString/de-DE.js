03652cf993da1312bec9762e7cb7db92830417ff
Ms2ger <Ms2ger@igalia.com>
2019-04-24 15:44:09 +0200

    Add some more BigInt.prototype.toLocaleString tests.

--- test/intl402/BigInt/prototype/toLocaleString/de-DE.js
+++ test/intl402/BigInt/prototype/toLocaleString/de-DE.js
@@ -4,20 +4,20 @@
 /*---
 esid: sec-bigint.prototype.tolocalestring
 description: Checks basic behavior for BigInt.prototype.toLocaleString.
-locale: [en-US]
+locale: [de-DE]
 features: [BigInt]
 ---*/
 
 const tests = [
   [0n, undefined, "0"],
   [-0n, undefined, "0"],
-  [88776655n, { "maximumSignificantDigits": 4 }, "88,780,000"],
-  [88776655n, { "maximumSignificantDigits": 4, "style": "percent" }, "8,878,000,000%"],
-  [88776655n, { "minimumFractionDigits": 3 }, "88,776,655.000"],
-  [90071992547409910n, undefined, "90,071,992,547,409,910"],
+  [88776655n, { "maximumSignificantDigits": 4 }, "88.780.000"],
+  [88776655n, { "maximumSignificantDigits": 4, "style": "percent" }, "8.878.000.000\u00a0%"],
+  [88776655n, { "minimumFractionDigits": 3 }, "88.776.655,000"],
+  [90071992547409910n, undefined, "90.071.992.547.409.910"],
 ];
 
 for (const [bigint, options, expected] of tests) {
-  const result = bigint.toLocaleString("en-US", options);
+  const result = bigint.toLocaleString("de-DE", options);
   assert.sameValue(result, expected);
 }
