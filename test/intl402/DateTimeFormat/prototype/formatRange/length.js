4b25f6fae88203fa73c5a9aa6fdb0e47a8049770
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2019-05-01 12:20:29 -0700

    Add initial tests for Intl.DateTimeFormat formatRange and formatRangeToParts (#2134)

---
+++ test/intl402/DateTimeFormat/prototype/formatRange/length.js
@@ -0,0 +1,17 @@
+// Copyright 2019 Google, Inc.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Intl.DateTimeFormat.prototype.formatRange.length.
+includes: [propertyHelper.js]
+features: [Intl.DateTimeFormat-formatRange]
+---*/
+
+assert.sameValue(Intl.DateTimeFormat.prototype.formatRange.length, 2);
+
+verifyProperty(Intl.DateTimeFormat.prototype.formatRange, 'length', {
+  value: 2,
+  enumerable: false,
+  writable: false,
+  configurable: true,
+});
---
+++ test/intl402/DateTimeFormat/prototype/formatRangeToParts/length.js
@@ -0,0 +1,17 @@
+// Copyright 2019 Google, Inc.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: Intl.DateTimeFormat.prototype.formatRangeToParts.length.
+includes: [propertyHelper.js]
+features: [Intl.DateTimeFormat-formatRange]
+---*/
+
+assert.sameValue(Intl.DateTimeFormat.prototype.formatRangeToParts.length, 2);
+
+verifyProperty(Intl.DateTimeFormat.prototype.formatRangeToParts, 'length', {
+  value: 2,
+  enumerable: false,
+  writable: false,
+  configurable: true,
+});
