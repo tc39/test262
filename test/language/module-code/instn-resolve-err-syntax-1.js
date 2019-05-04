fe5d572583214197dc1e336f3d160ecd73f4895c
Ross Kirsling <rkirsling@gmail.com>
2019-05-03 21:24:53 -0700

    Update module resolution tests too.

--- test/language/module-code/instn-resolve-err-syntax-1.js
+++ test/language/module-code/instn-resolve-err-syntax-1.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2016 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-description: Requested modules that produce an early ReferenceError
+description: Requested modules that produce an early SyntaxError
 esid: sec-moduledeclarationinstantiation
 info: |
     [...]
@@ -12,10 +12,10 @@ info: |
     [...]
 negative:
   phase: resolution
-  type: ReferenceError
+  type: SyntaxError
 flags: [module]
 ---*/
 
 $DONOTEVALUATE();
 
-import './instn-resolve-err-reference_FIXTURE.js';
+import './instn-resolve-err-syntax-1_FIXTURE.js';
