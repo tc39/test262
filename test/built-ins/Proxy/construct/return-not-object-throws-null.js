1265a600176e2ebfbb377329e4728528f2ddb8b3
Aleksey Shvayka <shvaikalesh@gmail.com>
2019-05-29 22:49:41 +0300

    Improve Proxy/construct coverage (#2159)

--- test/built-ins/Proxy/construct/return-not-object-throws-null.js
+++ test/built-ins/Proxy/construct/return-not-object-throws-null.js
@@ -1,25 +1,23 @@
-// Copyright (C) 2015 the V8 project authors. All rights reserved.
+// Copyright (C) 2019 Aleksey Shvayka. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-proxy-object-internal-methods-and-internal-slots-construct-argumentslist-newtarget
-es6id: 9.5.14
 description: >
-    Throws a TypeError if trap result is not an Object: String
+  Throws a TypeError if trap result is not an Object: null
 info: |
-    [[Construct]] ( argumentsList, newTarget)
+  [[Construct]] (argumentsList, newTarget)
 
-    11. If Type(newObj) is not Object, throw a TypeError exception.
+  [...]
+  11. If Type(newObj) is not Object, throw a TypeError exception.
 features: [Proxy]
 ---*/
-
-function Target() {}
 
 var P = new Proxy(function() {
   throw new Test262Error('target should not be called');
 }, {
   construct: function() {
-    return "";
-  }
+    return null;
+  },
 });
 
 assert.throws(TypeError, function() {
