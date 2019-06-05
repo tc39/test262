78e8a0d102a5170c9909a04c65d6c54043e9caa4
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2019-06-05 15:26:48 -0700

    add constructor-options-order-quarter.js

---
+++ test/intl402/DateTimeFormat/constructor-options-order-quarter.js
@@ -0,0 +1,40 @@
+// Copyright 2019 Googe Inc. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-initializedatetimeformat
+description: Checks the order of getting options of 'quarter' for the DateTimeFormat constructor.
+includes: [compareArray.js]
+features: [Intl.DateTimeFormat-quarter]
+
+---*/
+
+// Just need to ensure quarter are get between year and month.
+const expected = [
+  // ToDateTimeOptions step 4.
+  "year", "quarter", "month",
+  // InitializeDateTimeFormat step 22.
+  "year",
+  "quarter",
+  "month"
+];
+
+const actual = [];
+
+const options = {
+  get month() {
+    actual.push("month");
+    return "numeric";
+  },
+  get quarter() {
+    actual.push("quarter");
+    return "long";
+  },
+  get year() {
+    actual.push("year");
+    return "numeric";
+  },
+};
+
+new Intl.DateTimeFormat("en", options);
+assert.compareArray(actual, expected);
