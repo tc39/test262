843064d211a362ef7ee31cd0a2ff0c3047d794ec
Leo Balter <leonardo.balter@gmail.com>
2019-07-15 13:24:31 -0400

    Add tests for linter

--- tools/lint/test/fixtures/license_invalid_year_range.js
+++ tools/lint/test/fixtures/license_invalid_year_range.js
@@ -1,5 +1,7 @@
 LICENSE
 ^ expected errors | v input
+// Copyright (C) 2012-2019 Mike Pennisi. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-assignment-operators-static-semantics-early-errors
 description: Applied to a "covered" YieldExpression
