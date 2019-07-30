f4be160623a2f32f126bcb8031a3e1ba9d7205bf
André Bargull <andre.bargull@gmail.com>
2019-07-30 04:35:57 -0700

    Fix expected test values
    These two slipped through the cracks in #2097:
    
    test/intl402/Intl/getCanonicalLocales/non-iana-canon.js
    - Variant subtag canonicalisation is currrently not allowed.
    
    test/intl402/Locale/getters.js
    - Only the first "loc.caseFirst" test in this file was updated in #2097.

--- test/intl402/Locale/getters.js
+++ test/intl402/Locale/getters.js
@@ -114,7 +114,7 @@ assert.sameValue(loc.calendar, "gregory");
 assert.sameValue(loc.collation, "standard");
 assert.sameValue(loc.hourCycle, "h11");
 if ("caseFirst" in loc) {
-    assert.sameValue(loc.caseFirst, "true");
+    assert.sameValue(loc.caseFirst, "");
 }
 if ("numeric" in loc) {
     assert.sameValue(loc.numeric, false);
