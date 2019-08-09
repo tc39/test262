8946956f19ca17836b1d124ec0fa90b57469c8e6
Ms2ger <Ms2ger@igalia.com>
2019-08-09 15:44:23 +0200

    Lint: Make CheckHarnessFeatures output stable.
    The iteration order for a set is not guaranteed to be stable, but this test expected it to be.

--- tools/lint/test/fixtures/harness_features_multiple_includes.js
+++ tools/lint/test/fixtures/harness_features_multiple_includes.js
@@ -1,4 +1,4 @@
-HARNESS_FEATURES - Missing: `features: [Symbol.toPrimitive, BigInt]`
+HARNESS_FEATURES - Missing: `features: [BigInt, Symbol.toPrimitive]`
 ^ expected errors | v input
 // Copyright (C) 2017 Rick Waldron. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
