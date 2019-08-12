c4e5d69044f1941c2af5b06fe8fea12ddd6d3e88
Ms2ger <Ms2ger@igalia.com>
2019-08-12 15:49:14 +0200

    Tools: Update PyYAML version. (#2279)
    Fixes #2278.

--- tools/generation/lib/template.py
+++ tools/generation/lib/template.py
@@ -154,20 +154,20 @@ class Template:
         features += self.attribs['meta'].get('features', [])
         features = list(OrderedDict.fromkeys(features))
         if len(features):
-            lines += ['features: ' + re.sub('\n\s*', ' ', yaml.dump(features).strip())]
+            lines += ['features: ' + re.sub('\n\s*', ' ', yaml.dump(features, default_flow_style=True).strip())]
 
         flags = ['generated']
         flags += case_values['meta'].get('flags', [])
         flags += self.attribs['meta'].get('flags', [])
         flags = list(OrderedDict.fromkeys(flags))
-        lines += ['flags: ' + re.sub('\n\s*', ' ', yaml.dump(flags).strip())]
+        lines += ['flags: ' + re.sub('\n\s*', ' ', yaml.dump(flags, default_flow_style=True).strip())]
 
         includes = []
         includes += case_values['meta'].get('includes', [])
         includes += self.attribs['meta'].get('includes', [])
         includes = list(OrderedDict.fromkeys(includes))
         if len(includes):
-            lines += ['includes: ' + re.sub('\n\s*', ' ', yaml.dump(includes).strip())]
+            lines += ['includes: ' + re.sub('\n\s*', ' ', yaml.dump(includes, default_flow_style=True).strip())]
 
         if case_values['meta'].get('negative'):
             if self.attribs['meta'].get('negative'):
