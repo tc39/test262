c674362d1a865de42e226fe580b74164239994ff
Alexey Shvayka <shvaikalesh@gmail.com>
2019-08-24 13:24:33 +0300

    Use SameValue

--- harness/compareArray.js
+++ harness/compareArray.js
@@ -5,13 +5,20 @@ description: |
     Compare the contents of two arrays
 ---*/
 
+function isSameValue(a, b) {
+  if (a === 0 && b === 0) return 1 / a === 1 / b;
+  if (a !== a && b !== b) return true;
+
+  return a === b;
+}
+
 function compareArray(a, b) {
   if (b.length !== a.length) {
     return false;
   }
 
   for (var i = 0; i < a.length; i++) {
-    if (b[i] !== a[i]) {
+    if (!isSameValue(b[i], a[i])) {
       return false;
     }
   }
