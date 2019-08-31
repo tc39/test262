2b4591bb2caac8eb1f6e48fd3e6dd0457994bfc7
Gus Caplan <me@gus.host>
2019-08-31 17:34:29 -0500

    fix TypedArray.prototype.sort test

--- test/built-ins/TypedArray/prototype/sort/sorted-values.js
+++ test/built-ins/TypedArray/prototype/sort/sorted-values.js
@@ -25,10 +25,17 @@ testWithTypedArrayConstructors(function(TA) {
 
   sample = new TA([3, 4, 3, 1, 0, 1, 2]).sort();
   assert(compareArray(sample, [0, 1, 1, 2, 3, 3, 4]), "repeating numbers");
+});
 
+testWithTypedArrayConstructors(function(TA) {
+  sample = new TA([1, 0, -0, 2]).sort();
+  assert(compareArray(sample, [-0, 0, 1, 2]), "0s");
+}, floatArrayConstructors);
+
+testWithTypedArrayConstructors(function(TA) {
   sample = new TA([1, 0, -0, 2]).sort();
   assert(compareArray(sample, [0, 0, 1, 2]), "0s");
-});
+}, intArrayConstructors);
 
 testWithTypedArrayConstructors(function(TA) {
   var sample = new TA([-4, 3, 4, -3, 2, -2, 1, 0]).sort();
