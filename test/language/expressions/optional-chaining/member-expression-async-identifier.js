dd2f6e73fe0bd3c3dd7f9567a5f4bead18771f19
Benjamin E. Coe <bencoe@google.com>
2019-09-16 22:32:22 -0700

    Update test/language/expressions/optional-chaining/member-expression-async-identifier.js
    Co-Authored-By: Leo Balter <leonardo.balter@gmail.com>

--- test/language/expressions/optional-chaining/member-expression-async-identifier.js
+++ test/language/expressions/optional-chaining/member-expression-async-identifier.js
@@ -21,6 +21,18 @@ const a = undefined;
 const c = {d: returnSoon(11)};
 async function checkAssertions() {
   assert.sameValue(undefined, await a?.b);
-  assert.sameValue(11, await c?.d);
+  assert.sameValue(await a?.b, undefined);
+  assert.sameValue(await c?.d, 11);
+  
+  Promise.prototype.x = 42;
+  var res = await Promise.resolve(undefined)?.x;
+  assert.sameValue(res, 42, 'await unwraps the evaluation of the whole optional chaining expression #1');
+
+  Promise.prototype.y = 43;
+  var res = await Promise.reject(undefined)?.y;
+  assert.sameValue(res, 43, 'await unwraps the evaluation of the whole optional chaining expression #2');
+  
+  c.e = Promise.resolve(39);
+  assert.sameValue(await c?.e, 39, 'await unwraps the promise given after the evaluation of the OCE');
 }
 checkAssertions().then($DONE, $DONE);
