9fa82c2dc28d1b58fa5196d6a4faca8987737470
Leo Balter <leonardo.balter@gmail.com>
2019-10-10 14:10:02 -0400

    Inital commit for Intl.DisplayNames tests

---
+++ test/intl402/DisplayNames/ctor-custom-get-prototype-poison-throws.js
@@ -0,0 +1,42 @@
+// Copyright (C) 2019 Leo Balter. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-Intl.DisplayNames
+description: >
+  Return abrupt from Get Prototype from a custom NewTarget
+info: |
+  Intl.DisplayNames ([ locales [ , options ]])
+
+  1. If NewTarget is undefined, throw a TypeError exception.
+  2. Let displayNames be ? OrdinaryCreateFromConstructor(NewTarget, "%DisplayNamesPrototype%",
+    « [[InitializedDisplayNames]], [[Locale]], [[Style]], [[Type]], [[Fallback]], [[Fields]] »).
+  ...
+
+  OrdinaryCreateFromConstructor ( constructor, intrinsicDefaultProto [ , internalSlotsList ] )
+
+  ...
+  2. Let proto be ? GetPrototypeFromConstructor(constructor, intrinsicDefaultProto).
+  ...
+
+  GetPrototypeFromConstructor ( constructor, intrinsicDefaultProto )
+
+  3. Let proto be ? Get(constructor, "prototype").
+  4. If Type(proto) is not Object, then
+    a. Let realm be ? GetFunctionRealm(constructor).
+    b. Set proto to realm's intrinsic object named intrinsicDefaultProto.
+  5. Return proto.
+features: [Intl.DisplayNames, Reflect]
+---*/
+
+var custom = new Function();
+
+Object.defineProperty(custom, 'prototype', {
+  get() {
+    throw new Test262Error();
+  }
+})
+
+assert.throws(Test262Error, () => {
+  Reflect.construct(Intl.DisplayNames, [], custom);
+});
---
+++ test/intl402/DisplayNames/proto.js
@@ -0,0 +1,14 @@
+// Copyright (C) 2019 Leo Balter. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-Intl.DisplayNames
+description: >
+  The internal prototype of Intl.DisplayNames
+includes: [propertyHelper.js]
+features: [Intl.DisplayNames]
+---*/
+
+var proto = Object.getPrototypeOf(Intl.DisplayNames);
+
+assert.sameValue(proto, Function.prototype);
