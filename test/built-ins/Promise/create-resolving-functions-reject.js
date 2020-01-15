e59c63ac58478f7e33dba613e9fc9f4d6e1c39ce
Alexey Shvayka <shvaikalesh@gmail.com>
2020-01-15 18:48:04 +0200

    Add CreateResolvingFunctions tests

---
+++ test/built-ins/Promise/create-resolving-functions-reject.js
@@ -0,0 +1,27 @@
+// Copyright (C) 2019 Alexey Shvayka. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-createresolvingfunctions
+description: >
+  reject is anonymous built-in function with length of 1.
+info: |
+  CreateResolvingFunctions ( promise )
+
+  ...
+  7. Let reject be ! CreateBuiltinFunction(stepsReject, « [[Promise]], [[AlreadyResolved]] »).
+features: [Reflect.construct]
+includes: [isConstructor.js]
+flags: [async]
+---*/
+
+Promise.resolve(1).then(function() {
+  return Promise.resolve(2);
+});
+
+Promise.prototype.then = function(_resolve, reject) {
+  assert.sameValue(!isConstructor(reject));
+  assert.sameValue(reject.length, 1);
+  assert.sameValue(reject.name, '');
+
+  $DONE();
+};
