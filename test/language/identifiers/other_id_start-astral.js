b131e3d41673cdc3c0f6b5abe632a4ccc602cb42
Alexey Shvayka <shvaikalesh@gmail.com>
2020-02-06 21:49:28 +0200

    Add astral ID_Start identifiers test

---
+++ test/language/identifiers/other_id_start-astral.js
@@ -0,0 +1,45 @@
+// Copyright (C) 2020 Alexey Shvayka. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-names-and-keywords
+description: Test grandfathered astral characters of ID_Start.
+info: |
+  Grandfathered astral characters (Other_ID_Start)
+---*/
+
+// Other_ID_Start (Unicode 4.1)
+var 𐨪; // U+10A2A
+var 𐅫; // U+1016B
+
+// Other_ID_Start (Unicode 5.2)
+var 𓁬; // U+1306C
+var 𒅹; // U+12179
+
+// Other_ID_Start (Unicode 6.3)
+var 𖼟; // U+16F1F
+var 𑄥; // U+11125
+
+// Other_ID_Start (Unicode 7.0)
+var 𞠄; // U+1E804
+var 𞢗; // U+1E897
+
+// Other_ID_Start (Unicode 8.0)
+var 𒓇; // U+124C7
+var 𐳢; // U+10CE2
+
+// Other_ID_Start (Unicode 9.0)
+var 𘋁; // U+182C1
+var 𗌹; // U+17339
+
+// Other_ID_Start (Unicode 10.0)
+var 𛋐; // U+1B2D0
+var 𛂑; // U+1B091
+
+// Other_ID_Start (Unicode 11.0)
+var 𘟮; // U+187EE
+var 𐴁; // U+10D01
+
+// Other_ID_Start (Unicode 12.0)
+var 𞋧; // U+1E2E7
+var 𖽆; // U+16F46
