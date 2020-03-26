03799f88666895f55580a47e1ecd7ea79bebda70
Rick Waldron <waldron.rick@gmail.com>
2020-03-25 21:33:52 -0400

    Atomics.waitAsync: false, nan for timeouts

---
+++ test/built-ins/Atomics/waitAsync/false-for-timeout.js
@@ -0,0 +1,58 @@
+// Copyright (C) 2020 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.waitasync
+description: >
+  False timeout arg should result in an +0 timeout
+info: |
+  Atomics.waitAsync( typedArray, index, value, timeout )
+
+  1. Return DoWait(async, typedArray, index, value, timeout).
+
+  DoWait ( mode, typedArray, index, value, timeout )
+
+  6. Let q be ? ToNumber(timeout).
+
+flags: [async, CanBlockIsTrue]
+includes: [atomicsHelper.js]
+features: [Atomics.waitAsync, SharedArrayBuffer, TypedArray, Atomics]
+---*/
+const i32a = new Int32Array(
+  new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 4)
+);
+
+const valueOf = {
+  valueOf() {
+    return false;
+  }
+};
+
+const toPrimitive = {
+  [Symbol.toPrimitive]() {
+    return false;
+  }
+};
+
+(async () => {
+  try {
+    assert.sameValue(
+      await Atomics.waitAsync(i32a, 0, 0, false),
+      'timed-out',
+      'Atomics.waitAsync(i32a, 0, 0, false) returns "timed-out"'
+    );
+    assert.sameValue(
+      await Atomics.waitAsync(i32a, 0, 0, valueOf),
+      'timed-out',
+      'Atomics.waitAsync(i32a, 0, 0, valueOf) returns "timed-out"'
+    );
+    assert.sameValue(
+      await Atomics.waitAsync(i32a, 0, 0, toPrimitive),
+      'timed-out',
+      'Atomics.waitAsync(i32a, 0, 0, toPrimitive) returns "timed-out"'
+    );
+    $DONE();
+  } catch (error) {
+    $DONE(error);
+  }
+})();
