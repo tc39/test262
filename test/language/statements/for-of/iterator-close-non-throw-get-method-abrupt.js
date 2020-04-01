12f405b16168585ed48044784c62780e7a46808b
Alexey Shvayka <shvaikalesh@gmail.com>
2020-04-01 07:09:58 +0300

    Add IteratorClose tests

--- test/language/statements/for-of/iterator-close-non-throw-get-method-abrupt.js
+++ test/language/statements/for-of/iterator-close-non-throw-get-method-abrupt.js
@@ -1,10 +1,25 @@
 // Copyright (C) 2015 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-es6id: 13.6.4.13
+esid: sec-iteratorclose
 description: >
-    If retrieving an iterator's `return` method generates an error while
-    closing the iterator, the error should be forwarded to the runtime.
+  If retrieving an iterator's `return` method generates an error while
+  closing the iterator with non-throw completion, the error should be
+  forwarded to the runtime.
+info: |
+  IteratorClose ( iteratorRecord, completion )
+
+  [...]
+  4. Let innerResult be GetMethod(iterator, "return").
+  5. If innerResult.[[Type]] is normal,
+    [...]
+  6. If completion.[[Type]] is throw, return Completion(completion).
+  7. If innerResult.[[Type]] is throw, return Completion(innerResult).
+
+  GetMethod ( V, P )
+
+  [...]
+  2. Let func be ? GetV(V, P).
 features: [Symbol.iterator]
 ---*/
 
