23b23f64a1745924fb333b9bfac76c179ba17512
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2020-04-23 13:47:58 -0700

    Change compareArray to allow new property
    Change the compareArray to code which allow new property but still assert the actual output contains the expected and assert the order.

--- test/intl402/DateTimeFormat/prototype/resolvedOptions/order-dayPeriod.js
+++ test/intl402/DateTimeFormat/prototype/resolvedOptions/order-dayPeriod.js
@@ -4,8 +4,8 @@
 /*---
 esid: sec-intl.datetimeformat.prototype.resolvedoptions
 description: Verifies the property order for the object returned by resolvedOptions().
-includes: [compareArray.js]
-features: [Intl.DateTimeFormat-dayPeriod,Intl.DateTimeFormat-fractionalSecondDigits]
+includes: [arrayContains.js]
+features: [Intl.DateTimeFormat-dayPeriod]
 ---*/
 
 const options = new Intl.DateTimeFormat([], {
@@ -24,7 +24,14 @@ const expected = [
   "dayPeriod",
   "hour",
   "minute",
-  "fractionalSecondDigits",
 ];
 
-assert.compareArray(Object.getOwnPropertyNames(options), expected);
+let actual = Object.getOwnPropertyNames(options);
+
+// Ensure all expected items are in actual and also allow other property
+// implemented in other new proposal.
+assert(arrayContains(actual, expected));
+for (var i = 1; i < expected.length; i++) {
+  // Ensure the order as expected but allow additional new property in between
+  assert(actual.indexOf(expected[i-1]) < actual.indexOf(expected[i]));
+}
