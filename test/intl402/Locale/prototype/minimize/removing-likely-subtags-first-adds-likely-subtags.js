850c65317fdc19f10902a9824964f8f2e23f4dad
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2020-04-27 21:12:46 -0700

    Revert "Correct the expectation of zh-Hant"
    This reverts commit e8dfe549a886bf66cab86a2e715055e143a8a286.

--- test/intl402/Locale/prototype/minimize/removing-likely-subtags-first-adds-likely-subtags.js
+++ test/intl402/Locale/prototype/minimize/removing-likely-subtags-first-adds-likely-subtags.js
@@ -34,7 +34,7 @@ var testDataMinimal = {
     // https://unicode-org.atlassian.net/browse/ICU-12345
     "und-CW": "pap-CW",
     "und-US": "en",
-    "zh-Hant": "zh-Hant",
+    "zh-Hant": "zh-TW",
     "zh-Hani": "zh-Hani",
 };
 
