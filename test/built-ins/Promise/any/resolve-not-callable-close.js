3b853d60fb5b2494f3e35eef8e8efedceba52056
Marja Hölttä <marja@chromium.org>
2020-04-28 10:32:13 +0200

    fix

--- test/built-ins/Promise/any/resolve-not-callable-close.js
+++ test/built-ins/Promise/any/resolve-not-callable-close.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Leo Balter, 2020 Rick Waldron. All rights reserved.
+// Copyright (C) 2020 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
