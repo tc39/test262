d4ce3d6b4e99f3dcc6e8447b027ada872fd68f3e
Frank Yung-Fong Tang <41213225+FrankYFTang@users.noreply.github.com>
2020-05-07 20:59:45 -0700

    consider the canonicalized condiction
    consider the condition which the locale is supported in canonicalized locale

--- test/intl402/supportedLocalesOf-unicode-extensions-ignored.js
+++ test/intl402/supportedLocalesOf-unicode-extensions-ignored.js
@@ -28,8 +28,8 @@ testWithIntlConstructors(function (Constructor) {
             if (supported1.length === 1) {
                 assert.sameValue(supported2.length, 1, "#1.1: Presence of Unicode locale extension sequence affects whether locale " + locale + " is considered supported with matcher " + matcher + ".");
                 assert.sameValue(supported3.length, 1, "#1.2: Presence of Unicode locale extension sequence affects whether locale " + locale + " is considered supported with matcher " + matcher + ".");
-                assert.sameValue(supported2[0], locale + validExtension, "#2.1: Unicode locale extension sequence is not correctly returned for locale " + locale + " with matcher " + matcher + ".");
-                assert.sameValue(supported3[0], locale + invalidExtension, "#2.2: Unicode locale extension sequence is not correctly returned for locale " + locale + " with matcher " + matcher + ".");
+                assert.sameValue(supported2[0], supported1[0] + validExtension, "#2.1: Unicode locale extension sequence is not correctly returned for locale " + locale + " with matcher " + matcher + ".");
+                assert.sameValue(supported3[0], supported1[0] + invalidExtension, "#2.2: Unicode locale extension sequence is not correctly returned for locale " + locale + " with matcher " + matcher + ".");
             } else {
                 assert.sameValue(supported2.length, 0, "#3.1: Presence of Unicode locale extension sequence affects whether locale " + locale + " is considered supported with matcher " + matcher + ".");
                 assert.sameValue(supported3.length, 0, "#3.2: Presence of Unicode locale extension sequence affects whether locale " + locale + " is considered supported with matcher " + matcher + ".");
