14095fa7d90ff7484f23eed7d3cddbfbbc956755
Rick Waldron <waldron.rick@gmail.com>
2020-05-13 13:59:57 -0400

    Atomics.compareExchange will operate on TA when TA.buffer is not a SharedArrayBuffer

---
+++ test/built-ins/Atomics/compareExchange/bigint/non-shared-bufferdata.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2020 Rick Waldron.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.compareExchange
+description: >
+  Atomics.compareExchange will operate on TA when TA.buffer is not a SharedArrayBuffer
+features: [ArrayBuffer, Atomics, BigInt, TypedArray]
+---*/
+
+const i64a = new BigInt64Array(
+  new ArrayBuffer(BigInt64Array.BYTES_PER_ELEMENT * 4)
+);
+
+assert.sameValue(Atomics.compareExchange(i64a, 0, 0n, 1n), 0n);
+assert.sameValue(Atomics.load(i64a, 0), 1n);
