131165be7e7e0009c52aa766cac377e7f702af54
Rick Waldron <waldron.rick@gmail.com>
2020-05-20 14:18:33 -0400

    Atomics.or will operate on TA when TA.buffer is not a SharedArrayBuffer

---
+++ test/built-ins/Atomics/or/non-shared-bufferdata.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.or
+description: >
+  Atomics.or will operate on TA when TA.buffer is not a SharedArrayBuffer
+features: [ArrayBuffer, Atomics, TypedArray]
+---*/
+
+const i32a = new Int32Array(
+  new ArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 4)
+);
+
+assert.sameValue(Atomics.or(i32a, 0, 1), 0);
+assert.sameValue(Atomics.load(i32a, 0), 1);
+
