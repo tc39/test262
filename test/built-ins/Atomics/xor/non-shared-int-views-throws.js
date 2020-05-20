bb1e1ff999994b1af0fde62bbf73b3060baac1f4
Rick Waldron <waldron.rick@gmail.com>
2020-05-20 14:19:28 -0400

    Atomics.xor will operate on TA when TA.buffer is not a SharedArrayBuffer

---
+++ test/built-ins/Atomics/xor/non-shared-int-views-throws.js
@@ -0,0 +1,20 @@
+// Copyright (C) 2017 Mozilla Corporation.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.and
+description: >
+  Atomics.and throws when operating on non-sharable integer TypedArrays
+includes: [testTypedArray.js]
+features: [ArrayBuffer, Atomics, TypedArray]
+---*/
+
+const buffer = new ArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 4);
+
+testWithNonSharableTypedArrayConstructors(function(TA) {
+  const view = new TA(buffer);
+
+  assert.throws(TypeError, function() {
+    Atomics.and(view, 0, 1);
+  }, `Atomics.and(new ${TA.name}(buffer), 0, 1) throws TypeError`);
+});
