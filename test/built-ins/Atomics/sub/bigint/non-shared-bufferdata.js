a298d986bfb8e8b82cd3d1c30c07240b51cc11b2
Rick Waldron <waldron.rick@gmail.com>
2020-05-20 14:19:06 -0400

    Atomics.sub will operate on TA when TA.buffer is not a SharedArrayBuffer

---
+++ test/built-ins/Atomics/sub/bigint/non-shared-bufferdata.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 Rick Waldron.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.sub
+description: >
+  Atomics.sub will operate on TA when TA.buffer is not a SharedArrayBuffer
+features: [ArrayBuffer, Atomics, BigInt, TypedArray]
+---*/
+
+const i64a = new BigInt64Array(
+  new ArrayBuffer(BigInt64Array.BYTES_PER_ELEMENT * 4)
+);
+
+assert.sameValue(Atomics.store(i64a, 0, 1n), 1n);
+assert.sameValue(Atomics.sub(i64a, 0, 1n), 1n);
+assert.sameValue(Atomics.load(i64a, 0), 0n);
---
+++ test/built-ins/Atomics/sub/bigint/non-shared-int-views-throws.js
@@ -0,0 +1,19 @@
+// Copyright (C) 2020 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-atomics.sub
+description: >
+  Atomics.sub throws when operating on non-sharable integer TypedArrays
+includes: [testBigIntTypedArray.js]
+features: [ArrayBuffer, Atomics, BigInt, TypedArray]
+---*/
+
+const buffer = new ArrayBuffer(BigInt64Array.BYTES_PER_ELEMENT * 4);
+
+testWithNonShareableBigIntTypedArrayConstructors(function(TA) {
+  const view = new TA(buffer);
+  assert.throws(TypeError, function() {
+    Atomics.sub(view, 0, 1n);
+  }, `Atomics.sub(new ${TA.name}(buffer), 0, 1n) throws TypeError`);
+});
