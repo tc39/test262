91a9abff4e90385c7ee2814f3ecd214189913ca6
QuXing9 <40267809+QuXing9@users.noreply.github.com>
2020-07-10 00:57:55 +0800

    Add three testcases for test262 suite. (#2692)
    * Add tests for escape function when parameter is not a string.
    
    Fixes #2687
    Fixes #2637
    
    * Add test for indirect eval calls  when script is a for statement.
    
    When for statement doesn't have a body, it should throw a SyntaxError.
    
    Fixes #2661
    
    * Add tests for Function Constructor when body contains usestrict.
    
    Fixes #2688
    Fixes #2638

---
+++ test/annexB/built-ins/escape/argument_types.js
@@ -0,0 +1,19 @@
+// Copyright (C) 2020 Qu Xing. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-escape-string
+description: Input is a null, undefined or boolean
+info: |
+    B.2.1.1 escape ( string )
+
+    1. Let string be ? ToString(string).
+    ...
+---*/
+
+assert.sameValue(escape(null), 'null');
+
+assert.sameValue(escape(undefined), 'undefined');
+
+assert.sameValue(escape(true), 'true');
+
+assert.sameValue(escape(false), 'false');
