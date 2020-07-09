19653bdfc846a660d01cd7d5089cf4b21fe89fe0
QuXing9 <40267809+QuXing9@users.noreply.github.com>
2020-07-10 01:16:18 +0800

    Additional tests for escape and unescape methods (#2695)
    Co-authored-by: Leo Balter <leonardo.balter@gmail.com>

--- test/annexB/built-ins/escape/argument_types.js
+++ test/annexB/built-ins/escape/argument_types.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-escape-string
-description: Input is a null, undefined or boolean
+description: Input is a null, undefined, boolean or Number
 info: |
     B.2.1.1 escape ( string )
 
@@ -14,6 +14,20 @@ assert.sameValue(escape(null), 'null');
 
 assert.sameValue(escape(undefined), 'undefined');
 
+assert.sameValue(escape(), 'undefined');
+
 assert.sameValue(escape(true), 'true');
 
 assert.sameValue(escape(false), 'false');
+
+assert.sameValue(escape(-0), '0');
+
+assert.sameValue(escape(0), '0');
+
+assert.sameValue(escape(1), '1');
+
+assert.sameValue(escape(NaN), 'NaN');
+
+assert.sameValue(escape(Number.POSITIVE_INFINITY), 'Infinity');
+
+assert.sameValue(escape(Number.NEGATIVE_INFINITY), '-Infinity');
