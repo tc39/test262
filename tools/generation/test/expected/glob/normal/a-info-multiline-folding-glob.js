d8fcf19cc04559c92f58ea7493f697411eb3da54
Rick Waldron <waldron.rick@gmail.com>
2020-07-30 13:35:14 -0400

    Add globbing support to test generator

---
+++ tools/generation/test/expected/glob/normal/a-info-multiline-folding-glob.js
@@ -0,0 +1,30 @@
+// This file was procedurally generated from the following sources:
+// - tools/generation/test/fixtures/glob.case
+// - tools/generation/test/fixtures/glob/a/info-multiline-folding.template
+/*---
+description: foobar (First template name)
+es6id: 1.2.3
+flags: [generated, a, b]
+includes: [foo.js]
+info: |
+    This is an "info" field with an
+
+    empty line
+    Trailing white space in the template:
+    should be preserved:
+
+
+    case info
+---*/
+
+// Trailing white space in the test body:
+// should be preserved:
+
+
+
+
+
+
+
+
+'This is "teardown" code.';
