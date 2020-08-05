f2ac047e669cfed41aecb703d39bb3f9a889884f
Adam Kluball <adamkluball@gmail.com>
2020-08-04 23:37:33 -0400

    Add tests for ZWJ and ZWNJ in identifiers

---
+++ test/language/identifiers/part-zwj-escaped.js
@@ -0,0 +1,10 @@
+// Copyright (C) 2020 Adam Kluball. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+author: Adam Kluball
+esid: sec-names-and-keywords
+description: zero width joiner is a valid identifier part
+---*/
+
+var $\u200D;
