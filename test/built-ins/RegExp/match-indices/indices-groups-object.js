0d8a0ca3a9d218e8f2d259ebf12368a258abb28d
Gus Caplan <me@gus.host>
2020-09-24 14:12:17 -0500

    move __proto__ tests

--- test/built-ins/RegExp/match-indices/indices-groups-object.js
+++ test/built-ins/RegExp/match-indices/indices-groups-object.js
@@ -3,7 +3,7 @@
 
 /*---
 description: The groups object of indices is created with CreateDataProperty
-includes: [propertyHelper.js]
+includes: [compareArray.js]
 esid: sec-makeindicesarray
 features: [regexp-named-groups, regexp-match-indices]
 info: |
@@ -15,20 +15,8 @@ info: |
     10. Perform ! CreateDataProperty(_A_, `"groups"`, _groups_).
 ---*/
 
-
-// `groups` is created with Define, not Set.
-let counter = 0;
-Object.defineProperty(Array.prototype, "groups", {
-  set() { counter++; }
-});
-
-let indices = /(?<x>.)/.exec("a").indices;
-assert.sameValue(counter, 0);
-
-// `groups` is writable, enumerable and configurable
-// (from CreateDataProperty).
-verifyProperty(indices, 'groups', {
-    writable: true,
-    enumerable: true,
-    configurable: true
-});
+// The `__proto__` property on the groups object is not special,
+// and does not affect the [[Prototype]] of the resulting groups object.
+let {groups} = /(?<__proto__>.)/.exec("a").indices;
+assert.compareArray([0, 1], groups.__proto__);
+assert.sameValue(null, Object.getPrototypeOf(groups));
