881c9e3ba551bb655ca7b4e688f967fb3fac7754
Gus Caplan <me@gus.host>
2020-09-24 14:12:17 -0500

    move __proto__ tests

--- test/built-ins/RegExp/named-groups/groups-object.js
+++ test/built-ins/RegExp/named-groups/groups-object.js
@@ -3,7 +3,6 @@
 
 /*---
 description: Properties of the groups object are created with CreateDataProperty
-includes: [propertyHelper.js]
 esid: sec-regexpbuiltinexec
 features: [regexp-named-groups]
 info: |
@@ -15,19 +14,8 @@ info: |
     26. Perform ! CreateDataProperty(_A_, `"groups"`, _groups_).
 ---*/
 
-// `groups` is created with Define, not Set.
-let counter = 0;
-Object.defineProperty(Array.prototype, "groups", {
-  set() { counter++; }
-});
-
-let match = /(?<x>.)/.exec("a");
-assert.sameValue(counter, 0);
-
-// `groups` is writable, enumerable and configurable
-// (from CreateDataProperty).
-verifyProperty(match, "groups", {
-  writable: true,
-  enumerable: true,
-  configurable: true,
-});
+// The `__proto__` property on the groups object is not special,
+// and does not affect the [[Prototype]] of the resulting groups object.
+let {groups} = /(?<__proto__>.)/.exec("a");
+assert.sameValue("a", groups.__proto__);
+assert.sameValue(null, Object.getPrototypeOf(groups));
