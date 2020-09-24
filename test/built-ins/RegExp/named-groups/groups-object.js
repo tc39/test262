5fde8fa4124ae8ffb16062f5ba79451c677ee826
Gus Caplan <me@gus.host>
2020-09-24 14:20:06 -0500

    fixup

--- test/built-ins/RegExp/named-groups/groups-object.js
+++ test/built-ins/RegExp/named-groups/groups-object.js
@@ -3,6 +3,7 @@
 
 /*---
 description: Properties of the groups object are created with CreateDataProperty
+includes: [propertyHelper.js]
 esid: sec-regexpbuiltinexec
 features: [regexp-named-groups]
 info: |
@@ -13,6 +14,23 @@ info: |
       a. Let _groups_ be *undefined*.
     26. Perform ! CreateDataProperty(_A_, `"groups"`, _groups_).
 ---*/
+
+// `groups` is created with Define, not Set.
+let counter = 0;
+Object.defineProperty(Array.prototype, "groups", {
+  set() { counter++; }
+});
+
+let match = /(?<x>.)/.exec("a");
+assert.sameValue(counter, 0);
+
+// `groups` is writable, enumerable and configurable
+// (from CreateDataProperty).
+verifyProperty(match, "groups", {
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
 
 // The `__proto__` property on the groups object is not special,
 // and does not affect the [[Prototype]] of the resulting groups object.
