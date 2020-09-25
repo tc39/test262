faf0f674661ded008ccd08333872ecd8ee185c79
Rick Waldron <waldron.rick@gmail.com>
2020-09-25 13:21:51 -0400

    Generate tests

---
+++ test/language/expressions/class/cpn-class-expr-fields-computed-property-name-from-await-expression.js
@@ -0,0 +1,53 @@
+// This file was procedurally generated from the following sources:
+// - src/computed-property-names/computed-property-name-from-await-expression.case
+// - src/computed-property-names/evaluation/class-expression-fields.template
+/*---
+description: Computed property name from condition expression (ComputedPropertyName in ClassExpression)
+esid: prod-ComputedPropertyName
+features: [computed-property-names, top-level-await]
+flags: [generated, module]
+info: |
+    ClassExpression:
+      classBindingIdentifier opt ClassTail
+
+    ClassTail:
+      ClassHeritage opt { ClassBody opt }
+
+    ClassBody:
+      ClassElementList
+
+    ClassElementList:
+      ClassElement
+
+    ClassElement:
+      MethodDefinition
+
+    MethodDefinition:
+      PropertyName ...
+      get PropertyName ...
+      set PropertyName ...
+
+    PropertyName:
+      ComputedPropertyName
+
+    ComputedPropertyName:
+      [ AssignmentExpression ]
+---*/
+
+
+let C = class {
+  [await 9] = 9;
+
+  static [await 9] = 9;
+};
+
+let c = new C();
+
+assert.sameValue(
+  c[await 9],
+  9
+);
+assert.sameValue(
+  C[await 9],
+  9
+);
