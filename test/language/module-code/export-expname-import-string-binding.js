01ecea211d0334c0f4bf96e2df2c9caecca4b434
Bradley Farias <bfarias@godaddy.com>
2020-09-29 15:32:01 -0500

    Exports can be Arbitrary Strings

---
+++ test/language/module-code/export-expname-import-string-binding.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2020 Bradley Farias. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+description: >
+  ImportSpecifier :
+    ModuleExportName `as` IdentifierName
+  esid: prod-ImportSpecifier
+info: |
+  ImportSpecifier :
+    ModuleExportName `as` IdentifierName
+
+flags: [module]
+---*/
+import { "☿" as Ami } from "./export-expname_FIXTURE.js";
+
+assert.sameValue(Ami, globalThis.Mercury);
