c43c9d8448ecb270a8460f1fb3f7db629b0fe1ae
Bradley Farias <bfarias@godaddy.com>
2020-09-29 15:32:01 -0500

    Exports can be Arbitrary Strings

---
+++ test/language/module-code/export-expname-binding-string.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 Bradley Farias. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+description: Module Namespace Objects can have non-Identifier properties
+esid: prod-ExportSpecifier
+info:
+  ExportSpecifier[From] :
+    IdentifierName `as` ModuleExportName
+
+  ModuleExportName : StringLiteral
+
+flags: [module]
+---*/
+import * as Scouts from "./export-expname_FIXTURE.js";
+
+assert.sameValue(Scouts.Mercury, undefined);
+assert.sameValue(Scouts["☿"], globalThis.Mercury);
---
+++ test/language/module-code/export-expname_FIXTURE.js
@@ -0,0 +1,9 @@
+// Copyright (C) 2020 Bradley Farias. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+flags: [module]
+---*/
+
+export { Mercury as "☿" };
+function Mercury() {}
+globalThis.Mercury = Mercury;
