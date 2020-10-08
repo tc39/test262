740d02416049fb5f0386306e420202aa77469929
devsnek <me@gus.host>
2020-10-08 17:20:37 -0500

    Add missing feature

--- test/built-ins/Object/seal/seal-sharedarraybuffer.js
+++ test/built-ins/Object/seal/seal-sharedarraybuffer.js
@@ -30,7 +30,7 @@ info: |
           Let desc be the PropertyDescriptor { [[Configurable]]: false, [[Writable]]: false }.
         Perform ? DefinePropertyOrThrow(O, k, desc).
   Return true.
-
+features: [SharedArrayBuffer]
 ---*/
 
 Object.seal(new SharedArrayBuffer(4));
