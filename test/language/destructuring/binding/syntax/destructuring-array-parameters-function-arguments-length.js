b5ccbcb5e483714508cd8f1c63167ef348d654a4
Rick Waldron <waldron.rick@gmail.com>
2020-11-13 11:31:15 -0500

    Coverage: Missing test for function length and parameter destructuring. Closes gh-2895

---
+++ test/language/destructuring/binding/syntax/destructuring-array-parameters-function-arguments-length.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-destructuring-binding-patterns
+description: >
+  Funciton.length when ArrayBindingPattern in FormalParameterList
+features: [destructuring-binding]
+---*/
+
+assert.sameValue((([a,b]) => {}).length, 1);
+assert.sameValue((function([a,b]) {}).length, 1);
+assert.sameValue((function * ([a,b]) {}).length, 1);
+assert.sameValue((async ([a,b]) => {}).length, 1);
+assert.sameValue((async function([a,b]) {}).length, 1);
+assert.sameValue((async function * ([a,b]) {}).length, 1);
+
