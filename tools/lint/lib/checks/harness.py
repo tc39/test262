a00e2330cefb7e000ecd697d8d77129f90b9ba13
rwaldron <waldron.rick@gmail.com>
2021-06-24 14:09:52 -0400

    tools: add deprecated $ERROR api to linter checks

--- tools/lint/lib/checks/harness.py
+++ tools/lint/lib/checks/harness.py
@@ -9,5 +9,8 @@ class CheckHarness(Check):
     def run(self, name, meta, source):
         if 'verifyConfigurable(' in source and 'verifyProperty(' in source:
             return 'verifyConfigurable & verifyProperty may not be used in the same file'
-        else:
-            return
+
+        if '$ERROR' in source or '$ERROR(' in source:
+            return '$ERROR() is deprecated, do not use in new or edited tests. https://github.com/tc39/test262/blob/main/CONTRIBUTING.md#test-environment'
+
+        return
