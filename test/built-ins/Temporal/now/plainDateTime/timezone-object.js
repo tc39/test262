12aa613bcc566794a7dce781880026770f5b3467
Mike Pennisi <mike@mikepennisi.com>
2021-07-12 19:48:49 -0400

    Improve coverage

--- test/built-ins/Temporal/now/plainDateTime/timezone-object.js
+++ test/built-ins/Temporal/now/plainDateTime/timezone-object.js
@@ -11,11 +11,29 @@ features: [Temporal]
 const actual = [];
 const expected = [
   "has timeZone.timeZone",
-  "get timeZone.getOffsetNanosecondsFor",
-  "call timeZone.getOffsetNanosecondsFor",
+  "get timeZone.timeZone",
+  "has nestedTimeZone.timeZone",
+  "get nestedTimeZone.getOffsetNanosecondsFor",
+  "call nestedTimeZone.getOffsetNanosecondsFor",
 ];
-
+const nestedTimeZone = new Proxy({
+  getOffsetNanosecondsFor(instant) {
+    actual.push("call nestedTimeZone.getOffsetNanosecondsFor");
+    assert.sameValue(instant instanceof Temporal.Instant, true, "Instant");
+    return -Number(instant.epochNanoseconds % 86400_000_000_000n);
+  },
+}, {
+  has(target, property) {
+    actual.push(`has nestedTimeZone.${String(property)}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get nestedTimeZone.${String(property)}`);
+    return target[property];
+  },
+});
 const timeZone = new Proxy({
+  timeZone: nestedTimeZone,
   getOffsetNanosecondsFor(instant) {
     actual.push("call timeZone.getOffsetNanosecondsFor");
     assert.sameValue(instant instanceof Temporal.Instant, true, "Instant");
