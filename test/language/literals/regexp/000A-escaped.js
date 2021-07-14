f49999aeaec347705173448c19161c02785ff7bb
Leo Balter <leonardo.balter@gmail.com>
2021-07-14 14:04:20 -0700

    Generated tests

---
+++ test/language/literals/regexp/000A-escaped.js
@@ -0,0 +1,17 @@
+// This file was procedurally generated from the following sources:
+// - src/regexp-insignificant-ws/000A-escaped.case
+// - src/regexp-insignificant-ws/default/after.template
+/*---
+description: 000A (LF) (WhiteSpace character positioned after)
+esid: prod-RegularExpressionLiteral
+flags: [generated]
+info: |
+    In the ECMAScript lexical grammar, WhiteSpace is defined to include ASCII
+    tab/vertical tab/form feed/space, nonbreaking space, BOM/zero-width
+    nonbreaking space, and any code point with the Unicode property
+    “Space_Separator”, and LineTerminator is any of U+000A LINE FEED, U+000D
+    CARRIAGE RETURN, U+2028 LINE SEPARATOR, and U+2029 PARAGRAPH SEPARATOR. Both
+    are required to be insignificant in between expression tokens
+---*/
+
+/.//*{ character }*/;
