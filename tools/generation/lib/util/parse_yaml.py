b627ffbbd478966117e599c08023f99f817e1d8a
Richard Gibson <richard.gibson@gmail.com>
2021-07-15 01:29:38 -0400

    Improve YAML-capturing regex

--- tools/generation/lib/util/parse_yaml.py
+++ tools/generation/lib/util/parse_yaml.py
@@ -3,15 +3,16 @@
 
 import yaml, re
 
-yamlPattern = re.compile(r'\---\n([\s]*)((?:\s|\S)*)[\n\s*]---',
-                         flags=re.DOTALL|re.MULTILINE)
+yamlPattern = re.compile(
+        r'^\s*---\n\s*?(?P<indent>[^\n\S]*)(?P<content>\S.*?)(?:\n[^\n\S]*)?---\s*$',
+        flags=re.DOTALL)
 
 def parse_yaml(string):
     match = yamlPattern.match(string)
     if not match:
         return False
 
-    unindented = re.sub('^' + match.group(1), '',
-        match.group(2), flags=re.MULTILINE)
+    unindented = re.sub('^' + match.group('indent'), '',
+        match.group('content'), flags=re.MULTILINE)
 
     return yaml.safe_load(unindented)
