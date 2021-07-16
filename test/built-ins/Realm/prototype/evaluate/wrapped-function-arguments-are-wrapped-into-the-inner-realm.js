1399ab923e1c467dfef7c5ffce333e9544b308ef
rwaldron <waldron.rick@gmail.com>
2021-07-16 12:01:09 -0400

    tests: Realm.prototype.evaluate semantics

---
+++ test/built-ins/Realm/prototype/evaluate/wrapped-function-arguments-are-wrapped-into-the-inner-realm.js
@@ -0,0 +1,25 @@
+// Copyright (C) 2021 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-realm.prototype.evaluate
+description: >
+  Realm.prototype.evaluate wrapped function arguments are wrapped into the inner realm
+includes: [isConstructor.js]
+features: [callable-boundary-realms]
+---*/
+
+assert.sameValue(
+  typeof Realm.prototype.evaluate,
+  'function',
+  'This test must fail if Realm.prototype.evaluate is not a function'
+);
+
+const r = new Realm();
+const blueFn = (x, y) => x + y;
+
+const redWrappedFn = r.evaluate(`
+0, function(blueWrappedFn, a, b, c) {
+    return blueWrappedFn(a, b) * c;
+}
+`);
+assert.sameValue(redWrappedFn(blueFn, 2, 3, 4), 20);
