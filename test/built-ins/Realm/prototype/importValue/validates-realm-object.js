843b1baf7bf302e1febd12669863e441a7715dda
rwaldron <waldron.rick@gmail.com>
2021-07-16 13:58:47 -0400

    tests: Realm.prototype.importValue semantics

---
+++ test/built-ins/Realm/prototype/importValue/validates-realm-object.js
@@ -0,0 +1,22 @@
+// Copyright (C) 2021 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-realm.prototype.importvalue
+description: >
+  Realm.prototype.importValue validates realm object.
+includes: [isConstructor.js]
+features: [callable-boundary-realms]
+---*/
+
+assert.sameValue(
+  typeof Realm.prototype.importValue,
+  'function',
+  'This test must fail if Realm.prototype.importValue is not a function'
+);
+
+const r = new Realm();
+const bogus = {};
+
+assert.throws(TypeError, function() {
+  r.importValue.call(bogus);
+}, 'throws a TypeError if this is not a Realm object');
