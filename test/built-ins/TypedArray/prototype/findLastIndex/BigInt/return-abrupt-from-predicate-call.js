f86fb713479e2384107f4cb1a7a1274e7561a5b0
Wenlu Wang <wenlwan@microsoft.com>
2021-07-19 15:11:55 +0800

    Fix format

--- test/built-ins/TypedArray/prototype/findLastIndex/BigInt/return-abrupt-from-predicate-call.js
+++ test/built-ins/TypedArray/prototype/findLastIndex/BigInt/return-abrupt-from-predicate-call.js
@@ -5,7 +5,7 @@ esid: sec-%typedarray%.prototype.findlastindex
 description: >
   Return abrupt from predicate call.
 info: |
-   %TypedArray%.prototype.findLastIndex ( predicate [ , thisArg ] )
+  %TypedArray%.prototype.findLastIndex ( predicate [ , thisArg ] )
 
   ...
   5. Let k be len - 1.
