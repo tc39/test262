29cb47e376bde5528391276019a30075a79ce3f5
rwaldron <waldron.rick@gmail.com>
2021-07-19 13:23:41 -0400

    fix: correct capitalization

--- test/built-ins/Realm/length.js
+++ test/built-ins/Realm/length.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Rick waldron. All rights reserved.
+// Copyright (C) 2021 Rick Waldron. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-realm-constructor
