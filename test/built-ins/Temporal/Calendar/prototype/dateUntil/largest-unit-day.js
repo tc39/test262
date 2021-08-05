5d70cc4a1bd84bf2eaa4e6d9f42d39ae50c54424
Frank Tang <ftang@chromium.org>
2021-08-04 17:39:49 -0700

    use harness and add more tests

--- test/built-ins/Temporal/Calendar/prototype/dateUntil/largest-unit-day.js
+++ test/built-ins/Temporal/Calendar/prototype/dateUntil/largest-unit-day.js
@@ -3,7 +3,7 @@
 
 /*---
 esid: sec-temporal.calendar.prototype.dateuntil
-description: Temporal.Calendar.prototype.dateUntil with largestUnit === "day"
+description: Temporal.Calendar.prototype.dateUntil with largestUnit: "day"
 info: |
   1. Let calendar be the this value.
   2. Perform ? RequireInternalSlot(calendar, [[InitializedTemporalCalendar]]).
@@ -15,28 +15,45 @@ info: |
   8. Let result be ! DifferenceISODate(one.[[ISOYear]], one.[[ISOMonth]], one.[[ISODay]], two.[[ISOYear]], two.[[ISOMonth]], two.[[ISODay]], largestUnit).
   9. Return ? CreateTemporalDuration(result.[[Years]], result.[[Months]], result.[[Weeks]], result.[[Days]], 0, 0, 0, 0, 0, 0).
 features: [Temporal]
+includes: [temporalHelpers.js]
 ---*/
 let cal = new Temporal.Calendar("iso8601");
 
 ["day", "days"].forEach(function(largestUnit) {
   let opt = {largestUnit};
-  assert.sameValue("PT0S", cal.dateUntil("2021-07-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("P1D", cal.dateUntil("2021-07-16", "2021-07-17", opt).toJSON());
-  assert.sameValue("P32D", cal.dateUntil("2021-07-16", "2021-08-17", opt).toJSON());
-  assert.sameValue("P62D", cal.dateUntil("2021-07-16", "2021-09-16", opt).toJSON());
-  assert.sameValue("P365D",
-      cal.dateUntil("2021-07-16", "2022-07-16", opt).toJSON());
-  assert.sameValue("P3652D"
-      ,cal.dateUntil("2021-07-16", "2031-07-16", opt).toJSON());
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-07-16", opt),
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "same day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-07-17", opt),
+      0, 0, 0, 1, 0, 0, 0, 0, 0, 0, "one day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-08-17", opt),
+      0, 0, 0, 32, 0, 0, 0, 0, 0, 0, "32 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-09-16", opt),
+      0, 0, 0, 62, 0, 0, 0, 0, 0, 0, "62 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2022-07-16", opt),
+      0, 0, 0, 365, 0, 0, 0, 0, 0, 0, "365 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2031-07-16", opt),
+      0, 0, 0, 3652, 0, 0, 0, 0, 0, 0, "3652 days");
 
-  assert.sameValue("-P1D",
-      cal.dateUntil("2021-07-17", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P32D",
-      cal.dateUntil("2021-08-17", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P62D",
-      cal.dateUntil("2021-09-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P365D",
-      cal.dateUntil("2022-07-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P3652D",
-      cal.dateUntil("2031-07-16", "2021-07-16", opt).toJSON());
+
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-17", "2021-07-16", opt),
+      0, 0, 0, -1, 0, 0, 0, 0, 0, 0, "negative one day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-08-17", "2021-07-16", opt),
+      0, 0, 0, -32, 0, 0, 0, 0, 0, 0, "negative 32 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-09-16", "2021-07-16", opt),
+      0, 0, 0, -62, 0, 0, 0, 0, 0, 0, "negative 62 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2022-07-16", "2021-07-16", opt),
+      0, 0, 0, -365, 0, 0, 0, 0, 0, 0, "negative 365 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2031-07-16", "2021-07-16", opt),
+      0, 0, 0, -3652, 0, 0, 0, 0, 0, 0, "negative 3652 days");
 });
--- test/built-ins/Temporal/Calendar/prototype/dateUntil/largest-unit-week.js
+++ test/built-ins/Temporal/Calendar/prototype/dateUntil/largest-unit-week.js
@@ -3,7 +3,7 @@
 
 /*---
 esid: sec-temporal.calendar.prototype.dateuntil
-description: Temporal.Calendar.prototype.dateUntil with largestUnit === "week"
+description: Temporal.Calendar.prototype.dateUntil with largestUnit: "week"
 info: |
   1. Let calendar be the this value.
   2. Perform ? RequireInternalSlot(calendar, [[InitializedTemporalCalendar]]).
@@ -15,32 +15,57 @@ info: |
   8. Let result be ! DifferenceISODate(one.[[ISOYear]], one.[[ISOMonth]], one.[[ISODay]], two.[[ISOYear]], two.[[ISOMonth]], two.[[ISODay]], largestUnit).
   9. Return ? CreateTemporalDuration(result.[[Years]], result.[[Months]], result.[[Weeks]], result.[[Days]], 0, 0, 0, 0, 0, 0).
 features: [Temporal]
+includes: [temporalHelpers.js]
 ---*/
 let cal = new Temporal.Calendar("iso8601");
 
 ["week", "weeks"].forEach(function(largestUnit) {
   let opt = {largestUnit};
-  assert.sameValue("PT0S", cal.dateUntil("2021-07-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("P1D", cal.dateUntil("2021-07-16", "2021-07-17", opt).toJSON());
-  assert.sameValue("P1W", cal.dateUntil("2021-07-16", "2021-07-23", opt).toJSON());
-  assert.sameValue("P4W4D", cal.dateUntil("2021-07-16", "2021-08-17", opt).toJSON());
-  assert.sameValue("P4W", cal.dateUntil("2021-07-16", "2021-08-13", opt).toJSON());
-  assert.sameValue("P8W6D", cal.dateUntil("2021-07-16", "2021-09-16", opt).toJSON());
-  assert.sameValue("P52W1D",
-      cal.dateUntil("2021-07-16", "2022-07-16", opt).toJSON());
-  assert.sameValue("P521W5D"
-      ,cal.dateUntil("2021-07-16", "2031-07-16", opt).toJSON());
 
-  assert.sameValue("-P1D",
-      cal.dateUntil("2021-07-17", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P4W4D",
-      cal.dateUntil("2021-08-17", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P4W",
-      cal.dateUntil("2021-08-13", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P8W6D",
-      cal.dateUntil("2021-09-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P52W1D",
-      cal.dateUntil("2022-07-16", "2021-07-16", opt).toJSON());
-  assert.sameValue("-P521W5D",
-      cal.dateUntil("2031-07-16", "2021-07-16", opt).toJSON());
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-07-16", opt),
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "same day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-07-17", opt),
+      0, 0, 0, 1, 0, 0, 0, 0, 0, 0, "one day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-07-23", opt),
+      0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "7 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-08-16", opt),
+      0, 0, 4, 3, 0, 0, 0, 0, 0, 0, "4 weeks and 3 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-08-13", opt),
+      0, 0, 4, 0, 0, 0, 0, 0, 0, 0, "4 weeks");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2021-09-16", opt),
+      0, 0, 8, 6, 0, 0, 0, 0, 0, 0, "8 weeks and 6 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2022-07-16", opt),
+      0, 0, 52, 1, 0, 0, 0, 0, 0, 0, "52 weeks and 1 day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-16", "2031-07-16", opt),
+      0, 0, 521, 5, 0, 0, 0, 0, 0, 0, "521 weeks and 5 days");
+
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-17", "2021-07-16", opt),
+      0, 0, 0, -1, 0, 0, 0, 0, 0, 0, "negative one day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-07-23", "2021-07-16", opt),
+      0, 0, -1, 0, 0, 0, 0, 0, 0, 0, "negative 7 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-08-16", "2021-07-16", opt),
+      0, 0, -4, -3, 0, 0, 0, 0, 0, 0, "negative 4 weeks and 3 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-08-13", "2021-07-16", opt),
+      0, 0, -4, 0, 0, 0, 0, 0, 0, 0, "negative 4 weeks");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2021-09-16", "2021-07-16", opt),
+      0, 0, -8, -6, 0, 0, 0, 0, 0, 0, "negative 8 weeks and 6 days");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2022-07-16", "2021-07-16", opt),
+      0, 0, -52, -1, 0, 0, 0, 0, 0, 0, "negative 52 weeks and 1 day");
+  TemporalHelpers.assertDuration(
+      cal.dateUntil("2031-07-16", "2021-07-16", opt),
+      0, 0, -521, -5, 0, 0, 0, 0, 0, 0, "negative 521 weeks and 5 days");
 });
