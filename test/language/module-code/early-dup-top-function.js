ddff7c5ded4d0b8516b80c97f2c09bd5ad2e7257
legendecas <legendecas@gmail.com>
2021-08-17 11:48:10 +0800

    Add test for duplicated top level function declarations in modules

---
+++ test/language/module-code/early-dup-top-function.js
@@ -0,0 +1,25 @@
+// Copyright 2021 Chengzhong Wu. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+es6id: 10.2.1
+description: >
+    It is a Syntax Error if the LexicallyDeclaredNames of ModuleItemList
+    contains any duplicate entries.
+    At the top level of a Module, function declarations are treated like
+    lexical declarations rather than like var declarations.
+flags: [module]
+negative:
+  phase: parse
+  type: SyntaxError
+---*/
+
+$DONOTEVALUATE();
+
+function x() {
+
+}
+
+async function x() {
+
+}
