1667f56c04435127f94cf21bb94be8e0a68f8af2
Rick Waldron <waldron.rick@gmail.com>
2021-09-02 14:19:53 -0400

    Coverage for BigUint64Array & BigInt64Array backed by resizable / growable buffers. Fixes gh-3112 (#3154)

--- test/built-ins/TypedArray/prototype/reduceRight/BigInt/return-abrupt-from-this-out-of-bounds.js
+++ test/built-ins/TypedArray/prototype/reduceRight/BigInt/return-abrupt-from-this-out-of-bounds.js
@@ -1,16 +1,16 @@
 // Copyright (C) 2021 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-%typedarray%.prototype.values
+esid: sec-%typedarray%.prototype.reduceright
 description: Return abrupt when "this" value fails buffer boundary checks
-includes: [testTypedArray.js]
-features: [ArrayBuffer, TypedArray, arrow-function, resizable-arraybuffer]
+includes: [testBigIntTypedArray.js]
+features: [ArrayBuffer, BigInt, TypedArray, arrow-function, resizable-arraybuffer]
 ---*/
 
 assert.sameValue(
-  typeof TypedArray.prototype.values,
+  typeof TypedArray.prototype.reduceRight,
   'function',
-  'implements TypedArray.prototype.values'
+  'implements TypedArray.prototype.reduceRight'
 );
 
 assert.sameValue(
@@ -19,7 +19,7 @@ assert.sameValue(
   'implements ArrayBuffer.prototype.resize'
 );
 
-testWithTypedArrayConstructors(TA => {
+testWithBigIntTypedArrayConstructors(TA => {
   var BPE = TA.BYTES_PER_ELEMENT;
   var ab = new ArrayBuffer(BPE * 4, {maxByteLength: BPE * 5});
   var array = new TA(ab, BPE, 2);
@@ -29,14 +29,14 @@ testWithTypedArrayConstructors(TA => {
   } catch (_) {}
 
   // no error following grow:
-  array.values();
+  array.reduceRight(() => {});
 
   try {
     ab.resize(BPE * 3);
   } catch (_) {}
 
   // no error following shrink (within bounds):
-  array.values();
+  array.reduceRight(() => {});
 
   var expectedError;
   try {
@@ -47,13 +47,13 @@ testWithTypedArrayConstructors(TA => {
     expectedError = TypeError;
   } catch (_) {
     // The host is permitted to fail any "resize" operation at its own
-    // discretion. If that occurs, the values operation should complete
+    // discretion. If that occurs, the reduceRight operation should complete
     // successfully.
     expectedError = Test262Error;
   }
 
   assert.throws(expectedError, () => {
-    array.values();
-    throw new Test262Error('values completed successfully');
+    array.reduceRight(() => {});
+    throw new Test262Error('reduceRight completed successfully');
   });
 });
