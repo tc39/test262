41d34850d22bd14fa27902fe3eb378911c1a1e34
Mike Pennisi <mike@mikepennisi.com>
2021-09-10 20:46:25 -0400

    fixup! Correct metadata

--- test/built-ins/Temporal/Now/zonedDateTimeISO/length.js
+++ test/built-ins/Temporal/Now/zonedDateTimeISO/length.js
@@ -3,6 +3,7 @@
 
 /*---
 esid: sec-temporal.now.zoneddatetimeiso
+description: The `length` property of Temporal.Now.zonedDateTimeISO
 info: |
     Every built-in function object, including constructors, has a "length" property whose value is
     an integer. Unless otherwise specified, this value is equal to the largest number of named
