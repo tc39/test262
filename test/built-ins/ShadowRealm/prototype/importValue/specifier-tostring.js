b2eaa67e57777ad2137fc7bae393e975fed6b37e
Leo Balter <leonardo.balter@gmail.com>
2021-09-13 15:51:49 -0700

    Add more tests to ShadowRealms
    Ref #3044

--- test/built-ins/ShadowRealm/prototype/importValue/specifier-tostring.js
+++ test/built-ins/ShadowRealm/prototype/importValue/specifier-tostring.js
@@ -16,15 +16,58 @@ assert.sameValue(
 const r = new ShadowRealm();
 let count = 0;
 
-const specifier = {
-  toString() {
-    count += 1;
-    throw new Test262Error();
-  }
+const specifier = Object.create(null);
+
+// A - valueOF
+
+specifier.valueOf = function() {
+  count += 1;
+  throw new Test262Error();
 };
 
 assert.throws(Test262Error, () => {
   r.importValue(specifier);
-});
+}, 'ToString(specifier) returns abrupt from valueOf');
 
-assert.sameValue(count, 1);
+assert.sameValue(count, 1, 'ToString calls the valueOf method');
+
+
+// B - toString
+
+count = 0;
+
+specifier.valueOf = function() {
+  count += 1000;
+  throw new Error('valueOf is not reached if toString is present');
+};
+
+specifier.toString = function() {
+  count += 1;
+  throw new Test262Error();
+};
+
+assert.throws(Test262Error, () => {
+  r.importValue(specifier);
+}, 'ToString(specifier) returns abrupt from toString');
+
+assert.sameValue(count, 1, 'ToString calls the toString method');
+
+// C - @@toPrimitive
+
+count = 0;
+
+specifier[Symbol.toPrimitive] = function() {
+  count += 1;
+  throw new Test262Error();
+};
+
+specifier.toString = function() {
+  count += 1000;
+  throw new Error('toString is not reached if @@toPrimitive is present');
+};
+
+assert.throws(Test262Error, () => {
+  r.importValue(specifier);
+}, 'ToString(specifier) returns abrupt from @@toPrimitive');
+
+assert.sameValue(count, 1, 'ToString calls the @@toPrimitive method');
