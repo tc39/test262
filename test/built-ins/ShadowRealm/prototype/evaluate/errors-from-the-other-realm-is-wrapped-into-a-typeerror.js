ed0532e231f00f325b8767b12396442582f78e01
Phillip Mates <pmates@igalia.com>
2021-09-13 14:37:19 +0200

    syntax errors for `ShadowRealm.evaluate` should be exposed

--- test/built-ins/ShadowRealm/prototype/evaluate/errors-from-the-other-realm-is-wrapped-into-a-typeerror.js
+++ test/built-ins/ShadowRealm/prototype/evaluate/errors-from-the-other-realm-is-wrapped-into-a-typeerror.js
@@ -15,7 +15,7 @@ assert.sameValue(
 
 const r = new ShadowRealm();
 
-assert.throws(TypeError, () => r.evaluate('...'), 'SyntaxError => TypeError');
+assert.throws(SyntaxError, () => r.evaluate('...'), 'SyntaxError exposed to Parent');
 assert.throws(TypeError, () => r.evaluate('throw 42'), 'throw primitive => TypeError');
 assert.throws(TypeError, () => r.evaluate('throw new ReferenceError("aaa")'), 'custom ctor => TypeError');
 assert.throws(TypeError, () => r.evaluate('throw new TypeError("aaa")'), 'Child TypeError => Parent TypeError');
