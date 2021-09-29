56e537b9161fed58d80a953b02ac54461fffde99
Philip Chimento <philip.chimento@gmail.com>
2021-09-29 13:25:31 -0700

    Consolidate duplicate tests
    There were a few tests already in the tree that overlapped ones that I
    added in the previous commit. I've consolidated these and taken
    information from the deleted ones where applicable, and improved on the
    autogenerated assertion messages.

--- test/built-ins/Temporal/Calendar/prototype/dateFromFields/fields-not-object.js
+++ test/built-ins/Temporal/Calendar/prototype/dateFromFields/fields-not-object.js
@@ -4,11 +4,17 @@
 /*---
 esid: sec-temporal.calendar.prototype.datefromfields
 description: Throw a TypeError if the fields is not an object
-features: [Symbol, Temporal]
+info: |
+  4. If Type(_fields_) is not Object, throw a *TypeError* exception.
+features: [BigInt, Symbol, Temporal, arrow-function]
 ---*/
 
-const tests = [undefined, null, false, "string", Symbol("sym"), Math.PI, 42n];
+const tests = [undefined, null, true, false, "string", Symbol("sym"), Infinity, NaN, Math.PI, 42n];
 const iso = Temporal.Calendar.from("iso8601");
 for (const fields of tests) {
-  assert.throws(TypeError, () => iso.dateFromFields(fields, {}));
+  assert.throws(
+    TypeError,
+    () => iso.dateFromFields(fields, {})
+    `dateFromFields(${typeof fields}) throws a TypeError exception`
+  );
 }
