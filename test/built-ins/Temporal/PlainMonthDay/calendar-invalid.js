51227a4cba7a7ab3074a71c8e31d6feec4f2c27b
Frank Yung-Fong Tang <ftang@google.com>
2021-10-08 16:07:19 -0700

    Update calendar-invalid.js

--- test/built-ins/Temporal/PlainMonthDay/calendar-invalid.js
+++ test/built-ins/Temporal/PlainMonthDay/calendar-invalid.js
@@ -13,7 +13,7 @@ const actual = [];
 const args = [
   TemporalHelpers.toPrimitiveObserver(actual, 2, "month"),
   TemporalHelpers.toPrimitiveObserver(actual, 1, "day"),
-  "local",
+  "invalidCalendar",  // violate ISO8601 grammar
   TemporalHelpers.toPrimitiveObserver(actual, 1, "year")
 ];
 assert.throws(RangeError, () => new Temporal.PlainMonthDay(...args));
