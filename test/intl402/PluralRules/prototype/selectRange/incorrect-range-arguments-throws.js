069de7dfda1a758d9fc073dc429f6a86a7a87edb
Romulo Cintra <rcintra@igalia.com>
2021-11-10 12:24:23 +0100

    update Plural Rules tests

--- test/intl402/PluralRules/prototype/selectRange/incorrect-range-arguments-throws.js
+++ test/intl402/PluralRules/prototype/selectRange/incorrect-range-arguments-throws.js
@@ -3,12 +3,29 @@
 
 /*---
 esid: sec-Intl.PluralRules.prototype.selectRange
-description: Throws a RangeError if arguments aren't x < y or are equal
+description: Basic tests for the output of selectRange()
 features: [Intl.PluralRules-selectRange]
 ---*/
 
 const pr = new Intl.PluralRules("en-US");
-pr.selectRange(200,200);
-assert.throws(RangeError, function() {
-  pr.selectRange(200, 100);
-});
+
+// 1. Perform ? RequireInternalSlot(pr, [[InitializedPluralRules]])
+let p = pr['selectRange'];
+assert.throws(TypeError, () => { p(1, 201) });
+
+// 2. If arguments are undefined throw a TypeError exception.
+assert.throws(TypeError, () => { pr.selectRange(undefined, 201) });
+assert.throws(TypeError, () => { pr.selectRange(102, undefined) });
+assert.throws(TypeError, () => { pr.selectRange(undefined, undefined)});
+
+// 3. If arguments cannot be cast toNumber
+assert.throws(TypeError, () => { pr.selectRange(Symbol(102), 201) });
+assert.throws(TypeError, () => { pr.selectRange({}, -201) });
+assert.throws(TypeError, () => { pr.selectRange([], 201) });
+assert.throws(TypeError, () => { pr.selectRange("NaN", 201) });
+assert.throws(TypeError, () => { pr.selectRange("xyz", 201) });
+assert.throws(TypeError, () => { pr.selectRange(102, "NaN") });
+assert.throws(TypeError, () => { pr.selectRange(102, "xyz") });
+
+// 4. If x > y, throw a RangeError exception.
+assert.throws(RangeError, () => { pr.selectRange(201, 102) });
