ce511219e8647f79ae57673fd988cef94d7ace61
polsevev <rolf.martin@glomsrud.no>
2021-12-15 11:42:58 +0100

    add tests for array.prototype.groupByToMap

---
+++ test/built-ins/Array/prototype/groupByToMap/undefined.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2021 Ecma International.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-array.prototype.groupByToMap
+description: Array.prototype.groupByToMap applied to undefined throws a TypeError
+info: |
+  22.1.3.15 Array.prototype.groupByToMap ( callbackfn [ , thisArg ] )
+
+  ...
+
+  3. If IsCallable(callbackfn) is false, throw a TypeError exception.
+
+  ...
+
+---*/
+
+
+assert.throws(TypeError, function() {
+  Array.prototype.groupByToMap.call(undefined, undefined);
+});
