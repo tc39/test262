5ee4e0d60916fdc5dc1cf0f859a31f7a02071399
Frank Yung-Fong Tang <ftang@google.com>
2022-01-06 11:20:17 -0800

    change to minimumFractionDigits:1

--- test/intl402/NumberFormat/prototype/format/format-rounding-increment-50.js
+++ test/intl402/NumberFormat/prototype/format/format-rounding-increment-50.js
@@ -15,13 +15,13 @@ var numberingSystems = ['arab', 'latn', 'thai', 'hanidec'];
 testNumberFormat(
   locales,
   numberingSystems,
-  {roundingIncrement: 50, maximumFractionDigits: 2},
+  {roundingIncrement: 50, maximumFractionDigits: 2, minimumFractionDigits: 1},
   {
     '1.500': '1.5',
     '1.625': '1.5',
-    '1.750': '2',
-    '1.875': '2',
-    '2.000': '2',
+    '1.750': '2.0',
+    '1.875': '2.0',
+    '2.000': '2.0',
   }
 );
 
