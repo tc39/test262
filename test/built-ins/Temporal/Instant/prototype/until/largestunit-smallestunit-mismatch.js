4f20476e0a15ddad2331b19af456efa987999ba5
Philip Chimento <pchimento@igalia.com>
2022-01-18 15:41:07 -0800

    Add tests for largestUnit/smallestUnit mismatch
    These tests already existed for PlainDate. Copy them to the other types
    (and use the constructor instead of from() in order to be as simple as
    possible)

--- test/built-ins/Temporal/Instant/prototype/until/largestunit-smallestunit-mismatch.js
+++ test/built-ins/Temporal/Instant/prototype/until/largestunit-smallestunit-mismatch.js
@@ -2,18 +2,18 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.plaindate.prototype.since
+esid: sec-temporal.instant.prototype.until
 description: RangeError thrown when smallestUnit is larger than largestUnit
 features: [Temporal]
 ---*/
 
-const earlier = new Temporal.PlainDate(2000, 5, 2);
-const later = new Temporal.PlainDate(2001, 6, 3);
-const units = ["years", "months", "weeks", "days"];
+const earlier = new Temporal.Instant(1_000_000_000_000_000_000n);
+const later = new Temporal.Instant(1_000_090_061_987_654_321n);
+const units = ["hours", "minutes", "seconds", "milliseconds", "microseconds", "nanoseconds"];
 for (let largestIdx = 1; largestIdx < units.length; largestIdx++) {
   for (let smallestIdx = 0; smallestIdx < largestIdx; smallestIdx++) {
     const largestUnit = units[largestIdx];
     const smallestUnit = units[smallestIdx];
-    assert.throws(RangeError, () => later.since(earlier, { largestUnit, smallestUnit }));
+    assert.throws(RangeError, () => earlier.until(later, { largestUnit, smallestUnit }));
   }
 }
