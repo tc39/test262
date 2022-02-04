87d5d8ec1169d1fc49e7c62035fd708645f3bef6
Philip Chimento <pchimento@igalia.com>
2022-02-03 17:53:44 -0800

    Rewrite tests that relied on previous branding behaviour
    A follow up to the previous commit, this rewrites some tests that relied
    on the lack of brand checks for certain Temporal.TimeZone methods.
    https://github.com/tc39/proposal-temporal/pull/1693 added brand checks to
    these methods.
    
    We can no longer use a plain object time zone or even a Proxy with a real
    branded Temporal.TimeZone object as its handler to do these tests, so we
    instead create an instance of Temporal.TimeZone and define own accessor
    properties on it in order to test the observable property accesses that we
    need to see according to the spec text.
    
    This requires an improvement to TemporalHelpers.observeProperty() in order
    to be able to log property accesses to Symbol-valued properties.

--- test/built-ins/Temporal/TimeZone/prototype/id/custom-timezone.js
+++ test/built-ins/Temporal/TimeZone/prototype/id/custom-timezone.js
@@ -2,26 +2,26 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.timezone.prototype.tojson
-description: toJSON() calls toString() and returns its value
+esid: sec-get-temporal.timezone.prototype.id
+description: Getter calls toString() and returns its value
 includes: [compareArray.js, temporalHelpers.js]
 features: [Temporal]
 ---*/
 
 const actual = [];
 const expected = [
-  'get [Symbol.toPrimitive]',
-  'get toString',
-  'call timeZone.toString',
+  "get [Symbol.toPrimitive]",
+  "get toString",
+  "call timeZone.toString",
 ];
 
 const timeZone = new Temporal.TimeZone("UTC");
 TemporalHelpers.observeProperty(actual, timeZone, Symbol.toPrimitive, undefined);
 TemporalHelpers.observeProperty(actual, timeZone, "toString", function () {
   actual.push("call timeZone.toString");
-  return "Etc/TAI";
+  return "time zone";
 });
 
-const result = timeZone.toJSON();
-assert.sameValue(result, 'Etc/TAI', 'toString');
+const result = timeZone.id;
 assert.compareArray(actual, expected);
+assert.sameValue(result, "time zone");
