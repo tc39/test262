18ce639a4c4768de337fa30b83bbb6de02b0e1a5
Ms2ger <Ms2ger@gmail.com>
2022-02-16 15:57:55 +0100

    Port tests for PlainTime.

--- test/built-ins/Temporal/PlainTime/prototype/until/roundingmode-invalid-string.js
+++ test/built-ins/Temporal/PlainTime/prototype/until/roundingmode-invalid-string.js
@@ -9,6 +9,7 @@ features: [Temporal]
 
 const earlier = new Temporal.PlainTime(12, 34, 56, 0, 0, 0);
 const later = new Temporal.PlainTime(13, 35, 57, 123, 987, 500);
-for (const roundingMode of ["other string", "cile", "CEIL", "ce\u0131l"]) {
+
+for (const roundingMode of ["other string", "cile", "CEIL", "ce\u0131l", "auto"]) {
   assert.throws(RangeError, () => earlier.until(later, { smallestUnit: "microsecond", roundingMode }));
 }
