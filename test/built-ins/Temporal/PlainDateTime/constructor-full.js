3b710b6bd22f08d03c52654665c67c329f9aa676
Jesse Alama <jesse@serverracket.com>
2022-03-11 09:18:13 +0100

    Use double quotes

--- test/built-ins/Temporal/PlainDateTime/constructor-full.js
+++ test/built-ins/Temporal/PlainDateTime/constructor-full.js
@@ -8,28 +8,28 @@ features: [Temporal]
 includes: [compareArray.js, temporalHelpers.js]
 ---*/
 
-const calendar = Temporal.Calendar.from('iso8601');
+const calendar = Temporal.Calendar.from("iso8601");
 const datetime = new Temporal.PlainDateTime(1976, 11, 18, 15, 23, 30, 123, 456, 789, calendar);
 
 TemporalHelpers.assertPlainDateTime(datetime,
-  1976, 11, 'M11', 18, 15, 23, 30, 123, 456, 789,
+  1976, 11, "M11", 18, 15, 23, 30, 123, 456, 789,
   "check instance (all arguments supplied)"
 );
 
 assert.sameValue(
   datetime.calendar,
   calendar,
-  'calendar supplied in constructor can be extracted and is unchanged'
+  "calendar supplied in constructor can be extracted and is unchanged"
 );
 
-assert.sameValue(datetime.dayOfWeek, 4, 'check day of week information');
-assert.sameValue(datetime.dayOfYear, 323, 'check day of year information');
-assert.sameValue(datetime.weekOfYear, 47, 'check week of year information');
-assert.sameValue(datetime.daysInWeek, 7, 'check number of days in week');
-assert.sameValue(datetime.monthsInYear, 12, 'check number of months in year');
+assert.sameValue(datetime.dayOfWeek, 4, "check day of week information");
+assert.sameValue(datetime.dayOfYear, 323, "check day of year information");
+assert.sameValue(datetime.weekOfYear, 47, "check week of year information");
+assert.sameValue(datetime.daysInWeek, 7, "check number of days in week");
+assert.sameValue(datetime.monthsInYear, 12, "check number of months in year");
 
 assert.sameValue(
   `${datetime}`,
-  '1976-11-18T15:23:30.123456789',
-  'check string value'
+  "1976-11-18T15:23:30.123456789",
+  "check string value"
 );
