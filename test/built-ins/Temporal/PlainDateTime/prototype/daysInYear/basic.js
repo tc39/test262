9afdd7013f4b20155c779e4f71d3fca42dc281a3
Jesse Alama <jessealama@users.noreply.github.com>
2022-03-13 07:54:20 +0100

    Update test/built-ins/Temporal/PlainDateTime/prototype/daysInYear/basic.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/daysInYear/basic.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/daysInYear/basic.js
@@ -9,4 +9,4 @@ features: [Temporal]
 
 const calendar = Temporal.Calendar.from("iso8601");
 const datetime = new Temporal.PlainDateTime(1976, 11, 18, 15, 23, 30, 123, 456, 789, calendar);
-assert.sameValue(datetime.daysInYear, 366, "check days of year information");
+assert.sameValue(datetime.daysInYear, 366, "check days in year information");
