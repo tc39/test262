d215a8db12b5aa8f36470a892273e2b7db26b8a8
Jesse Alama <jessealama@users.noreply.github.com>
2022-03-13 07:54:10 +0100

    Update test/built-ins/Temporal/PlainDateTime/prototype/daysInWeek/basic.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/daysInWeek/basic.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/daysInWeek/basic.js
@@ -9,4 +9,4 @@ features: [Temporal]
 
 const calendar = Temporal.Calendar.from("iso8601");
 const datetime = new Temporal.PlainDateTime(1976, 11, 18, 15, 23, 30, 123, 456, 789, calendar);
-assert.sameValue(datetime.dayOfWeek, 4, "check day of week information");
+assert.sameValue(datetime.daysInWeek, 7, "check days in week information");
