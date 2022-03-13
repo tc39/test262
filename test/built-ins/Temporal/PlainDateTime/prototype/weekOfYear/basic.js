9653d496f5cc10356b9c0dde18dc64370e7893f5
Jesse Alama <jessealama@users.noreply.github.com>
2022-03-13 07:54:28 +0100

    Update test/built-ins/Temporal/PlainDateTime/prototype/weekOfYear/basic.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/weekOfYear/basic.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/weekOfYear/basic.js
@@ -9,4 +9,4 @@ features: [Temporal]
 
 const calendar = Temporal.Calendar.from("iso8601");
 const datetime = new Temporal.PlainDateTime(1976, 11, 18, 15, 23, 30, 123, 456, 789, calendar);
-assert.sameValue(datetime.weekOfYear, 47, "check months of year information");
+assert.sameValue(datetime.weekOfYear, 47, "check week of year information");
