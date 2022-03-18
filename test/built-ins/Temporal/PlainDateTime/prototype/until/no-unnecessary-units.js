9a2e6b15a3d79445dc35d03489b52af809860cd4
Jesse Alama <jessealama@users.noreply.github.com>
2022-03-18 15:20:21 +0100

    Update test/built-ins/Temporal/PlainDateTime/prototype/until/no-unnecessary-units.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/until/no-unnecessary-units.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/until/no-unnecessary-units.js
@@ -12,12 +12,6 @@ const lastFeb20 = new Temporal.PlainDateTime(2020, 2, 29, 0, 0);
 const lastFeb21 = new Temporal.PlainDateTime(2021, 2, 28, 0, 0);
 
 TemporalHelpers.assertDuration(
-  lastFeb20.until(lastFeb21),
-  0, 0, 0, 365, 0, 0, 0, 0, 0, 0,
-  "does not include higher units than necessary (no largest unit)"
-);
-
-TemporalHelpers.assertDuration(
   lastFeb20.until(lastFeb21, { largestUnit: "months" }),
   0, 12, 0, 0, 0, 0, 0, 0, 0, 0,
   "does not include higher units than necessary (largest unit = months)"
