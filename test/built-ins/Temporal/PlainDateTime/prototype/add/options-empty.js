94272e1c77094e5007d1c96d58f5a275a523f541
Jesse Alama <jesse@serverracket.com>
2022-03-18 15:06:40 +0100

    Make test more interesting by checking ambiguity

--- test/built-ins/Temporal/PlainDateTime/prototype/add/options-empty.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/add/options-empty.js
@@ -11,13 +11,13 @@ includes: [temporalHelpers.js]
 const jan31 = new Temporal.PlainDateTime(2020, 1, 31, 15, 0);
 
 TemporalHelpers.assertPlainDateTime(
-  jan31.add({ years: 1 }, {}),
-  2021, 1, "M01", 31, 15, 0, 0, 0, 0, 0,
+  jan31.add({ months: 1 }, {}),
+  2020, 2, "M02", 29, 15, 0, 0, 0, 0, 0,
   "options may be empty object"
 );
 
 TemporalHelpers.assertPlainDateTime(
-  jan31.add({ years: 1 }, () => {}),
-  2021, 1, "M01", 31, 15, 0, 0, 0, 0, 0,
+  jan31.add({ months: 1 }, () => {}),
+  2020, 2, "M02", 29, 15, 0, 0, 0, 0, 0,
   "options may be function object"
 );
