4e8ebd4718aa60a8ce61eccd7f1311937a2c6980
Jesse Alama <jessealama@users.noreply.github.com>
2022-03-21 12:37:54 +0100

    Update test/built-ins/Temporal/PlainDateTime/prototype/withPlainDate/argument-plaindate.js
    Co-authored-by: Philip Chimento <philip.chimento@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/withPlainDate/argument-plaindate.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/withPlainDate/argument-plaindate.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.plaindatetime.withplaindate
+esid: sec-temporal.plaindatetime.prototype.withplaindate
 description: PlainDate object is acceptable
 includes: [temporalHelpers.js]
 features: [Temporal]
