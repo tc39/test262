33a5433d1bd1f38af7967fca0dd0665fd1f7f1cc
Jesse Alama <jesse@serverracket.com>
2022-03-25 12:40:12 +0100

    Temporal: PlainDateTime: Port Demitasse `until` and `since` tests
    Co-authored-by: Philip Chimento <philip.chimento@gmail.com>

--- test/built-ins/Temporal/PlainDateTime/prototype/since/subseconds.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/since/subseconds.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.plaindatetime.prototype.until
+esid: sec-temporal.plaindatetime.prototype.since
 description: Returned granularity may be finer than seconds
 includes: [temporalHelpers.js]
 features: [Temporal]
@@ -12,19 +12,19 @@ const feb20 = new Temporal.PlainDateTime(2020, 2, 1, 0, 0);
 const feb21 = new Temporal.PlainDateTime(2020, 2, 2, 0, 0, 0, 250, 250, 250);
 
 TemporalHelpers.assertDuration(
-  feb20.until(feb21, { largestUnit: "milliseconds" }),
+  feb21.since(feb20, { largestUnit: "milliseconds" }),
   0, 0, 0, 0, 0, 0, 0, 86400250, 250, 250,
-  "can return subseconds (millisecond precision)"
+  "can return subseconds (milliseconds)"
 );
 
 TemporalHelpers.assertDuration(
-  feb20.until(feb21, { largestUnit: "microseconds" }),
+  feb21.since(feb20, { largestUnit: "microseconds" }),
   0, 0, 0, 0, 0, 0, 0, 0, 86400250250, 250,
-  "can return subseconds (microsecond precision)"
+  "can return subseconds (microseconds)"
 );
 
 TemporalHelpers.assertDuration(
-  feb20.until(feb21, { largestUnit: "nanoseconds" }),
+  feb21.since(feb20, { largestUnit: "nanoseconds" }),
   0, 0, 0, 0, 0, 0, 0, 0, 0, 86400250250250,
-  "can return subseconds (nanosecond precision)"
+  "can return subseconds (nanoseconds)"
 );
--- test/built-ins/Temporal/PlainDateTime/prototype/since/weeks-months-mutually-exclusive.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/since/weeks-months-mutually-exclusive.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.plaindatetime.prototype.until
+esid: sec-temporal.plaindatetime.prototype.since
 description: Weeks and months are mutually exclusive
 includes: [temporalHelpers.js]
 features: [Temporal]
@@ -12,13 +12,13 @@ const dt = new Temporal.PlainDateTime(1976, 11, 18, 15, 23, 30, 123, 456, 789);
 const laterDateTime = dt.add({ days: 42, hours: 3 });
 
 TemporalHelpers.assertDuration(
-  dt.until(laterDateTime, { largestUnit: "weeks" }),
+  laterDateTime.since(dt, { largestUnit: "weeks" }),
   0, 0, 6, 0, 3, 0, 0, 0, 0, 0,
-  "weeks and months mutually exclusive (prefer weeks)"
+  "weeks and months are mutually exclusive (prefer weeks)"
 );
 
 TemporalHelpers.assertDuration(
-  dt.until(laterDateTime, { largestUnit: "months" }),
-  0, 1, 0, 12, 3, 0, 0, 0, 0, 0,
-  "weeks and months mutually exclusive (prefer months)"
+  laterDateTime.since(dt, { largestUnit: "months" }),
+  0, 1, 0, 12, 3, 0, 0, 0,0, 0,
+  "weeks and months are mutually exclusive (prefer months)"
 );
