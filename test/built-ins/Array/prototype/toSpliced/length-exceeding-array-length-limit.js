9e692b8827f947f422f875c5ae2d28b8cdc3b13b
Tim Chevalier <tjc@igalia.com>
2022-03-30 22:44:06 -0700

    Test additional cases for long arrays in Array#toSpliced

--- test/built-ins/Array/prototype/toSpliced/length-exceeding-array-length-limit.js
+++ test/built-ins/Array/prototype/toSpliced/length-exceeding-array-length-limit.js
@@ -45,7 +45,27 @@ assert.throws(RangeError, function() {
   Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
 });
 
+arrayLike.length = 2 ** 32;
+assert.throws(RangeError, function() {
+  Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
+});
+
+arrayLike.length = 2 ** 32 + 1;
+assert.throws(RangeError, function() {
+  Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
+});
+
 arrayLike.length = 2 ** 53 - 1;
+assert.throws(TypeError, function() {
+  Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
+});
+
+arrayLike.length = 2 ** 53;
+assert.throws(TypeError, function() {
+  Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
+});
+
+arrayLike.length = 2 ** 53 + 1;
 assert.throws(TypeError, function() {
   Array.prototype.toSpliced.call(arrayLike, 0, 0, 1);
 });
