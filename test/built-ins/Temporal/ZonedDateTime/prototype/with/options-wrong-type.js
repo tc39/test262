4ac16c25896be91c8b73659be621488ca2d36074
Philip Chimento <pchimento@igalia.com>
2022-04-08 10:52:39 -0700

    Add tests for Temporal options bags being of the wrong type
    This consolidates the few existing tests for options bags in Temporal
    being of the wrong type, and adds them for every entry point in Temporal
    that accepts an options bag.
    
    These are mostly identical tests, but there is a variation for methods
    like round() where either an options bag or string is accepted.

--- test/built-ins/Temporal/ZonedDateTime/prototype/with/options-wrong-type.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/with/options-wrong-type.js
@@ -2,23 +2,22 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.instant.prototype.round
-description: TypeError thrown when options argument is missing or a non-string primitive
+esid: sec-temporal.zoneddatetime.prototype.with
+description: TypeError thrown when options argument is a primitive
 features: [BigInt, Symbol, Temporal]
 ---*/
 
 const badOptions = [
-  undefined,
   null,
   true,
+  "some string",
   Symbol(),
   1,
   2n,
 ];
 
-const instance = new Temporal.Instant(0n);
-assert.throws(TypeError, () => instance.round(), "TypeError on missing options argument");
+const instance = new Temporal.ZonedDateTime(0n, "UTC");
 for (const value of badOptions) {
-  assert.throws(TypeError, () => instance.round(value),
+  assert.throws(TypeError, () => instance.with({ day: 5 }, value),
     `TypeError on wrong options type ${typeof value}`);
 };
