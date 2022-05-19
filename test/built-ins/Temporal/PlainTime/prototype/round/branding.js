2de2fe3084756781ae5f0caccaac0aec14529b4b
Ms2ger <Ms2ger@gmail.com>
2022-05-19 11:53:12 +0200

    Temporal: Ensure PlainTime branding tests fail if needed.

--- test/built-ins/Temporal/PlainTime/prototype/round/branding.js
+++ test/built-ins/Temporal/PlainTime/prototype/round/branding.js
@@ -11,12 +11,12 @@ const round = Temporal.PlainTime.prototype.round;
 
 assert.sameValue(typeof round, "function");
 
-assert.throws(TypeError, () => round.call(undefined), "undefined");
-assert.throws(TypeError, () => round.call(null), "null");
-assert.throws(TypeError, () => round.call(true), "true");
-assert.throws(TypeError, () => round.call(""), "empty string");
-assert.throws(TypeError, () => round.call(Symbol()), "symbol");
-assert.throws(TypeError, () => round.call(1), "1");
-assert.throws(TypeError, () => round.call({}), "plain object");
-assert.throws(TypeError, () => round.call(Temporal.PlainTime), "Temporal.PlainTime");
-assert.throws(TypeError, () => round.call(Temporal.PlainTime.prototype), "Temporal.PlainTime.prototype");
+assert.throws(TypeError, () => round.call(undefined, "hour"), "undefined");
+assert.throws(TypeError, () => round.call(null, "hour"), "null");
+assert.throws(TypeError, () => round.call(true, "hour"), "true");
+assert.throws(TypeError, () => round.call("", "hour"), "empty string");
+assert.throws(TypeError, () => round.call(Symbol(), "hour"), "symbol");
+assert.throws(TypeError, () => round.call(1, "hour"), "1");
+assert.throws(TypeError, () => round.call({}, "hour"), "plain object");
+assert.throws(TypeError, () => round.call(Temporal.PlainTime, "hour"), "Temporal.PlainTime");
+assert.throws(TypeError, () => round.call(Temporal.PlainTime.prototype, "hour"), "Temporal.PlainTime.prototype");
