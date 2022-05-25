b48c7343d37a1ea3b3d6868f12ea8ee89f7ed492
Ms2ger <Ms2ger@gmail.com>
2022-05-25 11:51:40 +0200

    Temporal: Add tests for prototype.constructor.

---
+++ test/built-ins/Temporal/Instant/prototype/constructor.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.instant.prototype.constructor
+description: Test for Temporal.Instant.prototype.constructor.
+info: The initial value of Temporal.Instant.prototype.constructor is %Temporal.Instant%.
+includes: [propertyHelper.js]
+---*/
+
+verifyProperty(Temporal.Instant.prototype, "constructor", {
+  value: Temporal.Instant,
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
---
+++ test/built-ins/Temporal/PlainYearMonth/prototype/constructor.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plainyearmonth.prototype.constructor
+description: Test for Temporal.PlainYearMonth.prototype.constructor.
+info: The initial value of Temporal.PlainYearMonth.prototype.constructor is %Temporal.PlainYearMonth%.
+includes: [propertyHelper.js]
+---*/
+
+verifyProperty(Temporal.PlainYearMonth.prototype, "constructor", {
+  value: Temporal.PlainYearMonth,
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
