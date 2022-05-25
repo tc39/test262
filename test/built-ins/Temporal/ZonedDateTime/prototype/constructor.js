f7b626133dce7d56d3df20ac8822f0fc310b995e
Ms2ger <Ms2ger@gmail.com>
2022-05-25 11:51:40 +0200

    Temporal: Add tests for prototype.constructor.

---
+++ test/built-ins/Temporal/ZonedDateTime/prototype/constructor.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.prototype.constructor
+description: Test for Temporal.ZonedDateTime.prototype.constructor.
+info: The initial value of Temporal.ZonedDateTime.prototype.constructor is %Temporal.ZonedDateTime%.
+includes: [propertyHelper.js]
+---*/
+
+verifyProperty(Temporal.ZonedDateTime.prototype, "constructor", {
+  value: Temporal.ZonedDateTime,
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
