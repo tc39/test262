89ab2fd92b9d1df4c43980b7d06d35aaa4ff95f9
Romulo Cintra <romulocintra@gmail.com>
2022-06-29 18:29:04 +0200

    increase coverage Intl.DurationFormat

---
+++ test/intl402/DurationFormat/prototype/formatToParts/branding.js
@@ -0,0 +1,22 @@
+// Copyright 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-Intl.DurationFormat.prototype.formatToParts
+description: Verifies the branding check for the "formatToParts" function of the DurationFormat prototype object.
+features: [Intl.DurationFormat]
+---*/
+
+const formatToParts = Intl.DurationFormat.prototype.formatToParts;
+
+assert.sameValue(typeof formatToParts, "function");
+
+assert.throws(TypeError, () => formatToParts.call(undefined), "undefined");
+assert.throws(TypeError, () => formatToParts.call(null), "null");
+assert.throws(TypeError, () => formatToParts.call(true), "true");
+assert.throws(TypeError, () => formatToParts.call(""), "empty string");
+assert.throws(TypeError, () => formatToParts.call(Symbol()), "symbol");
+assert.throws(TypeError, () => formatToParts.call(1), "1");
+assert.throws(TypeError, () => formatToParts.call({}), "plain object");
+assert.throws(TypeError, () => formatToParts.call(Intl.DurationFormat), "Intl.DurationFormat");
+assert.throws(TypeError, () => formatToParts.call(Intl.DurationFormat.prototype), "Intl.DurationFormat.prototype");
