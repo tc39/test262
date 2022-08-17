ce8f8f0354b59c553120155ae7dd1b4f013c6a99
Philip Chimento <pchimento@igalia.com>
2022-08-17 16:55:22 -0700

    Temporal: Start moving collections of valid/invalid strings into TemporalHelpers
    This adds an object, TemporalHelpers.ISO, which has methods that return
    arrays of various ISO strings. The idea is to deduplicate more string
    tests into methods on this object.

--- test/built-ins/Temporal/PlainTime/prototype/since/argument-string-time-designator-required-for-disambiguation.js
+++ test/built-ins/Temporal/PlainTime/prototype/since/argument-string-time-designator-required-for-disambiguation.js
@@ -4,60 +4,31 @@
 /*---
 esid: sec-temporal.plaintime.prototype.since
 description: ISO 8601 time designator "T" required in cases of ambiguity
+includes: [temporalHelpers.js]
 features: [Temporal, arrow-function]
 ---*/
 
 const instance = new Temporal.PlainTime(12, 34, 56, 987, 654, 321);
 
-const ambiguousStrings = [
-  "2021-12",  // ambiguity between YYYY-MM and HHMM-UU
-  "1214",     // ambiguity between MMDD and HHMM
-  "0229",     //   ditto, including MMDD that doesn't occur every year
-  "1130",     //   ditto, including DD that doesn't occur in every month
-  "12-14",    // ambiguity between MM-DD and HH-UU
-  "202112",   // ambiguity between YYYYMM and HHMMSS
-];
-ambiguousStrings.forEach((stringWithoutCalendar) => {
-  // calendar annotation must not cause disambiguation in favour of time
-  const stringWithCalendar = stringWithoutCalendar + '[u-ca=iso8601]';
-  [stringWithoutCalendar, stringWithCalendar].forEach((string) => {
-    let arg = string;
-    assert.throws(
-      RangeError,
-      () => instance.since(arg),
-      `${string} is ambiguous and requires T prefix`
-    );
-    // The same string with a T prefix should not throw:
-    arg = `T${string}`;
-    instance.since(arg);
+TemporalHelpers.ISO.plainTimeStringsAmbiguous().forEach((string) => {
+  let arg = string;
+  assert.throws(
+    RangeError,
+    () => instance.since(arg),
+    `'${arg}' is ambiguous and requires T prefix`
+  );
+  // The same string with a T prefix should not throw:
+  arg = `T${string}`;
+  instance.since(arg);
 
-    arg = ` ${string}`;
-    assert.throws(
-      RangeError,
-      () => instance.since(arg),
-      "space is not accepted as a substitute for T prefix"
-    );
-  });
+  arg = ` ${string}`;
+  assert.throws(
+    RangeError,
+    () => instance.since(arg),
+    `space is not accepted as a substitute for T prefix: '${arg}'`
+  );
 });
 
 // None of these should throw without a T prefix, because they are unambiguously time strings:
-const unambiguousStrings = [
-  "2021-13",          // 13 is not a month
-  "202113",           //   ditto
-  "2021-13[-13:00]",  //   ditto
-  "202113[-13:00]",   //   ditto
-  "0000-00",          // 0 is not a month
-  "000000",           //   ditto
-  "0000-00[UTC]",     //   ditto
-  "000000[UTC]",      //   ditto
-  "1314",             // 13 is not a month
-  "13-14",            //   ditto
-  "1232",             // 32 is not a day
-  "0230",             // 30 is not a day in February
-  "0631",             // 31 is not a day in June
-  "0000",             // 0 is neither a month nor a day
-  "00-00",            //   ditto
-  "2021-12[-12:00]",  // HHMM-UU is ambiguous with YYYY-MM, but TZ disambiguates
-  "202112[UTC]",      // HHMMSS is ambiguous with YYYYMM, but TZ disambiguates
-];
-unambiguousStrings.forEach((arg) => instance.since(arg));
+TemporalHelpers.ISO.plainTimeStringsUnambiguous().forEach(
+  (arg) => instance.since(arg));
