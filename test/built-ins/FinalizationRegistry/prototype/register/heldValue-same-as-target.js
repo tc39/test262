554a18c34d44cce74aa98e64bba41e71dd6eab21
Philip Chimento <pchimento@igalia.com>
2022-09-23 16:48:55 -0700

    Add tests for regular and well-known Symbols as weak values
    This adds tests to WeakMap, WeakSet, WeakRef, and FinalizationRegistry for
    Symbols as weakly-held values. Regular symbols and well-known symbols are
    both tested. These tests correspond to existing tests for Objects as
    weakly-held values, but are put in separate files so that they can be
    filtered out with the "symbols-as-weakmap-keys" feature flag.
    
    Registered symbols are not allowed; this is already tested in the "cannot-
    be-held-weakly" tests.
    
    See: #2850

--- test/built-ins/FinalizationRegistry/prototype/register/heldValue-same-as-target.js
+++ test/built-ins/FinalizationRegistry/prototype/register/heldValue-same-as-target.js
@@ -10,10 +10,26 @@ info: |
   2. Perform ? RequireInternalSlot(_finalizationRegistry_, [[Cells]]).
   3. If CanBeHeldWeakly(_target_) is *false*, throw a *TypeError* exception.
   4. If SameValue(_target_, _heldValue_) is *true*, throw a *TypeError* exception.
-features: [FinalizationRegistry]
+features: [FinalizationRegistry, Symbol]
 ---*/
 
 var finalizationRegistry = new FinalizationRegistry(function() {});
 
 var target = {};
 assert.throws(TypeError, () => finalizationRegistry.register(target, target));
+
+// The following will throw regardless of whether the implementation supports
+// Symbols as weak values. Step 3 if no, Step 4 if yes.
+
+var symbolTarget = Symbol('a description');
+assert.throws(
+  TypeError,
+  () => finalizationRegistry.register(symbolTarget, symbolTarget),
+  'target and heldValue are the same regular symbol'
+);
+
+assert.throws(
+  TypeError,
+  () => finalizationRegistry.register(Symbol.hasInstance, Symbol.hasInstance),
+  'target and heldValue are the same well-known symbol'
+);
