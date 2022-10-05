ce64f8ebacbd7a033a79d4dff28c4214b41cc5e6
Tim Chevalier <chevalier@alum.wellesley.edu>
2022-10-04 18:07:28 -0700

    Update test/built-ins/Array/prototype/toSpliced/start-neg-infinity-is-zero.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Array/prototype/toSpliced/start-neg-infinity-is-zero.js
+++ test/built-ins/Array/prototype/toSpliced/start-neg-infinity-is-zero.js
@@ -4,7 +4,7 @@
 /*---
 esid: sec-array.prototype.toSpliced
 description: >
-  Array.prototype.toSpliced converts the this value length to a number.
+  Array.prototype.toSpliced treats negative Infinity as zero for start.
 info: |
   Array.prototype.toSpliced ( start, deleteCount, ...items )
 
