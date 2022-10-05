8844e396dee5f8baa315e2d138bb3741b45609a7
Tim Chevalier <chevalier@alum.wellesley.edu>
2022-10-04 18:07:49 -0700

    Update test/built-ins/Array/prototype/toSpliced/start-neg-less-than-minus-length-is-zero.js
    Co-authored-by: Ms2ger <Ms2ger@gmail.com>

--- test/built-ins/Array/prototype/toSpliced/start-neg-less-than-minus-length-is-zero.js
+++ test/built-ins/Array/prototype/toSpliced/start-neg-less-than-minus-length-is-zero.js
@@ -4,7 +4,7 @@
 /*---
 esid: sec-array.prototype.toSpliced
 description: >
-  Array.prototype.toSpliced converts the this value length to a number.
+  Array.prototype.toSpliced treats a value smaller than -length as zero for start.
 info: |
   Array.prototype.toSpliced ( start, deleteCount, ...items )
 
