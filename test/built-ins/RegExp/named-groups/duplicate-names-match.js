382e65321a16dab9dfb52f5831ab176e7212b167
Ms2ger <Ms2ger@igalia.com>
2022-11-03 15:45:18 +0100

    Add more tests for duplicated named capture groups.
    Fixes #3704.

--- test/built-ins/RegExp/named-groups/duplicate-names-match.js
+++ test/built-ins/RegExp/named-groups/duplicate-names-match.js
@@ -8,15 +8,28 @@ features: [regexp-duplicate-named-groups]
 includes: [compareArray.js]
 ---*/
 
-assert.compareArray(["b", undefined, "b"], "bab".match(/(?<x>a)|(?<x>b)/));
-assert.compareArray(["b", "b", undefined], "bab".match(/(?<x>b)|(?<x>a)/));
+assert.compareArray("bab".match(/(?<x>a)|(?<x>b)/), ["b", undefined, "b"]);
+assert.compareArray("bab".match(/(?<x>b)|(?<x>a)/), ["b", "b", undefined]);
 
-assert.compareArray(["aa", "a", undefined], "aa".match(/(?:(?<x>a)|(?<x>b))\k<x>/));
-assert.compareArray(["bb", undefined, "b"], "bb".match(/(?:(?<x>a)|(?<x>b))\k<x>/));
+assert.compareArray("aa".match(/(?:(?<x>a)|(?<x>b))\k<x>/), ["aa", "a", undefined]);
+assert.compareArray("bb".match(/(?:(?<x>a)|(?<x>b))\k<x>/), ["bb", undefined, "b"]);
 
 let matchResult = "aabb".match(/(?:(?:(?<x>a)|(?<x>b))\k<x>){2}/);
-assert.compareArray(["aabb", undefined, "b"], matchResult);
+assert.compareArray(matchResult, ["aabb", undefined, "b"]);
 assert.sameValue(matchResult.groups.x, "b");
 
-let notMatched = "abab".match(/(?:(?:(?<x>a)|(?<x>b))\k<x>){2}/);
-assert.sameValue(notMatched, null);
+assert.sameValue("abab".match(/(?:(?:(?<x>a)|(?<x>b))\k<x>){2}/), null);
+
+assert.sameValue("abab".match(/(?:(?<x>a)|(?<x>b))\k<x>/), null);
+
+assert.sameValue("cdef".match(/(?:(?<x>a)|(?<x>b))\k<x>/), null);
+
+assert.compareArray("xx".match(/^(?:(?<a>x)|(?<a>y)|z)\k<a>$/), ["xx", "x", undefined]);
+assert.compareArray("z".match(/^(?:(?<a>x)|(?<a>y)|z)\k<a>$/), ["z", undefined, undefined]);
+assert.sameValue("zz".match(/^(?:(?<a>x)|(?<a>y)|z)\k<a>$/), null);
+assert.compareArray("zy".match(/(?<a>x)|(?:zy\k<a>)/), ["zy", undefined]);
+
+assert.compareArray("xz".match(/^(?:(?<a>x)|(?<a>y)|z){2}\k<a>$/), ["xz", undefined, undefined]);
+assert.compareArray("yz".match(/^(?:(?<a>x)|(?<a>y)|z){2}\k<a>$/), ["yz", undefined, undefined]);
+assert.sameValue("xzx".match(/^(?:(?<a>x)|(?<a>y)|z){2}\k<a>$/), null);
+assert.sameValue("yzy".match(/^(?:(?<a>x)|(?<a>y)|z){2}\k<a>$/), null);
