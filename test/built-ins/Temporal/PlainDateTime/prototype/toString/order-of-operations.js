a939e4b9820d6d553072e1b5b905ddf34b03bd5c
Philip Chimento <pchimento@igalia.com>
2022-11-03 17:53:50 -0700

    Temporal: Add some more order-of-operations tests
    This adds order-of-operations tests that cover all of the Temporal entry
    points that accept options bags, that were not already covered. We'll be
    using these tests in the future to verify
    https://github.com/tc39/proposal-temporal/issues/2254
    However, the tests in this commit reflect the current state of the
    proposal, not the potential normative change.

---
+++ test/built-ins/Temporal/PlainDateTime/prototype/toString/order-of-operations.js
@@ -0,0 +1,61 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindatetime.prototype.tostring
+description: Properties on objects passed to toString() are accessed in the correct order
+includes: [compareArray.js, temporalHelpers.js]
+features: [Temporal]
+---*/
+
+const expected = [
+  "get options.roundingMode",
+  "get options.roundingMode.toString",
+  "call options.roundingMode.toString",
+  "get options.calendarName",
+  "get options.calendarName.toString",
+  "call options.calendarName.toString",
+  "get this.calendar[Symbol.toPrimitive]",
+  "get this.calendar.toString",
+  "call this.calendar.toString",
+];
+const actual = [];
+
+const calendar = TemporalHelpers.calendarObserver(actual, "this.calendar");
+const instance = new Temporal.PlainDateTime(1990, 11, 3, 15, 54, 37, 123, 456, 789, calendar);
+// clear observable operations that occurred during the constructor call
+actual.splice(0);
+
+const expectedForSmallestUnit = [
+  "get options.smallestUnit",
+  "get options.smallestUnit.toString",
+  "call options.smallestUnit.toString",
+].concat(expected);
+
+instance.toString(
+  TemporalHelpers.propertyBagObserver(actual, {
+    fractionalSecondDigits: 3,
+    roundingMode: "halfExpand",
+    smallestUnit: "millisecond",
+    calendarName: "auto",
+  }, "options"),
+);
+assert.compareArray(actual, expectedForSmallestUnit, "order of operations");
+actual.splice(0); // clear
+
+const expectedForFractionalSecondDigits = [
+  "get options.smallestUnit",
+  "get options.fractionalSecondDigits",
+  "get options.fractionalSecondDigits.toString",
+  "call options.fractionalSecondDigits.toString",
+].concat(expected);
+
+instance.toString(
+  TemporalHelpers.propertyBagObserver(actual, {
+    fractionalSecondDigits: "auto",
+    roundingMode: "halfExpand",
+    smallestUnit: undefined,
+    calendarName: "auto",
+  }, "options"),
+);
+assert.compareArray(actual, expectedForFractionalSecondDigits, "order of operations with smallestUnit undefined");
