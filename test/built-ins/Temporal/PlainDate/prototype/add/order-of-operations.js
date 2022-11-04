f2e17efaf8a2e054c02ee37b2aaeb381ec586534
Philip Chimento <pchimento@igalia.com>
2022-11-03 17:49:38 -0700

    Temporal: Expand order-of-operations tests to cover calendar methods
    Some of the existing order-of-operations tests didn't observe the calendar
    operations. Add a TemporalHelpers.calendarObserver() calendar to the
    invocations of these methods in the order-of-operations tests.

--- test/built-ins/Temporal/PlainDate/prototype/add/order-of-operations.js
+++ test/built-ins/Temporal/PlainDate/prototype/add/order-of-operations.js
@@ -8,8 +8,8 @@ includes: [compareArray.js, temporalHelpers.js]
 features: [Temporal]
 ---*/
 
-const instance = new Temporal.PlainDate(2000, 5, 2);
 const expected = [
+  // ToTemporalDuration
   "get fields.days",
   "get fields.days.valueOf",
   "call fields.days.valueOf",
@@ -40,12 +40,21 @@ const expected = [
   "get fields.years",
   "get fields.years.valueOf",
   "call fields.years.valueOf",
+  // CalendarDateAdd
+  "get this.calendar.dateAdd",
+  "call this.calendar.dateAdd",
   // inside Calendar.p.dateAdd
   "get options.overflow",
   "get options.overflow.toString",
   "call options.overflow.toString",
 ];
 const actual = [];
+
+const calendar = TemporalHelpers.calendarObserver(actual, "this.calendar");
+const instance = new Temporal.PlainDate(2000, 5, 2, calendar);
+// clear observable operations that occurred during the constructor call
+actual.splice(0);
+
 const fields = TemporalHelpers.propertyBagObserver(actual, {
   years: 1,
   months: 1,
