49882a17f8d7b6bfccdfa0c1827168aab0b450f7
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2022-12-02 13:59:32 +0100

    Add a copyright and a description

--- test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
+++ test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
@@ -1,2 +1,9 @@
+// Copyright (C) 2022 Rudolph Gottesheim. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+description: >
+    Short German weekdays should be followed by a comma.
+---*/
+
 let dtf = new Intl.DateTimeFormat("de", { weekday: "short", day: "numeric", month: "long"});
 assert.sameValue(dtf.format(new Date(2022, 12, 24)), "Sa., 24. Dezember", "German weekday without year");
