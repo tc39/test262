87c359fd7c541d8e9e43ab0644cacf3321d731dd
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2022-12-05 09:15:53 +0100

    Months are zero-based

--- test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
+++ test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
@@ -6,4 +6,4 @@ description: >
 ---*/
 
 let dtf = new Intl.DateTimeFormat("de", { weekday: "short", day: "numeric", month: "long"});
-assert.sameValue(dtf.format(new Date(2022, 12, 24)), "Sa., 24. Dezember", "German weekday without year");
+assert.sameValue(dtf.format(new Date(2022, 11, 24)), "Sa., 24. Dezember", "German weekday without year");
