7734e482220cc0c3f70932d728c1db614c2664e1
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2022-12-05 09:17:36 +0100

    Add an esid to the new test

--- test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
+++ test/intl402/DateTimeFormat/prototype/format/german-weekday-no-year.js
@@ -1,6 +1,7 @@
 // Copyright (C) 2022 Rudolph Gottesheim. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
+esid: sec-formatdatetime
 description: >
     Short German weekdays should be followed by a comma.
 ---*/
