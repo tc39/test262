4b89081375038a913ca7389e61cc44fb39276a46
Cam Tenny <cam@igalia.com>
2022-12-26 16:02:01 -0800

    regexp-modifiers: Test coverage for syntax errors
    Cases and templates to generate syntax error tests for regular expression modifiers, plus a few cases not worth templating.

---
+++ test/language/literals/regexp/early-err-arithmetic-modifiers-no-colon-3.js
@@ -0,0 +1,15 @@
+/*---
+description: Regular expression modifiers should not parse without the colon.
+esid: sec-patterns-static-semantics-early-errors
+features: [regexp-modifiers]
+negative:
+  phase: parse
+  type: SyntaxError
+info: |
+    Atom :: ( ? RegularExpressionFlags - RegularExpressionFlags : Disjunction )
+    ...
+---*/
+
+$DONOTEVALUATE();
+
+/(?i-)/;
