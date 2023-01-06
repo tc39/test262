55fdf608203558ead8f33bca732cdb26eab9373d
Philip Chimento <pchimento@igalia.com>
2023-01-05 19:11:23 -0700

    Remove calendar slot from Temporal.PlainTime
    Has several effects on existing tests:
    - Remove PlainTime from various tests that extract a Temporal.Calendar
      instance from another Temporal object.
    - Remove Temporal.PlainTime.prototype.calendar property.
    - Remove calendar property from object returned from
      Temporal.PlainTime.prototype.getISOFields().
    - Ignore calendar annotation when converting ISO string to PlainTime.
    
    Normative PR: https://github.com/tc39/proposal-temporal/pull/2482

--- test/built-ins/Temporal/Now/zonedDateTime/calendar-temporal-object.js
+++ test/built-ins/Temporal/Now/zonedDateTime/calendar-temporal-object.js
@@ -14,12 +14,11 @@ features: [Temporal]
 
 const plainDate = new Temporal.PlainDate(2000, 5, 2);
 const plainDateTime = new Temporal.PlainDateTime(2000, 5, 2, 12, 34, 56, 987, 654, 321);
-const plainTime = new Temporal.PlainTime(12, 34, 56, 987, 654, 321);
 const plainMonthDay = new Temporal.PlainMonthDay(5, 2);
 const plainYearMonth = new Temporal.PlainYearMonth(2000, 5);
 const zonedDateTime = new Temporal.ZonedDateTime(1_000_000_000_000_000_000n, "UTC");
 
-[plainDate, plainDateTime, plainTime, plainMonthDay, plainYearMonth, zonedDateTime].forEach((arg) => {
+[plainDate, plainDateTime, plainMonthDay, plainYearMonth, zonedDateTime].forEach((arg) => {
   const actual = [];
   const expected = [];
 
