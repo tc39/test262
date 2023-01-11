374aac475d9de91720b56c06aba5da6a43b306be
Philip Chimento <pchimento@igalia.com>
2023-01-11 12:20:09 -0800

    Regularize "calendar-case-insensitive" tests
    For each entry point where a string calendar name is accepted, we should
    have a test that ensures the calendar name is case-insensitive. These
    tests existed but several were incomplete as they didn't take nested
    properties into account, and several entry points were missing this test.
    
    Fix a minor copy-paste issue with double semicolons.

--- test/built-ins/Temporal/Calendar/prototype/yearOfWeek/argument-propertybag-calendar-case-insensitive.js
+++ test/built-ins/Temporal/Calendar/prototype/yearOfWeek/argument-propertybag-calendar-case-insensitive.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.calendar.prototype.dayofyear
+esid: sec-temporal.calendar.prototype.yearofweek
 description: The calendar name is case-insensitive
 features: [Temporal]
 ---*/
@@ -12,9 +12,9 @@ const instance = new Temporal.Calendar("iso8601");
 const calendar = "IsO8601";
 
 let arg = { year: 1976, monthCode: "M11", day: 18, calendar };
-const result1 = instance.dayOfYear(arg);
-assert.sameValue(result1, 323, "Calendar is case-insensitive");
+const result1 = instance.yearOfWeek(arg);
+assert.sameValue(result1, 1976, "Calendar is case-insensitive");
 
 arg = { year: 1976, monthCode: "M11", day: 18, calendar: { calendar } };
-const result2 = instance.dayOfYear(arg);
-assert.sameValue(result2, 323, "Calendar is case-insensitive (nested property)");
+const result2 = instance.yearOfWeek(arg);
+assert.sameValue(result2, 1976, "Calendar is case-insensitive (nested property)");
