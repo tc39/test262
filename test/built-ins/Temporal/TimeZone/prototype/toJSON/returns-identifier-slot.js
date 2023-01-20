4de096a7456e1104d461caef4b6042ea173fffd0
Philip Chimento <pchimento@igalia.com>
2023-01-20 13:30:07 -0800

    Change Temporal.TimeZone.prototype.toJSON not to call toString
    Normative PR: https://github.com/tc39/proposal-temporal/pull/2482

--- test/built-ins/Temporal/TimeZone/prototype/toJSON/returns-identifier-slot.js
+++ test/built-ins/Temporal/TimeZone/prototype/toJSON/returns-identifier-slot.js
@@ -3,25 +3,21 @@
 
 /*---
 esid: sec-temporal.timezone.prototype.tojson
-description: toJSON() calls toString() and returns its value
+description: toJSON() returns the internal slot value
 includes: [compareArray.js, temporalHelpers.js]
 features: [Temporal]
 ---*/
 
 const actual = [];
-const expected = [
-  'get [Symbol.toPrimitive]',
-  'get toString',
-  'call timeZone.toString',
-];
 
 const timeZone = new Temporal.TimeZone("UTC");
 TemporalHelpers.observeProperty(actual, timeZone, Symbol.toPrimitive, undefined);
+TemporalHelpers.observeProperty(actual, timeZone, "id", "Etc/Bogus");
 TemporalHelpers.observeProperty(actual, timeZone, "toString", function () {
   actual.push("call timeZone.toString");
   return "Etc/TAI";
 });
 
 const result = timeZone.toJSON();
-assert.sameValue(result, 'Etc/TAI', 'toString');
-assert.compareArray(actual, expected);
+assert.sameValue(result, "UTC", "toJSON gets the internal slot value");
+assert.compareArray(actual, [], "should not invoke any observable operations");
