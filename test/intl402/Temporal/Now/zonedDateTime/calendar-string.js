adbf03656d5759a2d215f16e35e1cfe64486bd63
Philip Chimento <pchimento@igalia.com>
2023-02-17 13:06:20 -0800

    Store strings or objects in Temporal.ZonedDateTime [[TimeZone]] slot
    Normative PR: https://github.com/tc39/proposal-temporal/pull/2482

--- test/intl402/Temporal/Now/zonedDateTime/calendar-string.js
+++ test/intl402/Temporal/Now/zonedDateTime/calendar-string.js
@@ -12,4 +12,5 @@ const tz = Temporal.Now.timeZoneId();
 assert(zdt instanceof Temporal.ZonedDateTime);
 assert.sameValue(typeof zdt.getISOFields().calendar, "string", "calendar slot should store a string");
 assert.sameValue(zdt.calendarId, "gregory");
+assert.sameValue(typeof zdt.getISOFields().timeZone, "string", "time zone slot should store a string");
 assert.sameValue(zdt.timeZoneId, tz);
