6fecc44859e3efa6142958143475d843b5ca1ae0
Shu-yu Guo <syg@chromium.org>
2023-03-14 17:03:53 -0700

    Add tests for ArrayBuffer.prototype.transferToFixedLength
    See https://tc39.es/proposal-arraybuffer-transfer/

---
+++ test/built-ins/ArrayBuffer/prototype/transferToFixedLength/new-length-excessive.js
@@ -0,0 +1,28 @@
+// Copyright (C) 2023 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-arraybuffer.prototype.transfertofixedlength
+description: >
+  Throws a RangeError the newLength value is too large to create a new
+  ArrayBuffer.
+info: |
+  ArrayBuffer.prototype.transferToFixedLength ( [ newLength ] )
+
+  1. Let O be the this value.
+  2. Perform ? RequireInternalSlot(O, [[ArrayBufferData]]).
+  3. If IsSharedArrayBuffer(O) is true, throw a TypeError exception.
+  4. If IsDetachedBuffer(O) is true, throw a TypeError exception.
+  5. If newLength is undefined, let newByteLength be
+     O.[[ArrayBufferByteLength]].
+  6. Else, let newByteLength be ? ToIntegerOrInfinity(newLength).
+  7. Let new be ? Construct(%ArrayBuffer%, « 𝔽(newByteLength) »).
+  [...]
+features: [resizable-arraybuffer, arraybuffer-transfer]
+---*/
+
+var ab = new ArrayBuffer(0);
+
+assert.throws(RangeError, function() {
+  // Math.pow(2, 53) = 9007199254740992
+  ab.transferToFixedLength(9007199254740992);
+});
