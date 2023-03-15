c2403146c2245d2ea14dfeead7bb146890f1e4c1
Michael Ficarra <mficarra@shapesecurity.com>
2023-03-14 20:05:59 -0600

    missed some

---
+++ test/built-ins/Iterator/prototype/every/next-method-throws.js
@@ -0,0 +1,26 @@
+// Copyright (C) 2023 Michael Ficarra. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-iteratorprototype.every
+description: >
+  Underlying iterator has throwing next method
+info: |
+  %Iterator.prototype%.every ( predicate )
+
+  4.a. Let next be ? IteratorStep(iterated).
+
+includes: [iterators.js]
+features: [iterator-helpers]
+flags: []
+---*/
+class ThrowingIterator extends Iterator {
+  next() {
+    throw new Test262Error;
+  }
+}
+
+let iterator = new ThrowingIterator;
+
+assert.throws(Test262Error, function () {
+  iterator.every(() => {});
+});
