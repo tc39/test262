71f2a1f95cefaf80cd9e006747e8cfbb6c1cd90e
Guillaume Emont <guijemont@igalia.com>
2023-03-31 22:09:49 +0200

    Added new generated tests with uppercase I

---
+++ test/language/literals/regexp/early-err-arithmetic-modifiers-other-code-point-uppercase-I.js
@@ -0,0 +1,20 @@
+// This file was procedurally generated from the following sources:
+// - src/regexp-modifiers/other-code-point-uppercase-I.case
+// - src/regexp-modifiers/flags-syntax-error/arithmetic-modifiers-literal.template
+/*---
+description: It is a Syntax Error if the source text matched by RegularExpressionFlags contains any code point other than i, m, or s, or if it contains the same code point more than once. (arithmetic regular expression flags)
+esid: sec-patterns-static-semantics-early-errors
+features: [regexp-modifiers]
+flags: [generated]
+negative:
+  phase: parse
+  type: SyntaxError
+info: |
+    Atom :: ( ? RegularExpressionFlags - RegularExpressionFlags : Disjunction )
+    ...
+
+---*/
+
+$DONOTEVALUATE();
+
+/(?-I:a)//*{ global-modifiers }*/;
---
+++ test/language/literals/regexp/early-err-modifiers-other-code-point-uppercase-I.js
@@ -0,0 +1,20 @@
+// This file was procedurally generated from the following sources:
+// - src/regexp-modifiers/other-code-point-uppercase-I.case
+// - src/regexp-modifiers/flags-syntax-error/modifiers-literal.template
+/*---
+description: It is a Syntax Error if the source text matched by RegularExpressionFlags contains any code point other than i, m, or s, or if it contains the same code point more than once. (regular expression flags)
+esid: sec-patterns-static-semantics-early-errors
+features: [regexp-modifiers]
+flags: [generated]
+negative:
+  phase: parse
+  type: SyntaxError
+info: |
+    Atom :: ( ? RegularExpresisonFlags : Disjunction )
+    It is a Syntax Error if the source text matched by RegularExpressionFlags contains any code points other than "i", "m", "s", or if it contains the same code point more than once.
+
+---*/
+
+$DONOTEVALUATE();
+
+/(?I:a)//*{ global-modifiers }*/;
