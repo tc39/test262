377231a1deb37ff0e621a6cbc16c6ca4ca69dd5a
Veera <sveera.2001@gmail.com>
2023-05-16 16:31:11 -0400

    Add tests for increment and decrement operations on `this`

---
+++ test/language/expressions/postfix-increment/this.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2023 Ecma International.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-update-expressions-static-semantics-early-errors
+description: >
+  It is an early Syntax Error if AssignmentTargetType of LeftHandSideExpression is not simple. (this)
+info: |
+  sec-static-semantics-assignmenttargettype
+
+  PrimaryExpression : this 
+
+  Return invalid.
+
+  sec-update-expressions-static-semantics-early-errors
+
+  UpdateExpression: LeftHandSideExpression ++
+
+  It is an early Syntax Error if AssignmentTargetType of LeftHandSideExpression is not simple.
+negative:
+  phase: parse
+  type: SyntaxError
+---*/
+
+$DONOTEVALUATE();
+
+this++;
