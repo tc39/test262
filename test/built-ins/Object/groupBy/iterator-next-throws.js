49237f30de6d9d4d7eed5bf6af91ad47a18851d0
Jordan Harband <ljharb@gmail.com>
2023-05-18 23:09:11 -0700

    Convert Array.prototype.group tests to Object.groupBy tests

---
+++ test/built-ins/Object/groupBy/iterator-next-throws.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 Ecma International.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-object.groupby
+description: Object.groupBy throws when iterator next throws
+info: |
+  Object.groupBy ( items, callbackfn )
+
+  ...
+
+  GroupBy ( items, callbackfn, coercion )
+
+  6. Repeat,
+    b. Let next be ? IteratorStep(iteratorRecord).
+
+  ...
+features: [array-grouping]
+---*/
+
+const throwingIterator = {
+  next: function next() {
+    throw new Test262Error('next() method was called');
+  }
+};
+
+assert.throws(Test262Error, function () {
+  Object.groupBy(throwingIterator, function () {
+    return 'key';
+  });
+});
