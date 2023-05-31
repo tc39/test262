b36d1d58963831c5f52d87c4359d66086f89c133
Michael Ficarra <mficarra@shapesecurity.com>
2023-05-31 13:37:49 -0600

    add iteator-helpers feature flag to Symbol.toStringTag test

--- test/built-ins/Iterator/prototype/Symbol.toStringTag/prop-desc.js
+++ test/built-ins/Iterator/prototype/Symbol.toStringTag/prop-desc.js
@@ -9,10 +9,9 @@ info: |
     Every other data property described in clauses 18 through 26 and in Annex
     B.2 has the attributes { [[Writable]]: true, [[Enumerable]]: false,
     [[Configurable]]: true } unless otherwise specified.
-features: [Symbol.iterator]
+features: [iterator-helpers]
 includes: [propertyHelper.js]
 ---*/
-
 verifyProperty(Iterator.prototype, Symbol.toStringTag, {
   value: 'Iterator',
   writable: true,
