28986b63ad1740d593852f951d0c2068d846bc91
Ben Allen <benallen@igalia.com>
2023-08-21 17:43:08 -0700

    added tests for valid region type values

---
+++ test/intl402/DisplayNames/prototype/of/type-region-valid.js
@@ -0,0 +1,20 @@
+// Copyright 2023 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-Intl.DisplayNames.prototype.of
+description: Returns string value for valid `region` codes
+info: |
+  12.3.3 Intl.DisplayNames.prototype.of ( code )
+
+  ...
+  2. If type is "region", then
+    a. If code cannot be matched by the unicode_region_subtag Unicode locale nonterminal, throw a RangeError exception.
+    b. Return the ASCII-uppercase of code.
+features: [Intl.DisplayNames]
+---*/
+
+var displayNames = new Intl.DisplayNames(undefined, {type: 'region'});
+
+assert.sameValue(typeof displayNames.of('aa'), 'string', 'aa');
+assert.sameValue(typeof displayNames.of('AA'), 'string', 'ZZ');
+assert.sameValue(typeof displayNames.of('000'), 'string', '000');
