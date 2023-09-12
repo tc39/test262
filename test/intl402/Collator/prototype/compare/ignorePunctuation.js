30356dac2bf71e0759076c384751e16178a63bae
Frank Tang <ftang@chromium.org>
2023-09-12 16:35:02 -0700

    Add locale tag, swap actual/expected position and add space

--- test/intl402/Collator/prototype/compare/ignorePunctuation.js
+++ test/intl402/Collator/prototype/compare/ignorePunctuation.js
@@ -3,14 +3,15 @@
 /*---
 esid: sec-collator-comparestrings
 description: test CompareStrings sync with resolvedOptions().ignorePunctuation.
+locale: [en, th, ja]
 ---*/
 // test on three locales, 'th' has different default.
-['en','th','ja'].forEach((locale) => {
+['en', 'th', 'ja'].forEach((locale) => {
   [undefined, true, false].forEach((ignorePunctuation) => {
     let col = new Intl.Collator(locale, {ignorePunctuation});
     // if ignorePunctuation is true, the comparison will be 0
     let expected = col.resolvedOptions().ignorePunctuation ? 0 : -1;
-    assert.sameValue(expected, col.compare("", " "));
-    assert.sameValue(expected, col.compare("", "*"));
+    assert.sameValue(col.compare("", " "), expected, "Compare to space");
+    assert.sameValue(col.compare("", "*"), expected, "Compare to star");
   });
 });
--- test/intl402/Collator/prototype/resolvedOptions/ignorePunctuation-not-default.js
+++ test/intl402/Collator/prototype/resolvedOptions/ignorePunctuation-not-default.js
@@ -3,12 +3,13 @@
 /*---
 esid: sec-initializecollator
 description: resolved ignorePunctuation is the same as the one specified in option bag.
+locale: [en, th, ja]
 ---*/
-['en','th','ja'].forEach((locale) => {
+['en', 'th', 'ja'].forEach((locale) => {
   [true, false].forEach((ignorePunctuation) => {
     assert.sameValue(
-    ignorePunctuation,
-    (new Intl.Collator(locale, {ignorePunctuation}))
-        .resolvedOptions().ignorePunctuation);
+      (new Intl.Collator(locale, {ignorePunctuation}))
+          .resolvedOptions().ignorePunctuation,
+      ignorePunctuation);
   });
 });
