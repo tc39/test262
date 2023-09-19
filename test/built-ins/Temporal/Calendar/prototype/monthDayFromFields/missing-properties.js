892a5dccd2a59a63f3a363db07f53bf896912699
Philip Chimento <pchimento@igalia.com>
2023-09-19 11:13:44 -0700

    Temporal: Use propertyBagObserver in monthDayFromFields test

--- test/built-ins/Temporal/Calendar/prototype/monthDayFromFields/missing-properties.js
+++ test/built-ins/Temporal/Calendar/prototype/monthDayFromFields/missing-properties.js
@@ -24,20 +24,13 @@ const missingDay = {
 assert.throws(TypeError, () => instance.monthDayFromFields(missingDay), "day should be checked before year and month");
 
 let got = [];
-const fieldsSpy = {
-  get year() {
-    got.push("year");
-  },
-  get month() {
-    got.push("month");
-  },
-  get monthCode() {
-    got.push("monthCode");
-  },
-  get day() {
-    got.push("day");
-    return 1;
-  },
-};
+const fieldsSpy = TemporalHelpers.propertyBagObserver(got, { day: 1 });
 assert.throws(TypeError, () => instance.monthDayFromFields(fieldsSpy), "incomplete fields should be rejected (but after reading all non-required fields)");
-assert.compareArray(got, ["day", "month", "monthCode", "year"], "fields should be read in alphabetical order");
+assert.compareArray(got, [
+  "get day",
+  "get day.valueOf",
+  "call day.valueOf",
+  "get month",
+  "get monthCode",
+  "get year",
+], "fields should be read in alphabetical order");
