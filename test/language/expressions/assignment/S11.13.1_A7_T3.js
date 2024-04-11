c6785d160eb055a1d3b3f42dc371196af1dd4c6e
Ross Kirsling <rkirsling@gmail.com>
2024-04-11 14:47:36 +0900

    Update test for o[p] = f()
    Update S11.13.1_A7_T3.js now that consensus has been reached on https://github.com/tc39/ecma262/pull/3307.

--- test/language/expressions/assignment/S11.13.1_A7_T3.js
+++ test/language/expressions/assignment/S11.13.1_A7_T3.js
@@ -1,25 +1,38 @@
 // Copyright (C) 2015 André Bargull. All rights reserved.
+// Copyright (C) 2024 Sony Interactive Entertainment Inc. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
 info: Assignment Operator evaluates its operands from left to right.
 description: >
     The left-hand side expression is evaluated before the right-hand side.
-    Left-hand side expression is MemberExpression: base[prop]. Evaluating
-    ToPropertyKey(prop) throws an error.
+    Left-hand side expression is MemberExpression: base[prop].
+    ToPropertyKey(prop) occurs after both sides are evaluated.
 ---*/
 
 function DummyError() { }
 
 assert.throws(DummyError, function() {
   var base = {};
-  var prop = {
-    toString: function() {
-      throw new DummyError();
-    }
+  var prop = function() {
+    throw new DummyError();
   };
   var expr = function() {
     throw new Test262Error("right-hand side expression evaluated");
+  };
+
+  base[prop()] = expr();
+});
+
+assert.throws(DummyError, function() {
+  var base = {};
+  var prop = {
+    toString: function() {
+      throw new Test262Error("property key evaluated");
+    }
+  };
+  var expr = function() {
+    throw new DummyError();
   };
 
   base[prop] = expr();
