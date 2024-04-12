a48113af0e0a47839fdc7c886558bbe0b2d319ae
Ross Kirsling <ross.kirsling@sony.com>
2024-04-12 14:20:15 +0900

    Rename test and add an analogous one for super.

--- test/language/expressions/assignment/target-member-computed-reference.js
+++ test/language/expressions/assignment/target-member-computed-reference.js
@@ -3,11 +3,12 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-info: Assignment Operator evaluates its operands from left to right.
-description: >
-    The left-hand side expression is evaluated before the right-hand side.
-    Left-hand side expression is MemberExpression: base[prop].
-    ToPropertyKey(prop) occurs after both sides are evaluated.
+esid: sec-assignment-operators
+description: Assignment Operator evaluates its operands from left to right (formerly S11.13.1_A7_T3)
+info: |
+  The left-hand side expression is evaluated before the right-hand side.
+  Left-hand side expression is MemberExpression: base[prop].
+  ToPropertyKey(prop) occurs after both sides are evaluated.
 ---*/
 
 function DummyError() { }
