b118a031bec202ae68f5adfcd50abcb6edcc04dc
Ms2ger <Ms2ger@igalia.com>
2024-05-06 14:47:56 +0200

    Import tests from SpiderMonkey

---
+++ test/staging/sm/AsyncIterator/prototype/filter/name.js
@@ -0,0 +1,24 @@
+// Copyright (C) 2024 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: pending
+description: |
+  %AsyncIterator.prototype%.filter.name value and descriptor.
+info: |
+  17 ECMAScript Standard Built-in Objects
+features:
+- async-iteration
+- iterator-helpers
+includes: [sm/non262-shell.js, sm/non262.js]
+flags:
+- noStrict
+---*/
+assert.sameValue(AsyncIterator.prototype.filter.name, 'filter');
+
+const propertyDescriptor = Reflect.getOwnPropertyDescriptor(AsyncIterator.prototype.filter, 'name');
+assert.sameValue(propertyDescriptor.value, 'filter');
+assert.sameValue(propertyDescriptor.enumerable, false);
+assert.sameValue(propertyDescriptor.writable, false);
+assert.sameValue(propertyDescriptor.configurable, true);
+
