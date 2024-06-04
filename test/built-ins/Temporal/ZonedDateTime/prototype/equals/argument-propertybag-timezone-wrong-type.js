6b0e6faa77cc54fdae250de1f3ba548e41e07cde
Philip Chimento <pchimento@igalia.com>
2024-06-04 12:38:24 +0200

    Temporal: Remove Calendar and TimeZone objects from "wrong-type" tests
    Test the "wrong type of object" with Duration objects where appropriate,
    otherwise just use plain objects.

--- test/built-ins/Temporal/ZonedDateTime/prototype/equals/argument-propertybag-timezone-wrong-type.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/equals/argument-propertybag-timezone-wrong-type.js
@@ -30,8 +30,8 @@ for (const [timeZone, description] of primitiveTests) {
 
 const typeErrorTests = [
   [Symbol(), "symbol"],
-  [{}, "object not implementing time zone protocol"],
-  [new Temporal.Calendar("iso8601"), "calendar instance"],
+  [{}, "object"],
+  [new Temporal.Duration(), "duration instance"],
 ];
 
 for (const [timeZone, description] of typeErrorTests) {
