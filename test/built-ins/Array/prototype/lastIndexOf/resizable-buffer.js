c197b54041304d2803cca93554e3bfb437818d79
Ioanna M. Dimitriou H <idimitriou@igalia.com>
2024-06-29 01:41:48 +0200

    Import relevant files from #3888

---
+++ test/built-ins/Array/prototype/lastIndexOf/resizable-buffer.js
@@ -0,0 +1,177 @@
+// Copyright 2023 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-array.prototype.lastindexof
+description: >
+  Array.p.lastIndexOf behaves correctly when receiver is backed by resizable
+  buffer
+features: [resizable-arraybuffer]
+---*/
+
+class MyUint8Array extends Uint8Array {
+}
+
+class MyFloat32Array extends Float32Array {
+}
+
+class MyBigInt64Array extends BigInt64Array {
+}
+
+const builtinCtors = [
+  Uint8Array,
+  Int8Array,
+  Uint16Array,
+  Int16Array,
+  Uint32Array,
+  Int32Array,
+  Float32Array,
+  Float64Array,
+  Uint8ClampedArray,
+  BigUint64Array,
+  BigInt64Array
+];
+
+const ctors = [
+  ...builtinCtors,
+  MyUint8Array,
+  MyFloat32Array,
+  MyBigInt64Array
+];
+
+function CreateResizableArrayBuffer(byteLength, maxByteLength) {
+  return new ArrayBuffer(byteLength, { maxByteLength: maxByteLength });
+}
+
+function WriteToTypedArray(array, index, value) {
+  if (array instanceof BigInt64Array || array instanceof BigUint64Array) {
+    array[index] = BigInt(value);
+  } else {
+    array[index] = value;
+  }
+}
+function ArrayLastIndexOfHelper(ta, n, fromIndex) {
+  if (typeof n == 'number' && (ta instanceof BigInt64Array || ta instanceof BigUint64Array)) {
+    if (fromIndex == undefined) {
+      return Array.prototype.lastIndexOf.call(ta, BigInt(n));
+    }
+    return Array.prototype.lastIndexOf.call(ta, BigInt(n), fromIndex);
+  }
+  if (fromIndex == undefined) {
+    return Array.prototype.lastIndexOf.call(ta, n);
+  }
+  return Array.prototype.lastIndexOf.call(ta, n, fromIndex);
+}
+
+function TestLastIndexOf() {
+  for (let ctor of ctors) {
+    const rab = CreateResizableArrayBuffer(4 * ctor.BYTES_PER_ELEMENT, 8 * ctor.BYTES_PER_ELEMENT);
+    const fixedLength = new ctor(rab, 0, 4);
+    const fixedLengthWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT, 2);
+    const lengthTracking = new ctor(rab, 0);
+    const lengthTrackingWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT);
+
+    // Write some data into the array.
+    const taWrite = new ctor(rab);
+    for (let i = 0; i < 4; ++i) {
+      WriteToTypedArray(taWrite, i, Math.floor(i / 2));
+    }
+
+    // Orig. array: [0, 0, 1, 1]
+    //              [0, 0, 1, 1] << fixedLength
+    //                    [1, 1] << fixedLengthWithOffset
+    //              [0, 0, 1, 1, ...] << lengthTracking
+    //                    [1, 1, ...] << lengthTrackingWithOffset
+
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0, 2), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0, -2), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0, -3), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 1, 1), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 1, -2), 2);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 1, -3), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 1, -2), 0);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 1, -1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0, 2), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0, -3), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 1, 1), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 1, 2), 2);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 1, -3), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1, -2), 0);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1, -1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, undefined), -1);
+
+    // Shrink so that fixed length TAs go out of bounds.
+    rab.resize(3 * ctor.BYTES_PER_ELEMENT);
+
+    // Orig. array: [0, 0, 1]
+    //              [0, 0, 1, ...] << lengthTracking
+    //                    [1, ...] << lengthTrackingWithOffset
+
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 1), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 1), -1);
+
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1), 0);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, undefined), -1);
+
+    // Shrink so that the TAs with offset go out of bounds.
+    rab.resize(1 * ctor.BYTES_PER_ELEMENT);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 0), -1);
+
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0), 0);
+
+    // Shrink to zero.
+    rab.resize(0);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 0), -1);
+
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, undefined), -1);
+
+    // Grow so that all TAs are back in-bounds.
+    rab.resize(6 * ctor.BYTES_PER_ELEMENT);
+    for (let i = 0; i < 6; ++i) {
+      WriteToTypedArray(taWrite, i, Math.floor(i / 2));
+    }
+
+    // Orig. array: [0, 0, 1, 1, 2, 2]
+    //              [0, 0, 1, 1] << fixedLength
+    //                    [1, 1] << fixedLengthWithOffset
+    //              [0, 0, 1, 1, 2, 2, ...] << lengthTracking
+    //                    [1, 1, 2, 2, ...] << lengthTrackingWithOffset
+
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 1), 3);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, 2), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLength, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, 2), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(fixedLengthWithOffset, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 1), 3);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, 2), 5);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTracking, undefined), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 0), -1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 1), 1);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, 2), 3);
+    assert.sameValue(ArrayLastIndexOfHelper(lengthTrackingWithOffset, undefined), -1);
+  }
+}
+
+TestLastIndexOf();
+
---
+++ test/built-ins/TypedArray/prototype/lastIndexOf/resizable-buffer-special-float-values.js
@@ -0,0 +1,38 @@
+// Copyright 2023 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-%typedarray%.prototype.lastindexof
+description: >
+  TypedArray.p.lastIndexOf behaves correctly for special float values when
+  receiver is a float TypedArray backed by a resizable buffer
+features: [resizable-arraybuffer, Array.prototype.includes]
+---*/
+
+class MyFloat32Array extends Float32Array {
+}
+
+const floatCtors = [
+  Float32Array,
+  Float64Array,
+  MyFloat32Array
+];
+
+function CreateResizableArrayBuffer(byteLength, maxByteLength) {
+  return new ArrayBuffer(byteLength, { maxByteLength: maxByteLength });
+}
+
+for (let ctor of floatCtors) {
+  const rab = CreateResizableArrayBuffer(4 * ctor.BYTES_PER_ELEMENT, 8 * ctor.BYTES_PER_ELEMENT);
+  const lengthTracking = new ctor(rab);
+  lengthTracking[0] = -Infinity;
+  lengthTracking[1] = -Infinity;
+  lengthTracking[2] = Infinity;
+  lengthTracking[3] = Infinity;
+  lengthTracking[4] = NaN;
+  lengthTracking[5] = NaN;
+  assert.sameValue(lengthTracking.lastIndexOf(-Infinity), 1);
+  assert.sameValue(lengthTracking.lastIndexOf(Infinity), 3);
+  // NaN is never found.
+  assert.sameValue(lengthTracking.lastIndexOf(NaN), -1);
+}
