f0956d81e468b6821980032d8cb728afe23ccff0
Ioanna M. Dimitriou H <idimitriou@igalia.com>
2024-06-29 01:40:39 +0200

    Removing parts in resizableArrayBufferUtils.js and adding it in includes, while applying review changes from PRs for previously tested methods.

--- test/built-ins/TypedArray/prototype/includes/resizable-buffer.js
+++ test/built-ins/TypedArray/prototype/includes/resizable-buffer.js
@@ -6,178 +6,133 @@ esid: sec-%typedarray%.prototype.includes
 description: >
   TypedArray.p.includes behaves correctly when receiver is backed by resizable
   buffer
+includes: [resizableArrayBufferUtils.js]
 features: [resizable-arraybuffer, Array.prototype.includes]
 ---*/
 
-class MyUint8Array extends Uint8Array {
-}
-
-class MyFloat32Array extends Float32Array {
-}
-
-class MyBigInt64Array extends BigInt64Array {
-}
-
-const builtinCtors = [
-  Uint8Array,
-  Int8Array,
-  Uint16Array,
-  Int16Array,
-  Uint32Array,
-  Int32Array,
-  Float32Array,
-  Float64Array,
-  Uint8ClampedArray,
-  BigUint64Array,
-  BigInt64Array
-];
-
-const ctors = [
-  ...builtinCtors,
-  MyUint8Array,
-  MyFloat32Array,
-  MyBigInt64Array
-];
-
-function CreateResizableArrayBuffer(byteLength, maxByteLength) {
-  return new ArrayBuffer(byteLength, { maxByteLength: maxByteLength });
-}
-
-function WriteToTypedArray(array, index, value) {
-  if (array instanceof BigInt64Array || array instanceof BigUint64Array) {
-    array[index] = BigInt(value);
-  } else {
-    array[index] = value;
-  }
-}
-
-function TypedArrayIncludesHelper(array, n, fromIndex) {
+function TypedArrayIncludesNumOrBigInt(array, n, fromIndex) {
   if (typeof n == 'number' && (array instanceof BigInt64Array || array instanceof BigUint64Array)) {
     return array.includes(BigInt(n), fromIndex);
   }
   return array.includes(n, fromIndex);
 }
 
-function TestIncludes() {
-  for (let ctor of ctors) {
-    const rab = CreateResizableArrayBuffer(4 * ctor.BYTES_PER_ELEMENT, 8 * ctor.BYTES_PER_ELEMENT);
-    const fixedLength = new ctor(rab, 0, 4);
-    const fixedLengthWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT, 2);
-    const lengthTracking = new ctor(rab, 0);
-    const lengthTrackingWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT);
+for (let ctor of ctors) {
+  const rab = CreateResizableArrayBuffer(4 * ctor.BYTES_PER_ELEMENT, 8 * ctor.BYTES_PER_ELEMENT);
+  const fixedLength = new ctor(rab, 0, 4);
+  const fixedLengthWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT, 2);
+  const lengthTracking = new ctor(rab, 0);
+  const lengthTrackingWithOffset = new ctor(rab, 2 * ctor.BYTES_PER_ELEMENT);
 
-    // Write some data into the array.
-    const taWrite = new ctor(rab);
-    for (let i = 0; i < 4; ++i) {
-      WriteToTypedArray(taWrite, i, 2 * i);
-    }
-
-    // Orig. array: [0, 2, 4, 6]
-    //              [0, 2, 4, 6] << fixedLength
-    //                    [4, 6] << fixedLengthWithOffset
-    //              [0, 2, 4, 6, ...] << lengthTracking
-    //                    [4, 6, ...] << lengthTrackingWithOffset
-
-    assert(TypedArrayIncludesHelper(fixedLength, 2));
-    assert(!TypedArrayIncludesHelper(fixedLength, undefined));
-    assert(TypedArrayIncludesHelper(fixedLength, 2, 1));
-    assert(!TypedArrayIncludesHelper(fixedLength, 2, 2));
-    assert(TypedArrayIncludesHelper(fixedLength, 2, -3));
-    assert(!TypedArrayIncludesHelper(fixedLength, 2, -2));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, 2));
-    assert(TypedArrayIncludesHelper(fixedLengthWithOffset, 4));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, undefined));
-    assert(TypedArrayIncludesHelper(fixedLengthWithOffset, 4, 0));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, 4, 1));
-    assert(TypedArrayIncludesHelper(fixedLengthWithOffset, 4, -2));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, 4, -1));
-    assert(TypedArrayIncludesHelper(lengthTracking, 2));
-    assert(!TypedArrayIncludesHelper(lengthTracking, undefined));
-    assert(TypedArrayIncludesHelper(lengthTracking, 2, 1));
-    assert(!TypedArrayIncludesHelper(lengthTracking, 2, 2));
-    assert(TypedArrayIncludesHelper(lengthTracking, 2, -3));
-    assert(!TypedArrayIncludesHelper(lengthTracking, 2, -2));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, 2));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 4));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, undefined));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 4, 0));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, 4, 1));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 4, -2));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, 4, -1));
-
-    // Shrink so that fixed length TAs go out of bounds.
-    rab.resize(3 * ctor.BYTES_PER_ELEMENT);
-
-    // Orig. array: [0, 2, 4]
-    //              [0, 2, 4, ...] << lengthTracking
-    //                    [4, ...] << lengthTrackingWithOffset
-
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLength, 2);
-    });
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLengthWithOffset, 2);
-    });
-
-    assert(TypedArrayIncludesHelper(lengthTracking, 2));
-    assert(!TypedArrayIncludesHelper(lengthTracking, undefined));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, 2));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 4));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, undefined));
-
-    // Shrink so that the TAs with offset go out of bounds.
-    rab.resize(1 * ctor.BYTES_PER_ELEMENT);
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLength, 2);
-    });
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLengthWithOffset, 2);
-    });
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(lengthTrackingWithOffset, 2);
-    });
-
-    // Shrink to zero.
-    rab.resize(0);
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLength, 2);
-    });
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(fixedLengthWithOffset, 2);
-    });
-    assert.throws(TypeError, () => {
-      TypedArrayIncludesHelper(lengthTrackingWithOffset, 2);
-    });
-    assert(!TypedArrayIncludesHelper(lengthTracking, 2));
-    assert(!TypedArrayIncludesHelper(lengthTracking, undefined));
-
-    // Grow so that all TAs are back in-bounds.
-    rab.resize(6 * ctor.BYTES_PER_ELEMENT);
-    for (let i = 0; i < 6; ++i) {
-      WriteToTypedArray(taWrite, i, 2 * i);
-    }
-
-    // Orig. array: [0, 2, 4, 6, 8, 10]
-    //              [0, 2, 4, 6] << fixedLength
-    //                    [4, 6] << fixedLengthWithOffset
-    //              [0, 2, 4, 6, 8, 10, ...] << lengthTracking
-    //                    [4, 6, 8, 10, ...] << lengthTrackingWithOffset
-
-    assert(TypedArrayIncludesHelper(fixedLength, 2));
-    assert(!TypedArrayIncludesHelper(fixedLength, undefined));
-    assert(!TypedArrayIncludesHelper(fixedLength, 8));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, 2));
-    assert(TypedArrayIncludesHelper(fixedLengthWithOffset, 4));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, undefined));
-    assert(!TypedArrayIncludesHelper(fixedLengthWithOffset, 8));
-    assert(TypedArrayIncludesHelper(lengthTracking, 2));
-    assert(!TypedArrayIncludesHelper(lengthTracking, undefined));
-    assert(TypedArrayIncludesHelper(lengthTracking, 8));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, 2));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 4));
-    assert(!TypedArrayIncludesHelper(lengthTrackingWithOffset, undefined));
-    assert(TypedArrayIncludesHelper(lengthTrackingWithOffset, 8));
+  // Write some data into the array.
+  const taWrite = new ctor(rab);
+  for (let i = 0; i < 4; ++i) {
+    WriteToTypedArray(taWrite, i, 2 * i);
   }
-}
 
-TestIncludes();
+  // Orig. array: [0, 2, 4, 6]
+  //              [0, 2, 4, 6] << fixedLength
+  //                    [4, 6] << fixedLengthWithOffset
+  //              [0, 2, 4, 6, ...] << lengthTracking
+  //                    [4, 6, ...] << lengthTrackingWithOffset
+
+  assert(TypedArrayIncludesNumOrBigInt(fixedLength, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLength, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLength, 2, 1));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLength, 2, 2));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLength, 2, -3));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLength, 2, -2));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 2));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4, 0));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4, 1));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4, -2));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4, -1));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 2, 1));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, 2, 2));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 2, -3));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, 2, -2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 2));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4, 0));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4, 1));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4, -2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4, -1));
+
+  // Shrink so that fixed length TAs go out of bounds.
+  rab.resize(3 * ctor.BYTES_PER_ELEMENT);
+
+  // Orig. array: [0, 2, 4]
+  //              [0, 2, 4, ...] << lengthTracking
+  //                    [4, ...] << lengthTrackingWithOffset
+
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLength, 2);
+  });
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 2);
+  });
+
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, undefined));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 2));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, undefined));
+
+  // Shrink so that the TAs with offset go out of bounds.
+  rab.resize(1 * ctor.BYTES_PER_ELEMENT);
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLength, 2);
+  });
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 2);
+  });
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 2);
+  });
+
+  // Shrink to zero.
+  rab.resize(0);
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLength, 2);
+  });
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 2);
+  });
+  assert.throws(TypeError, () => {
+    TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 2);
+  });
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, undefined));
+
+  // Grow so that all TAs are back in-bounds.
+  rab.resize(6 * ctor.BYTES_PER_ELEMENT);
+  for (let i = 0; i < 6; ++i) {
+    WriteToTypedArray(taWrite, i, 2 * i);
+  }
+
+  // Orig. array: [0, 2, 4, 6, 8, 10]
+  //              [0, 2, 4, 6] << fixedLength
+  //                    [4, 6] << fixedLengthWithOffset
+  //              [0, 2, 4, 6, 8, 10, ...] << lengthTracking
+  //                    [4, 6, 8, 10, ...] << lengthTrackingWithOffset
+
+  assert(TypedArrayIncludesNumOrBigInt(fixedLength, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLength, undefined));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLength, 8));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 2));
+  assert(TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 4));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, undefined));
+  assert(!TypedArrayIncludesNumOrBigInt(fixedLengthWithOffset, 8));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 2));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTracking, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTracking, 8));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 2));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 4));
+  assert(!TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, undefined));
+  assert(TypedArrayIncludesNumOrBigInt(lengthTrackingWithOffset, 8));
+}
