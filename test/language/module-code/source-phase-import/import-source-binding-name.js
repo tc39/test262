45af4b8b601d791e830d84fac806fe2ee70ae51a
Nicolò Ribaudo <nribaudo@igalia.com>
2024-06-28 15:08:35 +0200

    Update import-source-binding-name.js

--- test/language/module-code/source-phase-import/import-source-binding-name.js
+++ test/language/module-code/source-phase-import/import-source-binding-name.js
@@ -19,5 +19,5 @@ $DONOTEVALUATE();
 
 import "../resources/ensure-linking-error_FIXTURE.js";
 
-import source from from '<do not resolve>';
+import source from '<do not resolve>';
 import from from '<do not resolve>';
