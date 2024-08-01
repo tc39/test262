b4a8454794925d7418820c8708b576f7b77c73b5
Lucas Mirelmann <lgalfaso@gmail.com>
2024-08-01 11:03:53 +0200

    Fix typo
    Fix typo in an ObjectLiteral

--- test/built-ins/AsyncDisposableStack/prototype/disposed/length.js
+++ test/built-ins/AsyncDisposableStack/prototype/disposed/length.js
@@ -15,7 +15,7 @@ features: [explicit-resource-management]
 var descriptor = Object.getOwnPropertyDescriptor(AsyncDisposableStack.prototype, 'disposed');
 
 verifyProperty(descriptor.get, 'length', {
-  value: 0;
+  value: 0,
   enumerable: false,
   writable: false,
   configurable: true
