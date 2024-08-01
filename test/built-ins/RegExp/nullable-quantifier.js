708ba3255df470891826a30f143aed4e24a04f5a
Aurèle <aurele.barriere@gmail.com>
2024-08-01 15:16:43 +0200

    Update nullable-quantifier.js

--- test/built-ins/RegExp/nullable-quantifier.js
+++ test/built-ins/RegExp/nullable-quantifier.js
@@ -3,7 +3,7 @@
 
 /*---
 esid: sec-runtime-semantics-repeatmatcher-abstract-operation
-description: JavaScript nullable quantifiers have special semantics: optional iterations are not allowed to match the empty string. Point 2.b below shows that after each optional (min=0) iteration of a quantifier, if the iteration matched the empty string (y.[[EndIndex]] = x.[[EndIndex]]), then the iteration is discarded. In particular, for (a?b??)* on "ab", it is possible to do two iterations of the star, one matching "a" and the other matching "b".
+description: JavaScript nullable quantifiers have special semantics, optional iterations are not allowed to match the empty string. Point 2.b below shows that after each optional (min=0) iteration of a quantifier, if the iteration matched the empty string (y.[[EndIndex]] = x.[[EndIndex]]), then the iteration is discarded. In particular, for (a?b??)* on "ab", it is possible to do two iterations of the star, one matching "a" and the other matching "b".
 info: |
     RepeatMatcher ( m, min, max, greedy, x, c, parenIndex, parenCount )
 
