954d1809f831de8fd5749b6baab4cc03b6d9a5ab
Ioanna M. Dimitriou H <idimitriou@igalia.com>
2024-10-09 21:23:13 +0200

    Boilerplate tests for rawJSON and isRawJSON from the proposal json-parse-with-source

---
+++ test/built-ins/JSON/rawJSON/length.js
@@ -0,0 +1,25 @@
+// Copyright (C) 2024 Igalia S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-json.rawjson
+description: >
+  JSON.rawJSON.length value and descriptor.
+info: |
+  JSON.rawJSON ( text )
+
+  18 ECMAScript Standard Built-in Objects
+  Every other data property described in clauses 19 through 28 and in Annex B.2
+  has the attributes { [[Writable]]: true, [[Enumerable]]: false,
+  [[Configurable]]: true } unless otherwise specified.
+
+includes: [propertyHelper.js]
+features: [json-parse-with-source]
+---*/
+
+verifyProperty(JSON.rawJSON, 'length', {
+  value: 1,
+  writable: false,
+  enumerable: false,
+  configurable: true
+});
