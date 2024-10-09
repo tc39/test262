e885838b06e060ae96c8e6087bdeb664b0423ea3
Richard Gibson <richard.gibson@gmail.com>
2024-10-09 02:43:53 -0400

    harness/deepEqual.js: Improve some formatting

--- harness/deepEqual.js
+++ harness/deepEqual.js
@@ -19,18 +19,19 @@ assert.deepEqual.format = (function () {
     switch (typeof value) {
       case 'string':
         return typeof JSON !== "undefined" ? JSON.stringify(value) : `"${value}"`;
-      case 'number':
+      case 'bigint':
+        return `${value}n`;
       case 'boolean':
       case 'symbol':
-      case 'bigint':
-        return value.toString();
       case 'undefined':
-        return 'undefined';
+      case 'number':
+        return value === 0 && 1 / value === -Infinity ? '-0' : String(value);
       case 'function':
-        return `[Function${value.name ? `: ${value.name}` : ''}]`;
+        return `function${value.name ? ` ${String(value.name)}` : ''}`;
       case 'object':
         if (value === null) return 'null';
-        if (value instanceof Date) return `Date "${value.toISOString()}"`;
+        if (value instanceof Date) return `Date("${value.toISOString()}")`;
+        if (value instanceof Error) return `error ${value.name || 'Error'}("${value.message}")`;
         if (value instanceof RegExp) return value.toString();
         if (!seen) {
           seen = {
@@ -42,7 +43,7 @@ assert.deepEqual.format = (function () {
         let usage = seen.map.get(value);
         if (usage) {
           usage.used = true;
-          return `[Ref: #${usage.id}]`;
+          return `ref #${usage.id}`;
         }
 
         usage = { id: ++seen.counter, used: false };
@@ -52,16 +53,18 @@ assert.deepEqual.format = (function () {
           return `Set {${Array.from(value).map(value => assert.deepEqual.format(value, seen)).join(', ')}}${usage.used ? ` as #${usage.id}` : ''}`;
         }
         if (typeof Map !== "undefined" && value instanceof Map) {
-          return `Map {${Array.from(value).map(pair => `${assert.deepEqual.format(pair[0], seen)} => ${assert.deepEqual.format(pair[1], seen)}}`).join(', ')}}${usage.used ? ` as #${usage.id}` : ''}`;
+          return `Map {${Array.from(value).map(pair => `${assert.deepEqual.format(pair[0], seen)} => ${assert.deepEqual.format(pair[1], seen)}`).join(', ')}}${usage.used ? ` as #${usage.id}` : ''}`;
         }
         if (Array.isArray ? Array.isArray(value) : value instanceof Array) {
           return `[${value.map(value => assert.deepEqual.format(value, seen)).join(', ')}]${usage.used ? ` as #${usage.id}` : ''}`;
         }
-        let tag = Symbol.toStringTag in value ? value[Symbol.toStringTag] : 'Object';
+        let tag = Symbol.toStringTag && Symbol.toStringTag in value
+          ? value[Symbol.toStringTag]
+          : 'Object';
         if (tag === 'Object' && Object.getPrototypeOf(value) === null) {
           tag = '[Object: null prototype]';
         }
-        return `${tag ? `${tag} ` : ''}{ ${Object.keys(value).map(key => `${key.toString()}: ${assert.deepEqual.format(value[key], seen)}`).join(', ')} }${usage.used ? ` as #${usage.id}` : ''}`;
+        return `${tag ? `${tag} ` : ''}{${Object.keys(value).map(key => `${key.toString()}: ${assert.deepEqual.format(value[key], seen)}`).join(', ')}}${usage.used ? ` as #${usage.id}` : ''}`;
       default:
         return typeof value;
     }
