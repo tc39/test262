5d8348bfdc350826c85172c07cdc4fbfd89368ca
Ioanna M. Dimitriou H <idimitriou@igalia.com>
2024-10-14 21:39:39 +0200

    Address review comments

--- test/built-ins/JSON/rawJSON/length.js
+++ test/built-ins/JSON/rawJSON/length.js
@@ -2,16 +2,19 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-json.rawjson
+esid: sec-built-in-function-objects
 description: >
   JSON.rawJSON.length value and descriptor.
 info: |
-  JSON.rawJSON ( text )
+    Every built-in function object, including constructors, has a *"length"*
+    property whose value is a non-negative integral Number. Unless otherwise
+    specified, this value is the number of required parameters shown in the
+    subclause heading for the function description. Optional parameters and rest
+    parameters are not included in the parameter count.
 
-  18 ECMAScript Standard Built-in Objects
-  Every other data property described in clauses 19 through 28 and in Annex B.2
-  has the attributes { [[Writable]]: true, [[Enumerable]]: false,
-  [[Configurable]]: true } unless otherwise specified.
+    Unless otherwise specified, the *"length"* property of a built-in function
+    object has the attributes { [[Writable]]: *false*, [[Enumerable]]: *false*,
+    [[Configurable]]: *true* }.
 
 includes: [propertyHelper.js]
 features: [json-parse-with-source]
