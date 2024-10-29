a93fe76f52c59086a3652b893026efde81fbf336
Tim Chevalier <tjc@igalia.com>
2024-10-29 13:50:50 -0700

    Temporal: Move ZonedDateTime property bag tests out of staging

---
+++ test/built-ins/Temporal/ZonedDateTime/from/argument-propertybag-required-correctly-spelled-properties.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.from
+description: Options must contain at least the required correctly-spelled properties.
+features: [Temporal]
+---*/
+
+// object must contain at least the required correctly-spelled properties
+assert.throws(TypeError, () => Temporal.ZonedDateTime.from({
+  years: 1976,
+  months: 11,
+  days: 18,
+  timeZone: "+01:00"
+}));
