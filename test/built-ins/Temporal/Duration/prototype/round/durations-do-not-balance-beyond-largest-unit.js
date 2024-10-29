b4431cc8764d0bbd4ead538110d44aafc677ca4c
Tim Chevalier <tjc@igalia.com>
2024-10-28 17:34:47 -0700

    Add relativeTo option in test

--- test/built-ins/Temporal/Duration/prototype/round/durations-do-not-balance-beyond-largest-unit.js
+++ test/built-ins/Temporal/Duration/prototype/round/durations-do-not-balance-beyond-largest-unit.js
@@ -9,6 +9,14 @@ features: [Temporal]
 ---*/
 
 const fortyDays = new Temporal.Duration(0, 0, 0, 40, 0, 0, 0, 0, 0, 0);
+const one85Minutes = new Temporal.Duration(0, 0, 0, 0, 185, 0, 0, 0, 0, 0);
+const relativeTo = new Temporal.PlainDate(2020, 1, 1);
 
-TemporalHelpers.assertDuration(fortyDays.round({ smallestUnit: "seconds" }),
-                               0, 0, 0, 40, 0, 0, 0, 0, 0, 0);
+TemporalHelpers.assertDuration(fortyDays.round({
+    smallestUnit: "seconds",
+    relativeTo
+}), 0, 0, 0, 40, 0, 0, 0, 0, 0, 0);
+
+TemporalHelpers.assertDuration(one85Minutes.round({
+    smallestUnit: "seconds",
+}), 0, 0, 0, 0, 185, 0, 0, 0, 0, 0);
