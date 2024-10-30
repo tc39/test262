5505758111a55f09a19863cacb27a00ecf3a6241
Tim Chevalier <tjc@igalia.com>
2024-10-30 16:45:28 -0700

    Temporal: Fix Temporal/Duration/p/round tests (#4299)

--- test/built-ins/Temporal/Duration/prototype/round/rounding-relative-to-date.js
+++ test/built-ins/Temporal/Duration/prototype/round/rounding-relative-to-date.js
@@ -12,29 +12,29 @@ const days = new Temporal.Duration(0, 0, 0, 45, 0, 0, 0, 0, 0, 0);
 const yearAndHalf = new Temporal.Duration(0, 0, 0, 547, 12, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(days.round({
-    relativeTo: new Temporal.PlainDate(2019, 1, 1)
+    relativeTo: new Temporal.PlainDate(2019, 1, 1),
     smallestUnit: "months"}), 0, 2, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(days.negated().round({
-    relativeTo: new Temporal.PlainDate(2019, 2, 15)
+    relativeTo: new Temporal.PlainDate(2019, 2, 15),
     smallestUnit: "months"}), 0, -1, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(yearAndHalf.round({
-    relativeTo: new Temporal.PlainDate(2018, 1, 1)
+    relativeTo: new Temporal.PlainDate(2018, 1, 1),
     smallestUnit: "years"}), 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(yearAndHalf.round({
-    relativeTo: new Temporal.PlainDate(2018, 7, 1)
+    relativeTo: new Temporal.PlainDate(2018, 7, 1),
     smallestUnit: "years"}), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(yearAndHalf.round({
-    relativeTo: new Temporal.PlainDate(2019, 1, 1)
+    relativeTo: new Temporal.PlainDate(2019, 1, 1),
     smallestUnit: "years"}), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(yearAndHalf.round({
-    relativeTo: new Temporal.PlainDate(2020, 1, 1)
+    relativeTo: new Temporal.PlainDate(2020, 1, 1),
     smallestUnit: "years"}), 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 TemporalHelpers.assertDuration(yearAndHalf.round({
-    relativeTo: new Temporal.PlainDate(2020, 7, 1)
+    relativeTo: new Temporal.PlainDate(2020, 7, 1),
     smallestUnit: "years"}), 2, 0, 0, 0, 0, 0, 0, 0, 0, 0);
