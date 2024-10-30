cd3edc9b16285c3bed75318063d3212527860cc0
Tim Chevalier <tjc@igalia.com>
2024-10-30 11:53:50 -0700

    Update test/built-ins/Temporal/Instant/prototype/since/can-return-minutes-and-hours.js
    Co-authored-by: Philip Chimento <philip.chimento@gmail.com>

--- test/built-ins/Temporal/Instant/prototype/since/can-return-minutes-and-hours.js
+++ test/built-ins/Temporal/Instant/prototype/since/can-return-minutes-and-hours.js
@@ -12,7 +12,7 @@ const feb20 = Temporal.Instant.from("2020-02-01T00:00Z");
 const feb21 = Temporal.Instant.from("2021-02-01T00:00Z");
 
 // can return minutes and hours
-TemporalHelpers.assertDurationsEqual(feb21.since(feb20, { largestUnit: "hours" }),
-                                     new Temporal.Duration(0, 0, 0, 0, 8784, 0, 0, 0, 0, 0));
-TemporalHelpers.assertDurationsEqual(feb21.since(feb20, { largestUnit: "minutes" }),
-                                     new Temporal.Duration(0, 0, 0, 0, 0, 527040, 0, 0, 0, 0));
+TemporalHelpers.assertDuration(feb21.since(feb20, { largestUnit: "hours" }),
+                                     0, 0, 0, 0, 8784, 0, 0, 0, 0, 0);
+TemporalHelpers.assertDuration(feb21.since(feb20, { largestUnit: "minutes" }),
+                                     0, 0, 0, 0, 0, 527040, 0, 0, 0, 0);
