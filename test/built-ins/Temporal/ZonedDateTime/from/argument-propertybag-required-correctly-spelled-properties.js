0f7dd543fa79916117feea7b41da862d46482752
Tim Chevalier <tjc@igalia.com>
2024-10-30 17:09:04 -0700

    Fix test for required properties

--- test/built-ins/Temporal/ZonedDateTime/from/argument-propertybag-required-correctly-spelled-properties.js
+++ test/built-ins/Temporal/ZonedDateTime/from/argument-propertybag-required-correctly-spelled-properties.js
@@ -16,18 +16,18 @@ assert.throws(TypeError, () => Temporal.ZonedDateTime.from({
 }));
 assert.throws(TypeError, () => Temporal.ZonedDateTime.from({
   years: 1976,
-  months: 11,
+  month: 11,
   day: 18,
   timeZone: "+01:00"
 }));
 assert.throws(TypeError, () => Temporal.ZonedDateTime.from({
   year: 1976,
   months: 11,
-  days: 18,
+  day: 18,
   timeZone: "+01:00"
 }));
 assert.throws(TypeError, () => Temporal.ZonedDateTime.from({
-  years: 1976,
+  year: 1976,
   month: 11,
   days: 18,
   timeZone: "+01:00"
