0c84bdfde5fa4bb87b12275817275523d4d70aa1
Philip Chimento <pchimento@igalia.com>
2024-11-04 11:22:28 -0800

    Migrate linter workflow to GitHub Actions
    This runs faster and allows for future improvements.
    
    I'm following a general principle of keeping code that isn't portable
    between CI providers inside the config file for the CI provider. So in
    this case we remove the Circle-CI-specific stuff from the file in
    tools/scripts/, and into .github/workflows/. We use an external action
    (tj-actions/changed-files) to gather the list of files to lint.

--- tools/scripts/ci_lint.sh
+++ tools/scripts/ci_lint.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-if [ "$CIRCLE_PULL_REQUEST" != "" ]; then
-  paths=$(git diff --diff-filter ACMR --name-only origin/main.. -- test/)
-
-  if [ "$paths" == "" ]; then
-    echo No test files added or modified. Exiting.
-    exit 0
-  fi
-
-  echo New or modified test files:
-  echo "$paths"
-
-else
-  paths="test/"
+if [ "$#" -eq 0 ]; then
+  set -- test/
 fi
 
-./tools/lint/lint.py --exceptions lint.exceptions $paths
+./tools/lint/lint.py --exceptions lint.exceptions $@
