e9a282be081f9fd00fcb86caebed49dd5071d42c
Tim Chevalier <tjc@igalia.com>
2024-11-04 13:38:13 -0800

    Temporal: Add test for balancing up to weeks when year/month are present
    See https://github.com/tc39/proposal-temporal/issues/2813

---
+++ test/built-ins/Temporal/Duration/prototype/round/balances-up-to-weeks.js
@@ -0,0 +1,52 @@
+// Copyright (C) 2018 Bloomberg LP. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.duration.prototype.round
+description: Balances up to weeks correctly when years and months are present.
+includes: [temporalHelpers.js]
+features: [Temporal]
+---*/
+
+const oneMonthOneDay = new Temporal.Duration(0, 1, 0, 1, 0, 0, 0, 0, 0, 0);
+const oneYearOneMonthOneDay = new Temporal.Duration(1, 1, 0, 1, 0, 0, 0, 0, 0, 0);
+
+// largestUnit must be included
+assert.throws(RangeError, () => oneMonthOneDay.round({
+        relativeTo: '2024-01-01',
+        smallestUnit: 'weeks',
+        roundingIncrement: 99,
+        roundingMode: 'ceil'
+}));
+
+TemporalHelpers.assertDuration(oneMonthOneDay.round({
+    relativeTo: '2024-01-01',
+    largestUnit: 'weeks',
+    smallestUnit: 'weeks',
+    roundingIncrement: 99,
+    roundingMode: 'ceil'
+}), 0, 0, 99, 0, 0, 0, 0, 0, 0, 0);
+
+TemporalHelpers.assertDuration(oneMonthOneDay.round({
+    relativeTo: '2024-01-01',
+    largestUnit: 'weeks',
+    smallestUnit: 'weeks',
+    roundingIncrement: 6,
+    roundingMode: 'ceil'
+}), 0, 0, 6, 0, 0, 0, 0, 0, 0, 0);
+
+TemporalHelpers.assertDuration(oneYearOneMonthOneDay.round({
+    relativeTo: '2024-01-01',
+    largestUnit: 'weeks',
+    smallestUnit: 'weeks',
+    roundingIncrement: 99,
+    roundingMode: 'ceil'
+}), 0, 0, 99, 0, 0, 0, 0, 0, 0, 0);
+
+TemporalHelpers.assertDuration(oneYearOneMonthOneDay.round({
+    relativeTo: '2024-01-01',
+    largestUnit: 'weeks',
+    smallestUnit: 'weeks',
+    roundingIncrement: 57,
+    roundingMode: 'ceil'
+}), 0, 0, 57, 0, 0, 0, 0, 0, 0, 0);
