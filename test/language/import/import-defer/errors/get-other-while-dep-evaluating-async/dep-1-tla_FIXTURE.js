7ca8a1527ad4c1cb112b09120f7272d35e80a79e
Nicolò Ribaudo <nribaudo@igalia.com>
2024-11-11 10:39:31 +0100

    More fixes

--- test/language/import/import-defer/errors/get-other-while-dep-evaluating-async/dep-1-tla_FIXTURE.js
+++ test/language/import/import-defer/errors/get-other-while-dep-evaluating-async/dep-1-tla_FIXTURE.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2024 Igalia, S.L. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
-import { resolveFirst, resolveThird } from "./promises_FIXTURE.js";
+import { resolveFirst, resolveThird, second } from "./promises_FIXTURE.js";
 import "./dep-1.1_FIXTURE.js"
 
 await Promise.resolve();
