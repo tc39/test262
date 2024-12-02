f3a90fcecfd24e9c2b7f1309115acb09abab834c
Michael Ficarra <m.ficarra@f5.com>
2024-12-02 14:02:51 -0700

    Update fresh-iterator-result.js
    See https://github.com/tc39/proposal-iterator-sequencing/pull/18#issuecomment-2512825730

--- test/built-ins/Iterator/concat/fresh-iterator-result.js
+++ test/built-ins/Iterator/concat/fresh-iterator-result.js
@@ -46,4 +46,4 @@ let iterResult = iterator.next();
 assert.sameValue(iterResult.done, false);
 assert.sameValue(iterResult.value, 123);
 
-assert.notSameValue(iterResult, oldIterResult);
+assert.sameValue(iterResult, oldIterResult);
