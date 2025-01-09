92f95057aac24ea657204819e658a76764679485
Shu-yu Guo <syg@chromium.org>
2025-01-08 16:26:42 -0800

    Add Mozilla copyrights

--- test/staging/sm/module/bug1693261-c1_FIXTURE.js
+++ test/staging/sm/module/bug1693261-c1_FIXTURE.js
@@ -1,4 +1,6 @@
 // |reftest| skip -- support file
+// Copyright (C) 2024 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
 import "./bug1693261-async_FIXTURE.js";
 if (globalThis.testArray === undefined) {
   globalThis.testArray = [];
--- test/staging/sm/module/bug1693261-x_FIXTURE.js
+++ test/staging/sm/module/bug1693261-x_FIXTURE.js
@@ -1,4 +1,6 @@
 // |reftest| skip -- support file
+// Copyright (C) 2024 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
 import "./bug1693261-c1_FIXTURE.js";
 if (globalThis.testArray === undefined) {
   globalThis.testArray = [];
