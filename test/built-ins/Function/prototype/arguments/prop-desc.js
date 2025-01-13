5790fd4173d5bb19b1889b6c541ed9ca22757bfc
Justin Dorfman <justindorfman@gmail.com>
2025-01-13 08:45:10 -0800

    Update test/built-ins/Function/prototype/arguments/prop-desc.js
    Co-authored-by: Richard Gibson <richard.gibson@gmail.com>

--- test/built-ins/Function/prototype/arguments/prop-desc.js
+++ test/built-ins/Function/prototype/arguments/prop-desc.js
@@ -1,12 +1,14 @@
 // Copyright (C) 2024 Justin Dorfman. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-function.prototype.arguments
+esid: sec-addrestrictedfunctionproperties
 description: >
-  Function.prototype.arguments property descriptor
-info: |
   Function.prototype.arguments is an accessor property whose set and get
-  accessor functions are both %ThrowTypeError%.
+  functions are both %ThrowTypeError%.
+info: |
+  2. Let _thrower_ be _realm_.[[Intrinsics]].[[%ThrowTypeError%]].
+  3. Perform ! DefinePropertyOrThrow(_F_, *"caller"*, PropertyDescriptor { [[Get]]: _thrower_, [[Set]]: _thrower_, [[Enumerable]]: *false*, [[Configurable]]: *true* }).
+  4. Perform ! DefinePropertyOrThrow(_F_, *"arguments"*, PropertyDescriptor { [[Get]]: _thrower_, [[Set]]: _thrower_, [[Enumerable]]: *false*, [[Configurable]]: *true* }).
 includes: [propertyHelper.js]
 ---*/
 
