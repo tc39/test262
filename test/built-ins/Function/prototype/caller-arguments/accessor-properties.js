c8dc1f6351c1ce4d38fbc78abe4cd6ec98d47206
Richard Gibson <richard.gibson@gmail.com>
2025-01-29 13:19:16 -0500

    fixup! 1c42cfc33a6fea01d24aec61229b0d9663041b21

--- test/built-ins/Function/prototype/caller-arguments/accessor-properties.js
+++ test/built-ins/Function/prototype/caller-arguments/accessor-properties.js
@@ -1,3 +1,5 @@
+// Copyright (C) 2024 Justin Dorfman. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
 /*---
 esid: sec-addrestrictedfunctionproperties
 description: >
