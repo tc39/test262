36b12c8384c1cf0ffba882a6716493cebfd6b1be
Peter Hoddie <peter@moddable.com>
2025-04-04 10:50:08 -0700

    simplify copyright date

--- test/built-ins/ArrayBuffer/prototype/transferToImmutable/this-is-detached.js
+++ test/built-ins/ArrayBuffer/prototype/transferToImmutable/this-is-detached.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2024-2025 Moddable Tech, Inc. All rights reserved.
+// Copyright (C) 2025 Moddable Tech, Inc. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
 includes: [detachArrayBuffer.js]
