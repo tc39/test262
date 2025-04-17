a98d02f4e128f057b63a9d336ff88173248824ca
Brage Hogstad <brageh01@gmail.com>
2025-04-17 15:12:20 +0200

    Merge and extract for ZonedDateTime/prototype withCalendar

--- test/built-ins/Temporal/ZonedDateTime/prototype/withCalendar/calendar-invalid-iso-string.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/withCalendar/calendar-invalid-iso-string.js
@@ -1,25 +1,23 @@
-// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// Copyright (C) 2025 Igalia, S.L. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
 esid: sec-temporal.zoneddatetime.prototype.withcalendar
-description: A number is not allowed to be a calendar
+description: >
+  An ISO string that cannot be converted to a calendar ID should throw a RangeError
 features: [Temporal]
 ---*/
 
 const instance = new Temporal.ZonedDateTime(1_000_000_000_000_000_000n, "UTC", "iso8601");
 
-const numbers = [
-  1,
-  -19761118,
-  19761118,
-  1234567890,
+const invalidStrings = [
+  ["", "empty string"]
 ];
 
-for (const arg of numbers) {
+for (const [arg, description] of invalidStrings) {
   assert.throws(
-    TypeError,
+    RangeError,
     () => instance.withCalendar(arg),
-    "A number is not a valid ISO string for Calendar"
+    `${description} is not a valid calendar ID`
   );
 }
