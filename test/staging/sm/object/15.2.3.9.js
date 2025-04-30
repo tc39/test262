1b63fba5ca3ff86e289cb27e9138c6a73dd63caa
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:15:30 +0200

    Remove 'harness/sm/non262-object-shell.js' with unused functions

--- test/staging/sm/object/15.2.3.9.js
+++ test/staging/sm/object/15.2.3.9.js
@@ -4,7 +4,7 @@
  */
 
 /*---
-includes: [sm/non262.js, sm/non262-shell.js, sm/non262-object-shell.js, propertyHelper.js]
+includes: [sm/non262.js, sm/non262-shell.js, propertyHelper.js]
 flags:
   - noStrict
 description: |
