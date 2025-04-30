1aa246f0b1602df06beb887b68e6901a0676bed6
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:10 +0200

    Remove no longer needed allow_unused

--- harness/sm/non262-generators-shell.js
+++ harness/sm/non262-generators-shell.js
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /*---
 defines: [assertIteratorResult, assertIteratorNext, assertIteratorDone]
-allow_unused: True
 ---*/
 
 function assertIteratorResult(result, value, done) {
