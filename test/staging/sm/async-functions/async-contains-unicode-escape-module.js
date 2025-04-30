4afc56651af4dc3d53966c2bdabe434b1ef8906e
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:45 +0200

    Replace SpiderMonkey-specific parseModule in async-functions/async-contains-unicode-escape.js and fix a test bug

---
+++ test/staging/sm/async-functions/async-contains-unicode-escape-module.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2024 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: |
+  async/await containing escapes
+esid: pending
+negative:
+  phase: parse
+  type: SyntaxError
+flags:
+  - module
+---*/
+
+export default \u0061sync function f() {}
