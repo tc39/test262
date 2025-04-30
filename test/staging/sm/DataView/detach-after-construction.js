6e1eba2a09c57fc9e2e553d89ab191f8630b433d
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:24 +0200

    Enable strict mode testing for sm/DataView

--- test/staging/sm/DataView/detach-after-construction.js
+++ test/staging/sm/DataView/detach-after-construction.js
@@ -2,16 +2,14 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-flags:
-  - noStrict
 description: |
   pending
 esid: pending
 ---*/
+
 var buf = new ArrayBuffer([1,2]);
 var bufView = new DataView(buf);
 
 $262.detachArrayBuffer(buf);
 
 assert.throws(TypeError, () => bufView.getInt8(0));
-
