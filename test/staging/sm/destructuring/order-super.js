e815b2298ccb9e40037c77f57b64c2aab8ba38c3
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:25 +0200

    Enable strict mode testing for sm/destructuring

--- test/staging/sm/destructuring/order-super.js
+++ test/staging/sm/destructuring/order-super.js
@@ -2,8 +2,6 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-flags:
-  - noStrict
 description: |
   Destructuring should evaluate lhs reference before rhs in super property
 esid: pending
