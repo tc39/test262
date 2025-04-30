be99bdd0b32d2c90e8469a2be8d985f398602e07
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:15:52 +0200

    Replace assertThrowsInstanceOf with assert.throws in sm/expressions

--- test/staging/sm/expressions/inNotObjectError.js
+++ test/staging/sm/expressions/inNotObjectError.js
@@ -21,21 +21,21 @@ assert.throws(TypeError, () => 'this is subString' in 'this is base');
 assert.throws(TypeError, () => 'HEAD' + 'subString'.repeat(30000) in 'HEAD' + 'base'.repeat(30000));
 
 // These test cases check if it does not crash and throws appropriate error.
-assertThrowsInstanceOf(() => { 1 in 'hello' }, TypeError);
-assertThrowsInstanceOf(() => { 'hello' in 1 }, TypeError);
-assertThrowsInstanceOf(() => { 'hello' in null }, TypeError);
-assertThrowsInstanceOf(() => { null in 'hello' }, TypeError);
-assertThrowsInstanceOf(() => { null in null }, TypeError);
-assertThrowsInstanceOf(() => { 'hello' in true }, TypeError);
-assertThrowsInstanceOf(() => { false in 1.1 }, TypeError);
-assertThrowsInstanceOf(() => { Symbol.iterator in undefined }, TypeError);
-assertThrowsInstanceOf(() => { [] in undefined }, TypeError);
-assertThrowsInstanceOf(() => { /a/ in 'hello' }, TypeError);
+assert.throws(TypeError, () => { 1 in 'hello' });
+assert.throws(TypeError, () => { 'hello' in 1 });
+assert.throws(TypeError, () => { 'hello' in null });
+assert.throws(TypeError, () => { null in 'hello' });
+assert.throws(TypeError, () => { null in null });
+assert.throws(TypeError, () => { 'hello' in true });
+assert.throws(TypeError, () => { false in 1.1 });
+assert.throws(TypeError, () => { Symbol.iterator in undefined });
+assert.throws(TypeError, () => { [] in undefined });
+assert.throws(TypeError, () => { /a/ in 'hello' });
 var str = 'hello';
-assertThrowsInstanceOf(() => { str in 'hello' }, TypeError);
+assert.throws(TypeError, () => { str in 'hello' });
 class A {};
-assertThrowsInstanceOf(() => { new A() in undefined }, TypeError);
+assert.throws(TypeError, () => { new A() in undefined });
 var a = new A();
 a.b = 1.1;
-assertThrowsInstanceOf(() => { a.b in 1.1 }, TypeError);
+assert.throws(TypeError, () => { a.b in 1.1 });
 
