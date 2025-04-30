d980c319d8039934c7bbe98f85bdbabf4b3bf27a
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:21 +0200

    Enable strict mode testing for sm/ArrayBuffer

--- test/staging/sm/ArrayBuffer/constructorNotCallable.js
+++ test/staging/sm/ArrayBuffer/constructorNotCallable.js
@@ -2,15 +2,13 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-flags:
-  - noStrict
 description: |
   pending
 esid: pending
 ---*/
+
 assert.throws(TypeError, () => ArrayBuffer());
 assert.throws(TypeError, () => ArrayBuffer(1));
 assert.throws(TypeError, () => ArrayBuffer.call(null));
 assert.throws(TypeError, () => ArrayBuffer.apply(null, []));
 assert.throws(TypeError, () => Reflect.apply(ArrayBuffer, null, []));
-
