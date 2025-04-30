4233b10a55e684f38febecd4c97b5c6b884902b8
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:30 +0200

    Enable strict mode testing for sm/generators

--- test/staging/sm/generators/return-finally.js
+++ test/staging/sm/generators/return-finally.js
@@ -2,8 +2,6 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-flags:
-  - noStrict
 description: |
   Return value should not be overwritten by finally block with normal execution.
 esid: pending
