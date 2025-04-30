09761b6c8cfb65254bbc7fe80afd575c02680a9f
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:09 +0200

    Remove unused includes to non262-strict-shell.js

--- test/staging/sm/strict/10.4.3.js
+++ test/staging/sm/strict/10.4.3.js
@@ -4,7 +4,6 @@
  */
 
 /*---
-includes: [sm/non262-strict-shell.js]
 flags:
   - noStrict
 description: |
--- test/staging/sm/strict/15.4.5.1.js
+++ test/staging/sm/strict/15.4.5.1.js
@@ -4,7 +4,6 @@
  */
 
 /*---
-includes: [sm/non262-strict-shell.js, compareArray.js]
 flags:
   - noStrict
 description: |
