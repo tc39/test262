1b58c83e0a3b22fb0cd280b0596cc68c429a61ad
André Bargull <andre.bargull@gmail.com>
2025-04-30 14:16:35 +0200

    Enable strict mode testing for sm/Promise

--- test/staging/sm/Promise/promise-species.js
+++ test/staging/sm/Promise/promise-species.js
@@ -2,16 +2,14 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-flags:
-  - noStrict
 description: |
   pending
 esid: pending
 ---*/
+
 assert.sameValue(Promise[Symbol.species], Promise);
 let prop = Object.getOwnPropertyDescriptor(Promise, Symbol.species);
 assert.sameValue('get' in prop, true);
 assert.sameValue(typeof prop.get, 'function');
 assert.sameValue('set' in prop, true);
 assert.sameValue(prop.set, undefined);
-
