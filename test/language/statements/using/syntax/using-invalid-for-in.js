5befb7348b3ab7a8873be320d6b21c4ad63debf6
Ron Buckton <3902892+rbuckton@users.noreply.github.com>
2025-05-21 17:17:53 -0400

    Fix incorrect test, esid

--- test/language/statements/using/syntax/using-invalid-for-in.js
+++ test/language/statements/using/syntax/using-invalid-for-in.js
@@ -1,7 +1,7 @@
 // Copyright (C) 2011 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: pending
+esid: sec-for-in-and-for-of-statements
 description: >
     using: not allowed in for..in
 negative:
