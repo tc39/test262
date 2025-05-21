21b557769fd75854ca0834d4de921221989149b1
Ron Buckton <3902892+rbuckton@users.noreply.github.com>
2025-05-21 17:17:53 -0400

    Fix incorrect test, esid

--- test/language/statements/using/syntax/using-invalid-for-using-of-of.js
+++ test/language/statements/using/syntax/using-invalid-for-using-of-of.js
@@ -1,14 +1,22 @@
 // Copyright (C) 2011 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: pending
+esid: sec-for-in-and-for-of-statements
 description: >
     using: 'for (using of' is always interpreted as identifier 
-negative:
-  phase: parse
-  type: SyntaxError
 features: [explicit-resource-management]
 ---*/
 
-$DONOTEVALUATE();
-for (using of of [1, 2, 3]) { }
+var using, of = [[9], [8], [7]], result = [];
+for (using of of [0, 1, 2]) {
+  // ^^^^^    ^^^^^^^^^^^^
+  // |        |
+  // |        interpreted as element access `of[2]`
+  // |
+  // interpreted as identifier named `using`
+
+  result.push(using);
+}
+
+asserts.sameValue(result.length, 1);
+asserts.sameValue(result[0], 7);
