71d4c41ed0d53985f667ef40c8ab6807507f2a32
Ron Buckton <3902892+rbuckton@users.noreply.github.com>
2025-06-02 13:33:06 -0400

    Additional updates

--- test/language/statements/await-using/syntax/await-using.js
+++ test/language/statements/await-using/syntax/await-using.js
@@ -2,7 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: pending
+esid: sec-let-const-using-and-await-using-declarations
 description: >
     module and block scope await using
 flags: [module]
