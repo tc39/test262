ab27e31c30b299ecd12a910be0d10c876fa61d26
Tim Chevalier <tjc@igalia.com>
2025-10-29 14:51:17 -0700

    Temporal: Add an overflow test for date addition with maximum year

---
+++ test/built-ins/Temporal/PlainDate/prototype/add/overflow-adding-months-to-max-year.js
@@ -0,0 +1,13 @@
+// Copyright (C) 2022 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindate.prototype.add
+description: Adding months to maximum year should throw
+features: [Temporal]
+---*/
+
+const maxYear = new Temporal.PlainDate(275760, 1, 1);
+const duration = new Temporal.Duration(0, 5432, 5432, 0, 0, 0, 0, 0, 0, 0);
+assert.throws(RangeError, () => maxYear.add(duration));
+
