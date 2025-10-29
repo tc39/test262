0e496d580829a4355921ac4e9d16ea427a1807e7
Philip Chimento <pchimento@igalia.com>
2025-10-29 10:00:42 -0700

    Minor updates to text in Temporal calendar-wrong-type tests
    These were missed in https://github.com/tc39/test262/pull/4415

--- test/built-ins/Temporal/ZonedDateTime/prototype/since/argument-propertybag-calendar-wrong-type.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/since/argument-propertybag-calendar-wrong-type.js
@@ -5,7 +5,7 @@
 esid: sec-temporal.zoneddatetime.prototype.since
 description: >
   Appropriate error thrown when a calendar property from a property bag cannot
-  be converted to a calendar object or string
+  be converted to a calendar ID
 features: [BigInt, Symbol, Temporal]
 ---*/
 
@@ -15,7 +15,7 @@ const instance = new Temporal.ZonedDateTime(0n, timeZone);
 const wrongTypeTests = [
   [null, "null"],
   [true, "boolean"],
-  [1, "number that doesn't convert to a valid ISO string"],
+  [1, "number"],
   [1n, "bigint"],
   [19970327, "large number"],
   [-19970327, "negative number"],
@@ -30,6 +30,6 @@ for (const [calendar, description] of wrongTypeTests) {
   assert.throws(
     TypeError,
     () => instance.since(arg),
-    `${description} does not convert to a valid ISO string`
+    `${description} is not a valid calendar`
   );
 }
