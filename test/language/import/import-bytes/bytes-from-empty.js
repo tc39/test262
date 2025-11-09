bdf83571ba63313336cf7d9854f3db5acc92a4ff
Steven <steven@ceriously.com>
2025-11-09 15:52:32 -0500

    [import-bytes] add initial tests for import bytes proposal

---
+++ test/language/import/import-bytes/bytes-from-empty.js
@@ -0,0 +1,27 @@
+// Copyright (C) 2021 the V8 project authors. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-create-bytes-module
+description: Creates bytes module from txt file
+flags: [module]
+features: [import-attributes, immutable-arraybuffer, import-bytes]
+includes: [compareArray.js]
+---*/
+
+import value from './bytes-from-empty-FIXTURE.bin' with { type: 'bytes' };
+
+assert(value instanceof Uint8Array);
+assert(value.buffer instanceof ArrayBuffer);
+
+assert.sameValue(value.length, 0);
+assert.sameValue(value.buffer.byteLength, 0);
+
+assert.compareArray(Array.from(value), []);
+
+assert.throws(TypeError, function() {
+  value.buffer.resize(0);
+});
+
+assert.throws(TypeError, function() {
+  value.buffer.transfer();
+});
