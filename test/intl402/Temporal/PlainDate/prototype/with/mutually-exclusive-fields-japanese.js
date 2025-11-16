359a303fb110990bf51c047fc589584cffe8c140
Philip Chimento <pchimento@igalia.com>
2025-11-15 18:29:02 -0800

    Intl Era Monthcode: Tests for mutually exclusive fields in with()
    There was some coverage for mutually exclusive fields (month/monthCode,
    and era+eraYear/year). This expands it to cover all supported calendars
    and for PlainDateTime/ZonedDateTime as well.

--- test/intl402/Temporal/PlainDate/prototype/with/mutually-exclusive-fields-japanese.js
+++ test/intl402/Temporal/PlainDate/prototype/with/mutually-exclusive-fields-japanese.js
@@ -3,48 +3,52 @@
 
 /*---
 esid: sec-temporal.plaindate.prototype.with
-description: Calendar-specific mutually exclusive keys in mergeFields
+description: Calendar-specific mutually exclusive keys in the japanese calendar
+info: NonIsoFieldKeysToIgnore ( _calendar_, _keys_ )
 includes: [temporalHelpers.js]
 features: [Temporal, Intl.Era-monthcode]
 ---*/
 
-const lastDayOfShowa = Temporal.PlainDate.from({ era: "showa", eraYear: 64, year: 1989, month: 1, monthCode: "M01", day: 7, calendar: "japanese" });
+const options = { overflow: "reject" };
+const lastDayOfShowa = Temporal.PlainDate.from({ era: "showa", eraYear: 64, year: 1989, month: 1, monthCode: "M01", day: 7, calendar: "japanese" }, options);
 
-TemporalHelpers.assertPlainDate(lastDayOfShowa, 1989, 1, "M01", 7,
+TemporalHelpers.assertPlainDate(
+  lastDayOfShowa,
+  1989, 1, "M01", 7,
   "check expected fields",
   /* era = */ "showa", /* eraYear = */ 64
 );
 
 TemporalHelpers.assertPlainDate(
-  lastDayOfShowa.with({ day: 10 }),
+  lastDayOfShowa.with({ day: 10 }, options),
   1989, 1, "M01", 10,
   "day excludes era and eraYear",
   /* era = */ "heisei", /* eraYear = */ 1
 );
 
 TemporalHelpers.assertPlainDate(
-  lastDayOfShowa.with({ month: 2 }),
+  lastDayOfShowa.with({ month: 2 }, options),
   1989, 2, "M02", 7,
   "month excludes monthCode, era, and eraYear",
   "heisei", 1
 );
 
 TemporalHelpers.assertPlainDate(
-  lastDayOfShowa.with({ monthCode: "M03" }),
+  lastDayOfShowa.with({ monthCode: "M03" }, options),
   1989, 3, "M03", 7,
   "monthCode excludes month, era, and eraYear",
   "heisei", 1
 );
 
 TemporalHelpers.assertPlainDate(
-  lastDayOfShowa.with({ year: 1988 }),
+  lastDayOfShowa.with({ year: 1988 }, options),
   1988, 1, "M01", 7,
   "year excludes era and eraYear (within same era)",
   "showa", 63
 );
 
 TemporalHelpers.assertPlainDate(
-  lastDayOfShowa.with({ year: 1990 }),
+  lastDayOfShowa.with({ year: 1990 }, options),
   1990, 1, "M01", 7,
   "year excludes era and eraYear (in a different era)",
   "heisei", 2
