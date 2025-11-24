bb87cea80e75c205fb5ffa1b6920b165abb60477
Tim Chevalier <tjc@igalia.com>
2025-11-24 11:56:31 -0800

    Use more recent years

--- test/intl402/Temporal/PlainDateTime/prototype/until/wrapping-at-end-of-month-indian.js
+++ test/intl402/Temporal/PlainDateTime/prototype/until/wrapping-at-end-of-month-indian.js
@@ -12,15 +12,15 @@ const calendar = "indian";
 
 // Difference between end of longer month to end of following shorter month
 {
-  const end = Temporal.PlainDateTime.from({ year: 1970, monthCode: "M07", day: 30, hour: 12, minute: 34, calendar });
+  const end = Temporal.PlainDateTime.from({ year: 1945, monthCode: "M07", day: 30, hour: 12, minute: 34, calendar });
   for (const largestUnit of ["years", "months"]) {
     TemporalHelpers.assertDuration(
-      Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
+      Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
       0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
       `Bhadra 30th to Asvina 30th is one month (${largestUnit})`
     );
     TemporalHelpers.assertDuration(
-      Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
+      Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
       0, 0, 0, 30, 0, 0, 0, 0, 0, 0,
       `Bhadra 31st to Asvina 30th is 30 days, not one month (${largestUnit})`
     );
@@ -30,15 +30,15 @@ const calendar = "indian";
 // Difference between end of longer month to end of not-immediately-following
 // shorter month
 {
-  const end = Temporal.PlainDateTime.from({ year: 1970, monthCode: "M08", day: 30, hour: 12, minute: 34, calendar });
+  const end = Temporal.PlainDateTime.from({ year: 1945, monthCode: "M08", day: 30, hour: 12, minute: 34, calendar });
   for (const largestUnit of ["years", "months"]) {
     TemporalHelpers.assertDuration(
-      Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
+      Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
       0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
       `Bhadra 30th to Kartika 30th is 2 months (${largestUnit})`
     );
     TemporalHelpers.assertDuration(
-      Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
+      Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit }),
       0, 1, 0, 30, 0, 0, 0, 0, 0, 0,
       `Bhadra 31st to Kartika 30th is 1 month 30 days, not 2 months (${largestUnit})`
     );
@@ -48,25 +48,25 @@ const calendar = "indian";
 // Difference between end of longer month in one year to shorter month in
 // later year
 {
-  const end = Temporal.PlainDateTime.from({ year: 1973, monthCode: "M07", day: 30, hour: 12, minute: 34, calendar });
+  const end = Temporal.PlainDateTime.from({ year: 1950, monthCode: "M07", day: 30, hour: 12, minute: 34, calendar });
   TemporalHelpers.assertDuration(
-    Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "months" }),
-    0, 37, 0, 0, 0, 0, 0, 0, 0, 0,
-    "Bhadra 30th 1970 to Asvina 30th 1973 is 37 months"
+    Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "months" }),
+    0, 61, 0, 0, 0, 0, 0, 0, 0, 0,
+    "Bhadra 30th 1945 to Asvina 30th 1950 is 61 months"
   );
   TemporalHelpers.assertDuration(
-    Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "years" }),
-    3, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    "Bhadra 30th 1970 to Asvina 30th 1973 is 3 years, 1 month"
+    Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 30, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "years" }),
+    5, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    "Bhadra 30th 1945 to Asvina 30th 1950 is 5 years, 1 month"
   );
   TemporalHelpers.assertDuration(
-    Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "months" }),
-    0, 36, 0, 30, 0, 0, 0, 0, 0, 0,
-    "Bhadra 31st 1970 to Asvina 30th 1973 is 36 months, 30 days, not 37 months"
+    Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "months" }),
+    0, 60, 0, 30, 0, 0, 0, 0, 0, 0,
+    "Bhadra 31st 1945 to Asvina 30th 1950 is 60 months, 30 days, not 61 months"
   );
   TemporalHelpers.assertDuration(
-    Temporal.PlainDateTime.from({ year: 1970, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "years" }),
-    3, 0, 0, 30, 0, 0, 0, 0, 0, 0,
-    "Bhadra 31st 1970 to Asvina 30th 1973 is 3 years, 30 days"
+    Temporal.PlainDateTime.from({ year: 1945, monthCode: "M06", day: 31, hour: 12, minute: 34, calendar }).until(end, { largestUnit: "years" }),
+    5, 0, 0, 30, 0, 0, 0, 0, 0, 0,
+    "Bhadra 31st 1945 to Asvina 30th 1950 is 5 years, 30 days"
   );
 }
