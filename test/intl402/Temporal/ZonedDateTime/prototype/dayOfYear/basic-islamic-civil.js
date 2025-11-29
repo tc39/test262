35b2786471cb1bf5213e7a8e0b6c0d8483304ff2
Tim Chevalier <tjc@igalia.com>
2025-11-28 16:00:17 -0800

    Temporal: Remove Islamic calendar staging tests and add new equivalent tests
    Test the following properties for relevant objects, Islamic calendars:
    inLeapYear, daysInYear, monthsInYear, dayOfYear, daysInMonth
    
    (A future PR will address these properties for other calendars.)

---
+++ test/intl402/Temporal/ZonedDateTime/prototype/dayOfYear/basic-islamic-civil.js
@@ -0,0 +1,31 @@
+// Copyright (C) 2025 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.prototype.dayofyear
+description: dayOfYear property in the islamic-civil calendar
+includes: [temporalHelpers.js]
+features: [Temporal, Intl.Era-monthcode]
+---*/
+
+const calendar = "islamic-civil";
+const options = { overflow: "reject" };
+
+// 1390 = ISO year 1970
+const year = 1390;
+
+var date = Temporal.ZonedDateTime.from({
+    year,
+    month: 1,
+    day: 1,
+    calendar, hour: 12, minute: 34, timeZone: "UTC"
+});
+
+var expectedDay = 1;
+const days1 = new Temporal.Duration(0, 0, 0, 1);
+
+while (date.year == year) {
+    assert.sameValue(date.dayOfYear, expectedDay);
+    date = date.add(days1);
+    expectedDay++;
+}
