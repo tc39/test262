237fccc129fd0dc7b99aac703d8d3ab08f7b1137
Tim Chevalier <tjc@igalia.com>
2025-11-29 18:22:30 -0800

    Split out tests from test/staging/Intl402/Temporal/old/non-iso-calendars-gregory.js
    * Add more tests for add/subtract/since/until
    * Add tests for inLeapYear, monthsInYear, daysInMonth properties
    * Add tests checking that leap months are disallowed
    * Add tests for wrapping at the end of the month in with

---
+++ test/intl402/Temporal/ZonedDateTime/prototype/monthCode/no-leap-months-gregory.js
@@ -0,0 +1,21 @@
+// Copyright (C) 2025 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.zoneddatetime.prototype.monthcode
+description: gregory calendar does not have leap months
+features: [Temporal, Intl.Era-monthcode]
+---*/
+
+const calendar = "gregory";
+
+for (var year = 1970; year < 1975; year++) {
+  for (var month = 1; month < 13; month++) {
+    const date = Temporal.ZonedDateTime.from({
+        year,
+        month,
+        calendar, day: 1, hour: 12, minute: 34, timeZone: "UTC"
+    });
+    assert.sameValue(date.monthCode.endsWith("L"), false);
+  }
+}
