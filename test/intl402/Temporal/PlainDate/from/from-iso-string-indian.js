773c79afa0298256457f3f93e45cb95d1d95c3bb
Tim Chevalier <tjc@igalia.com>
2025-12-01 12:58:46 -0800

    Rename/rewrite string parsing tests

---
+++ test/intl402/Temporal/PlainDate/from/from-iso-string-indian.js
@@ -0,0 +1,24 @@
+// Copyright (C) 2025 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindate.prototype.from
+description: >
+  Check from with leap years and common years
+  (indian calendar)
+includes: [temporalHelpers.js]
+features: [Temporal, Intl.Era-monthcode]
+---*/
+
+const calendar = "indian";
+
+const cases = [
+  ["2004-03-21[+00:00][u-ca=indian]", 1926, 1, "M01", 1, "first day of leap year"],
+  ["2005-03-22[+00:00][u-ca=indian]", 1927, 1, "M01", 1, "first day of common year"],
+];
+for (const [isoString, year, month, monthCode, day, descr] of cases) {
+  const date = Temporal.PlainDate.from(isoString);
+
+  TemporalHelpers.assertPlainDate(date, year, month, monthCode, day, descr, "shaka", year);
+}
+
