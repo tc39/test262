79ceafa124f32df609d174faf55969dceccf8ec8
Tim Chevalier <tjc@igalia.com>
2025-12-01 15:43:58 -0800

    Split out tests from test/staging/Intl402/Temporal/old/non-iso-calendars-iso8601.js
    * Duplicate basic-arithmetic tests across types, then add new cases
    * Add `with` tests for different types
    * Duplicate since/until tests across types, then add new cases
    * Test addition/subtraction at month boundary
    * Add a test checking that there are no leap months in iso8601 calendar
    * Add tests for `with` wrapping at end of month

--- test/built-ins/Temporal/PlainDateTime/prototype/monthCode/no-leap-months.js
+++ test/built-ins/Temporal/PlainDateTime/prototype/monthCode/no-leap-months.js
@@ -11,8 +11,9 @@ features: [Temporal]
 for (var year = 1970; year < 1975; year++) {
   for (var month = 1; month < 13; month++) {
     const date = Temporal.PlainDateTime.from({
-        year,
-        month, day: 1, hour: 12, minute: 34
+      year,
+      month,
+      day: 1, hour: 12, minute: 34
     });
     assert.sameValue(date.monthCode.endsWith("L"), false);
   }
