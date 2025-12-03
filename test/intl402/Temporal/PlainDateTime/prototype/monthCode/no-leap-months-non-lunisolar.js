8a879d7a6f81e4ed530a8e1a6f58eaff1c8966d5
Tim Chevalier <tjc@igalia.com>
2025-12-03 13:08:31 -0800

    Temporal: Refactor tests for absence of leap months
    Incorporate all non-lunisolar calendars into a single test.

--- test/intl402/Temporal/PlainDateTime/prototype/monthCode/no-leap-months-non-lunisolar.js
+++ test/intl402/Temporal/PlainDateTime/prototype/monthCode/no-leap-months-non-lunisolar.js
@@ -3,16 +3,28 @@
 
 /*---
 esid: sec-temporal.plaindatetime.prototype.monthcode
-description: Buddhist calendar does not have leap months
+description: Non-lunisolar calendars do not have leap months
 features: [Temporal, Intl.Era-monthcode]
 ---*/
 
-const calendar = "buddhist";
+const calendars = {
+  "buddhist": 2513,
+  "coptic": 1686,
+  "ethioaa": 7462,
+  "ethiopic": 1962,
+  "gregory": 1970,
+  "indian": 1892,
+  "islamic-civil": 1390,
+  "islamic-tbla": 1390,
+  "islamic-umalqura": 1390,
+  "persian": 1348,
+  "roc": 60,
+};
 
-for (var year = 2513; year < 2518; year++) {
+for (let [calendar, year] of Object.entries(calendars)) {
   for (var month = 1; month < 13; month++) {
     const date = Temporal.PlainDateTime.from({
-        year,
+        year: year,
         month,
         calendar, day: 1, hour: 12, minute: 34
     });
