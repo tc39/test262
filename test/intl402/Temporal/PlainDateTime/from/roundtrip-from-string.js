2a4b8296c6d197386ff08f2faba0d1a3be9bed66
Philip Chimento <pchimento@igalia.com>
2025-12-02 16:53:20 -0800

    Intl Era Monthcode: Move formatToParts test out of staging
    Moves test/staging/sm/Temporal/Calendar/compare-to-datetimeformat.js
    into the main tree, removing the SpiderMonkey-specific skips and
    workarounds. I found it easier to split it into two files, one for
    lunisolar calendars and one for the rest.
    
    The specific dates mentioned in comments as problematic, I've added to
    roundtrip-from-string.js.

--- test/intl402/Temporal/PlainDateTime/from/roundtrip-from-string.js
+++ test/intl402/Temporal/PlainDateTime/from/roundtrip-from-string.js
@@ -71,6 +71,9 @@ for (const [calendar, year, month, monthCode, day, era, eraYear, descr] of year1
 const additionalCases = [
   ["2004-03-21T12:34[u-ca=indian]", 1926, 1, "M01", 1, "shaka", 1926, "first day of leap year"],
   ["2005-03-22T12:34[u-ca=indian]", 1927, 1, "M01", 1, "shaka", 1927, "first day of common year"],
+  ["2006-07-25T12:34[u-ca=islamic-umalqura]", 1427, 6, "M06", 29, "ah", 1427, "ICU4C/ICU4X discrepancy"],
+  ["2025-04-19T12:34[u-ca=persian]", 1404, 1, "M01", 31, "ap", 1404, "ICU4C/ICU4X discrepancy"],
+  ["2046-10-30T12:34[u-ca=hebrew]", 5807, 1, "M01", 30, "am", 5807, "ICU4C/ICU4X discrepancy"],
 ];
 
 for (const [string, year, month, monthCode, day, era, eraYear, descr] of additionalCases) {
