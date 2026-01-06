43d94f4e7f3399b8d1fc3f62563078c9bf90a2c8
Tim Chevalier <tjc@igalia.com>
2026-01-06 15:31:20 -0800

    [Temporal] Remove test/staging/Temporal/v8/calendar-date-until.js
    And move functionality into one existing test and one new test.

---
+++ test/built-ins/Temporal/PlainDate/prototype/until/throws-with-time-units.js
@@ -0,0 +1,32 @@
+// Copyright (C) 2026 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.plaindate.prototype.until
+description: Throws if largestUnit or smallestUnit is a time unit
+features: [Temporal]
+---*/
+
+const from = new Temporal.PlainDate(2021, 7, 16);
+const to = new Temporal.PlainDate(2021, 7, 17);
+
+const units = ['hour', 'minute', 'second', 'millisecond', 'microsecond', 'nanosecond'];
+
+for (const largestUnit of units) {
+  assert.throws(RangeError, () => from.until(to, { largestUnit }),
+                `Can't use ${largestUnit} as largestUnit for PlainDate`);
+}
+
+for (const smallestUnit of units) {
+  assert.throws(RangeError, () => from.until(to, { smallestUnit }),
+                `Can't use ${smallestUnit} as smallestUnit for PlainDate`);
+}
+
+for (const largestUnit of units) {
+  for (const smallestUnit of units) {
+    assert.throws(RangeError, () => from.until(to, { largestUnit, smallestUnit }),
+                 `Can't use ${largestUnit} and ${smallestUnit} as largestUnit and smallestUnit for PlainDate`);
+    assert.throws(RangeError, () => from.until(to, { smallestUnit, largestUnit }),
+                 `Can't use ${smallestUnit} and ${largestUnit} as largestUnit and smallestUnit for PlainDate`);
+  }
+}
