281da746c719205a60b1f9e755807fa6dc833bc7
Tim Chevalier <tjc@igalia.com>
2026-01-09 18:48:53 -0800

    Update tests to reflect new version of normative change

--- test/built-ins/Temporal/PlainYearMonth/prototype/add/end-of-month-out-of-range.js
+++ test/built-ins/Temporal/PlainYearMonth/prototype/add/end-of-month-out-of-range.js
@@ -10,7 +10,12 @@ features: [Temporal]
 
 // Based on a test case by André Bargull <andre.bargull@gmail.com>
 
-const duration = new Temporal.Duration(0, 0, 0, -1);
+const oneDay = new Temporal.Duration(0, 0, 0, 1);
+const twoWeeks = new Temporal.Duration(0, 0, 2, 0);
 
 // Calendar addition result is not out of range
-TemporalHelpers.assertPlainYearMonth(new Temporal.PlainYearMonth(275760, 9).add(duration), 275760, 9, "M09", "adding 1 month to 275760-09");
+TemporalHelpers.assertPlainYearMonth(new Temporal.PlainYearMonth(275760, 9).add(oneDay), 275760, 9, "M09", "adding 1 day to 275760-09");
+
+// Calendar addition result is out of range
+assert.throws(RangeError, () => new Temporal.PlainYearMonth(275760, 9).add(twoWeeks), "adding 2 weeks to 275760-09 is out of range");
+
--- test/built-ins/Temporal/PlainYearMonth/prototype/subtract/end-of-month-out-of-range.js
+++ test/built-ins/Temporal/PlainYearMonth/prototype/subtract/end-of-month-out-of-range.js
@@ -3,14 +3,18 @@
 
 /*---
 esid: sec-temporal.plainyearmonth.prototype.subtract
-description: RangeError should not be thrown when subtracting positive duration and end of month is out of range
+description: RangeError should be thrown when subtracting positive duration and end of month is out of range
 includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
 
 // Based on a test case by André Bargull <andre.bargull@gmail.com>
 
-const duration = new Temporal.Duration(0, 0, 0, 1);
+const oneDay = new Temporal.Duration(0, 0, 0, 1);
+const twoWeeks = new Temporal.Duration(0, 0, 2, 0);
 
-// Calendar subtraction result is not out of range
-TemporalHelpers.assertPlainYearMonth(new Temporal.PlainYearMonth(275760, 9).subtract(duration), 275760, 9, "M09", "subtractig 1 month from 275760-09");
+// Calendar subtraction result is out of range
+assert.throws(RangeError, () => new Temporal.PlainYearMonth(275760, 9).subtract(oneDay), "subtracting 1 day from 275760-09 is out of range");
+
+// Calendar subtraction result is out of range
+assert.throws(RangeError, () => new Temporal.PlainYearMonth(275760, 9).subtract(twoWeeks), "subtracting 2 weeks from 275760-09 is out of range");
