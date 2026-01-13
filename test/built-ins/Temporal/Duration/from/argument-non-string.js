b33c50e09c8f02829d8e6c93a25cc2e861ae43d4
Tim Chevalier <tjc@igalia.com>
2026-01-13 14:58:59 -0800

    Reorganize tests for empty string / objects

--- test/built-ins/Temporal/Duration/from/argument-non-string.js
+++ test/built-ins/Temporal/Duration/from/argument-non-string.js
@@ -13,39 +13,27 @@ const primitiveTests = [
   [undefined, "undefined"],
   [null, "null"],
   [true, "boolean"],
-  ["", "empty string"],
   [1, "number that doesn't convert to a valid ISO string"],
   [19761118, "number that would convert to a valid ISO string in other contexts"],
   [1n, "bigint"],
-];
-
-for (const [arg, description] of primitiveTests) {
-  assert.throws(
-    typeof arg === 'string' ? RangeError : TypeError,
-    () => Temporal.Duration.from(arg),
-    `${description} does not convert to a valid ISO string`
-  );
-
-  for (options of [undefined, { overflow: 'constrain' }, { overflow: 'reject' }]) {
-    assert.throws(
-      typeof arg === 'string' ? RangeError : TypeError,
-      () => Temporal.Duration.from(arg, options),
-      `${description} does not convert to a valid ISO string with options ${options}`
-    );
-  }
-}
-
-const typeErrorTests = [
   [Symbol(), "symbol"],
   [{}, "plain object"],
   [Temporal.Duration, "Temporal.Duration, object"],
   [Temporal.Duration.prototype, "Temporal.Duration.prototype, object"],
 ];
 
-for (const [arg, description] of typeErrorTests) {
-  assert.throws(TypeError, () => Temporal.Duration.from(arg), `${description} is not a valid property bag and does not convert to a string`);
+for (const [arg, description] of primitiveTests) {
+  assert.throws(
+    TypeError,
+    () => Temporal.Duration.from(arg),
+    `${description} does not convert to a valid ISO string`
+  );
 
   for (options of [undefined, { overflow: 'constrain' }, { overflow: 'reject' }]) {
-    assert.throws(TypeError, () => Temporal.Duration.from(arg, options), `${description} is not a valid property bag and does not convert to a string with options ${options}`);
+    assert.throws(
+      TypeError,
+      () => Temporal.Duration.from(arg, options),
+      `${description} does not convert to a valid ISO string with options ${options}`
+    );
   }
 }
