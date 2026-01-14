4df9c710fc88f03202d7cb02a2b1124fe0fc2731
Ioanna M. Dimitriou H. <idimitriou@igalia.com>
2026-01-14 14:50:16 +0100

    [Temporal] move v8 staging test plain-date-time-get-iso-fields.js
    Some tests in `test/staging/Temporal/v8/plain-date-time-get-iso-fields.js` are covered by
    - `test/built-ins/Temporal/PlainDateTime/argument-convert.js`
    - `test/built-ins/Temporal/PlainDateTime/missing-arguments.js`
    
    Move the rest to `test/built-ins/Temporal/PlainDateTime/basic.js`

--- test/built-ins/Temporal/PlainDateTime/basic.js
+++ test/built-ins/Temporal/PlainDateTime/basic.js
@@ -2,10 +2,8 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: pending
-description: >
-  Automatically ported from plain-date-time-get-iso-fields test
-  in V8's mjsunit test plain-date-time-get-iso-fields.js
+esid: sec-temporal.plaindatetime.constructor
+description: basic tests for the PlainDateTime constructor
 includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
@@ -17,10 +15,5 @@ TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(1, 2, 3, 4, 5, 6)
 TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(1, 2, 3, 4, 5), 1, 2, 'M02', 3, 4, 5, 0, 0, 0, 0);
 TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(1, 2, 3, 4), 1, 2, 'M02', 3, 4, 0, 0, 0, 0, 0);
 TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(1, 2, 3), 1, 2, 'M02', 3, 0, 0, 0, 0, 0, 0);
-assert.throws(RangeError, () => new Temporal.PlainDateTime(1, 2));
-assert.throws(RangeError, () => new Temporal.PlainDateTime(1));
-assert.throws(RangeError, () => new Temporal.PlainDateTime());
 TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(-25406, 1, 1), -25406, 1, 'M01', 1, 0, 0, 0, 0, 0, 0);
 TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(29345, 12, 31, 23, 59, 59, 999, 999, 999), 29345, 12, 'M12', 31, 23, 59, 59, 999, 999, 999);
-TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(false, true, true, undefined, true), 0, 1, 'M01', 1, 0, 1, 0, 0, 0, 0, 0);
-TemporalHelpers.assertPlainDateTime(new Temporal.PlainDateTime(11.9, 12.8, 13.7, 14.6, 15.5, 16.6, 17.7, 18.8, 1.999999), 11, 12, 'M12', 13, 14, 15, 16, 17, 18, 1);
