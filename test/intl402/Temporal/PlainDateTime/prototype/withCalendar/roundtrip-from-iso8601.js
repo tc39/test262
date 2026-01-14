1a5f28dafc3060ee8ee279ef2e32450a38c20490
Ioanna M. Dimitriou H. <idimitriou@igalia.com>
2026-01-14 13:38:32 +0100

    [Temporal] Remove v8 staging test plain-date-time-with-calendar and -with-plain-time
    The tests in `test/staging/Temporal/v8/plain-date-time-with-calendar.js` are covered by `test/built-ins/Temporal/PlainDateTime/prototype/withCalendar/branding.js`. Move the rest to
    `test/intl402/Temporal/PlainDateTime/prototype/withCalendar/roundtrip-from-iso8601.js`.
    
    Some tests in `test/staging/Temporal/v8/plain-date-time-with-plain-time.js` are covered by tests in
    - `test/built-ins/Temporal/PlainDateTime/prototype/withPlainTime/branding.js`
    - `test/built-ins/Temporal/PlainDateTime/prototype/withPlainTime/argument-propertybag-optional-properties.js`
    - `test/built-ins/Temporal/PlainDateTime/prototype/withPlainTime/argument-wrong-type.js`
    Move the rest to `test/intl402/Temporal/PlainDate/prototype/withPlainTime/basic-roc.js` (in new directory).

--- test/intl402/Temporal/PlainDateTime/prototype/withCalendar/roundtrip-from-iso8601.js
+++ test/intl402/Temporal/PlainDateTime/prototype/withCalendar/roundtrip-from-iso8601.js
@@ -2,18 +2,13 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: pending
-description: >
-  Automatically ported from plain-date-time-with-calendar test
-  in V8's mjsunit test plain-date-time-with-calendar.js
+esid: sec-temporal.plaindatetime.prototype.withcalendar
+description: Converting to iso8601 and back works as expeced.
 includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
 
 let d1 = new Temporal.PlainDateTime(1911, 10, 10, 4, 5, 6, 7, 8, 9);
-let badDateTime = { withCalendar: d1.withCalendar };
-assert.throws(TypeError, () => badDateTime.withCalendar('iso8601'));
-
 let d2 = d1.withCalendar('roc');
 assert.sameValue('roc', d2.calendarId);
 TemporalHelpers.assertPlainDateTime(d2, 0, 10, 'M10', 10, 4, 5, 6, 7, 8, 9, '', 'broc', 1);
