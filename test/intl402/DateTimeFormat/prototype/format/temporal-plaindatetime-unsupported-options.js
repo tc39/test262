c281c25beb91ca10cb3bda29d4045d9738b6d479
André Bargull <andre.bargull@gmail.com>
2026-01-15 16:38:24 +0100

    The "timeZoneName" option can't be used in a standalone context
    See <https://github.com/tc39/ecma402/issues/461>.

--- test/intl402/DateTimeFormat/prototype/format/temporal-plaindatetime-unsupported-options.js
+++ test/intl402/DateTimeFormat/prototype/format/temporal-plaindatetime-unsupported-options.js
@@ -3,7 +3,7 @@
 
 /*---
 esid: sec-datetime-format-functions
-description: PlainDateTime cannot be formatted with a formatter created with the timeZoneName option.
+description: PlainDateTime can be formatted with a formatter created with the timeZoneName option.
 locale: [en-US]
 features: [Temporal]
 ---*/
@@ -16,6 +16,6 @@ const pdt = new Temporal.PlainDateTime(2026, 1, 5, 11, 22);
 
 for (const timeZoneNameStyle of timeZoneNameStyles) {
   const dtf = new Intl.DateTimeFormat(locale, { timeZoneName: timeZoneNameStyle });
-  assert.throws(TypeError, () => dtf.format(pdt),
+  assert.sameValue(typeof dtf.format(pdt), "string",
     `cannot format a PlainDateTime with timeZoneName = ${timeZoneNameStyle}`);
 }
--- test/intl402/DateTimeFormat/prototype/formatRange/temporal-plaindatetime-unsupported-options.js
+++ test/intl402/DateTimeFormat/prototype/formatRange/temporal-plaindatetime-unsupported-options.js
@@ -3,7 +3,7 @@
 
 /*---
 esid: sec-datetime-format-functions
-description: PlainDateTime cannot be formatted with a formatter created with the timeZoneName option.
+description: PlainDateTime can be formatted with a formatter created with the timeZoneName option.
 locale: [en-US]
 features: [Temporal]
 ---*/
@@ -17,6 +17,6 @@ const pdt2 = new Temporal.PlainDateTime(2026, 1, 5, 11, 23);
 
 for (const timeZoneNameStyle of timeZoneNameStyles) {
   const dtf = new Intl.DateTimeFormat(locale, { timeZoneName: timeZoneNameStyle });
-  assert.throws(TypeError, () => dtf.formatRange(pdt1, pdt2),
+  assert.sameValue(typeof dtf.formatRange(pdt1, pdt2), "string",
     `cannot format a PlainDateTime with timeZoneName = ${timeZoneNameStyle}`);
 }
