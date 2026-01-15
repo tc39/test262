7f13a59538a6e735de720bff4ad9b27331c21be5
André Bargull <andre.bargull@gmail.com>
2026-01-15 16:30:57 +0100

    Expect TypeError when calling Temporal.PlainTime.p.toLocaleString with dateStyle

--- test/intl402/Temporal/PlainTime/prototype/toLocaleString/datestyle-and-timestyle.js
+++ test/intl402/Temporal/PlainTime/prototype/toLocaleString/datestyle-and-timestyle.js
@@ -3,10 +3,12 @@
 
 /*---
 esid: sec-temporal.plaintime.prototype.tolocalestring
-description: Using both dateStyle and timeStyle should not throw
+description: Using dateStyle, even if timeStyle is present, should throw
 features: [Temporal]
 ---*/
 
 const item = new Temporal.PlainTime(0, 0);
-var result = item.toLocaleString("en", { dateStyle: "full", timeStyle: "full" });
-assert.sameValue(result.includes(":00"), true, "using both dateStyle and timeStyle should not throw");
+
+assert.throws(TypeError, function() {
+  item.toLocaleString("en", { dateStyle: "full", timeStyle: "full" });
+});
