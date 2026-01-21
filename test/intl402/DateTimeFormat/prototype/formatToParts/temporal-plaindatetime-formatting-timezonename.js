704de2df1f300814645eaaa432f4bc08753d742c
Philip Chimento <pchimento@igalia.com>
2026-01-20 16:59:30 -0800

    Temporal: Improve tests for formatting Plain objects with timeZoneName
    The timeZoneName should be ignored and therefore default options should
    be used, so not only should the formatting succeed but it should also
    not print the time zone name.
    
    Expand the existing tests from just PlainDateTime to cover all the Plain
    types.
    
    See https://github.com/tc39/proposal-temporal/issues/3254

--- test/intl402/DateTimeFormat/prototype/formatToParts/temporal-plaindatetime-formatting-timezonename.js
+++ test/intl402/DateTimeFormat/prototype/formatToParts/temporal-plaindatetime-formatting-timezonename.js
@@ -3,7 +3,9 @@
 
 /*---
 esid: sec-datetime-format-functions
-description: PlainDateTime can be formatted with a formatter created with the timeZoneName option.
+description: >
+  PlainDateTime can be formatted with a formatter created with the timeZoneName
+  option, but no time zone name is included.
 locale: [en-US]
 features: [Temporal]
 ---*/
@@ -16,6 +18,11 @@ const pdt = new Temporal.PlainDateTime(2026, 1, 5, 11, 22);
 
 for (const timeZoneNameStyle of timeZoneNameStyles) {
   const dtf = new Intl.DateTimeFormat(locale, { timeZoneName: timeZoneNameStyle });
-  assert(Array.isArray(dtf.formatToParts(pdt)),
+  const result = dtf.formatToParts(pdt);
+  assert(Array.isArray(result),
     `can format a PlainDateTime with timeZoneName = ${timeZoneNameStyle}`);
+  for (const { type } of result) {
+    assert.notSameValue(type, "timeZoneName",
+      `formatting a PlainDateTime with timeZoneName = ${timeZoneNameStyle} should not print a time zone`);
+  }
 }
