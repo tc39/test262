cf227b7485f3cebb708d5623e88fe68baccc917b
G-Lee1031 <t01092750632@gmail.com>
2026-02-09 15:09:35 +0900

    combined two files and modified file name

--- test/built-ins/BigInt/prototype/toString/radix-tointegerorinfinity-throws-toprimitive-or-bigint.js
+++ test/built-ins/BigInt/prototype/toString/radix-tointegerorinfinity-throws-toprimitive-or-bigint.js
@@ -4,7 +4,7 @@
 /*---
 esid: sec-bigint.prototype.tostring
 description: > 
-    If _radix_ is a BigInt or a Symbol, ToNumber called by BigInt.prototype.toString throws a TypeError.
+    ToNumber called by BigInt.prototype.toString throws when _radix_ is a BigInt or when ToPrimitive Throws
 info: |
     BigInt.prototype.toString ( [ _radix_ ] )
 
@@ -17,8 +17,17 @@ info: |
     ToNumber (_argument_)
 
     2. If _argument_ is either a Symbol or a BigInt, throw a *TypeError* exception.
+
+    7. Assert: _argument_ is an Object.
+    8. Let _primValue_ be ? ToPrimitive(_argument_, ~number~).
 features: [BigInt]
 ---*/
+//Check #1
 assert.throws(TypeError, function() {
     (0n).toString(0n);
 }, "If _radix_ is BigInt, BigInt.prototype.toString must throw a TypeError")
+
+//Check #2
+assert.throws(TypeError, function() {
+    BigInt.prototype.toString.call(10n, {valueOf: undefined, toString: undefined})
+}, "TypeError is thrown when _radix_ cannot be converted to a primitive")
