fa9e42d1a832a6fb1df3cb46fe6780cdd18c8e5e
Jesse Alama <jesse@igalia.com>
2026-03-03 15:03:43 +0100

    Temporal: Use TemporalHelpers.assertPlainDate in Japanese pre-Meiji test

--- test/intl402/Temporal/PlainDate/from/japanese-pre-meiji.js
+++ test/intl402/Temporal/PlainDate/from/japanese-pre-meiji.js
@@ -9,46 +9,35 @@ info: |
   The Japanese calendar in Temporal only supports the five modern eras (meiji,
   taisho, showa, heisei, reiwa) plus "ce" and "bce" for dates outside those
   ranges. Dates before Meiji 6 (1873) are represented using the "ce" era.
+includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
 
-// A date before the Meiji era (which starts at 1873 in the polyfill).
+// A date before the Meiji era (which starts at 1873).
 // 1800-06-15 in ISO calendar should be in the "ce" era in the Japanese calendar.
 var preMeiji = Temporal.PlainDate.from({ calendar: "japanese", era: "ce", eraYear: 1800, month: 6, day: 15 });
-assert.sameValue(preMeiji.era, "ce", "pre-Meiji date should be in ce era");
-assert.sameValue(preMeiji.eraYear, 1800, "pre-Meiji date eraYear should be 1800");
-assert.sameValue(preMeiji.year, 1800, "pre-Meiji date year should be 1800");
-assert.sameValue(preMeiji.month, 6, "pre-Meiji date month");
-assert.sameValue(preMeiji.day, 15, "pre-Meiji date day");
+TemporalHelpers.assertPlainDate(preMeiji, 1800, 6, "M06", 15, "pre-Meiji date", "ce", 1800);
 
 // Also verify round-tripping through ISO
 var isoDate = preMeiji.withCalendar("iso8601");
-assert.sameValue(isoDate.year, 1800, "round-tripped ISO year");
-assert.sameValue(isoDate.month, 6, "round-tripped ISO month");
-assert.sameValue(isoDate.day, 15, "round-tripped ISO day");
+TemporalHelpers.assertPlainDate(isoDate, 1800, 6, "M06", 15, "round-tripped ISO date");
 
 // A date in early Meiji, but before the cutoff at Meiji 6 (1873)
 // 1870-03-01 should still be in "ce" era
 var earlyMeiji = Temporal.PlainDate.from({ calendar: "japanese", era: "ce", eraYear: 1870, month: 3, day: 1 });
-assert.sameValue(earlyMeiji.era, "ce", "early pre-Meiji date should be in ce era");
-assert.sameValue(earlyMeiji.eraYear, 1870, "early pre-Meiji date eraYear should be 1870");
+TemporalHelpers.assertPlainDate(earlyMeiji, 1870, 3, "M03", 1, "early pre-Meiji date", "ce", 1870);
 
 // A BCE date in the Japanese calendar
 var bceDate = Temporal.PlainDate.from({ calendar: "japanese", era: "bce", eraYear: 100, month: 1, day: 1 });
-assert.sameValue(bceDate.era, "bce", "BCE date should be in bce era");
-assert.sameValue(bceDate.eraYear, 100, "BCE date eraYear");
-assert.sameValue(bceDate.year, -99, "BCE date year should be -99 (since 1 BCE = year 0 = year 0)");
+TemporalHelpers.assertPlainDate(bceDate, -99, 1, "M01", 1, "BCE date", "bce", 100);
 
 // Converting an ISO date to Japanese calendar should also trigger the era
-// revision for pre-Meiji dates. This exercises the isoToCalendarDate path.
+// assignment for pre-Meiji dates.
 var isoPreMeiji = new Temporal.PlainDate(1800, 6, 15);
 var japanesePreMeiji = isoPreMeiji.withCalendar("japanese");
-assert.sameValue(japanesePreMeiji.era, "ce", "ISO date converted to Japanese should be in ce era");
-assert.sameValue(japanesePreMeiji.eraYear, 1800, "ISO date converted to Japanese eraYear should be 1800");
-assert.sameValue(japanesePreMeiji.year, 1800, "ISO date converted to Japanese year");
+TemporalHelpers.assertPlainDate(japanesePreMeiji, 1800, 6, "M06", 15, "ISO date converted to Japanese", "ce", 1800);
 
 // Also test a BCE date via ISO-to-Japanese conversion
 var isoBce = new Temporal.PlainDate(-99, 1, 1);
 var japaneseBce = isoBce.withCalendar("japanese");
-assert.sameValue(japaneseBce.era, "bce", "ISO BCE date converted to Japanese should be in bce era");
-assert.sameValue(japaneseBce.eraYear, 100, "ISO BCE date converted to Japanese eraYear should be 100");
+TemporalHelpers.assertPlainDate(japaneseBce, -99, 1, "M01", 1, "ISO BCE date converted to Japanese", "bce", 100);
