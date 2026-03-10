1c851e71276c1778d8621ce92fcd70c6ad02a8b6
Jesse Alama <jesse@igalia.com>
2026-03-10 08:18:20 +0100

    Test that ISO date strings are rejected as calendar identifiers
    Strings like "11111111" (compact ISO 8601 date) and "1111-11-11"
    (extended ISO date) are syntactically valid calendar annotation values
    but are not built-in calendar identifiers. Ensure they throw RangeError
    in both string annotations and direct calendar arguments.
    
    See https://github.com/tc39/proposal-temporal/issues/2546

--- test/built-ins/Temporal/ZonedDateTime/prototype/withCalendar/calendar-invalid-iso-string.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/withCalendar/calendar-invalid-iso-string.js
@@ -12,6 +12,8 @@ const instance = new Temporal.ZonedDateTime(1_000_000_000_000_000_000n, "UTC", "
 const invalidStrings = [
   ["", "empty string"],
   ["1997-12-04[u-ca=notacal]", "Unknown calendar"],
+  ["11111111", "compact ISO date used as calendar name"],
+  ["1111-11-11", "extended ISO date used as calendar name"],
 ];
 
 for (const [arg, description] of invalidStrings) {
