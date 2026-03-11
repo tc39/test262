e5e4a997c5cec97e64076e507da2703d074916b8
Jesse Alama <jesse@igalia.com>
2026-03-11 11:54:11 +0100

    Temporal: Test GetOptionsObject ordering in ZonedDateTime

--- test/built-ins/Temporal/ZonedDateTime/prototype/add/order-of-operations.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/add/order-of-operations.js
@@ -66,3 +66,58 @@ const options = TemporalHelpers.propertyBagObserver(actual, { overflow: "constra
 
 instance.add(duration, options);
 assert.compareArray(actual, expected, "order of operations");
+
+actual.splice(0); // clear
+
+const expectedOpsForPrimitiveOptions = [
+  // ToTemporalDurationRecord
+  "get duration.days",
+  "get duration.days.valueOf",
+  "call duration.days.valueOf",
+  "get duration.hours",
+  "get duration.hours.valueOf",
+  "call duration.hours.valueOf",
+  "get duration.microseconds",
+  "get duration.microseconds.valueOf",
+  "call duration.microseconds.valueOf",
+  "get duration.milliseconds",
+  "get duration.milliseconds.valueOf",
+  "call duration.milliseconds.valueOf",
+  "get duration.minutes",
+  "get duration.minutes.valueOf",
+  "call duration.minutes.valueOf",
+  "get duration.months",
+  "get duration.months.valueOf",
+  "call duration.months.valueOf",
+  "get duration.nanoseconds",
+  "get duration.nanoseconds.valueOf",
+  "call duration.nanoseconds.valueOf",
+  "get duration.seconds",
+  "get duration.seconds.valueOf",
+  "call duration.seconds.valueOf",
+  "get duration.weeks",
+  "get duration.weeks.valueOf",
+  "call duration.weeks.valueOf",
+  "get duration.years",
+  "get duration.years.valueOf",
+  "call duration.years.valueOf",
+];
+
+const duration2 = TemporalHelpers.propertyBagObserver(actual, {
+  years: 1,
+  months: 1,
+  weeks: 1,
+  days: 1,
+  hours: 1,
+  minutes: 1,
+  seconds: 1,
+  milliseconds: 1,
+  microseconds: 1,
+  nanoseconds: 1,
+}, "duration");
+
+assert.throws(TypeError, () => instance.add(duration2, null));
+assert.compareArray(actual, expectedOpsForPrimitiveOptions,
+  "duration fields are read before TypeError is thrown for primitive options");
+
+actual.splice(0); // clear
--- test/built-ins/Temporal/ZonedDateTime/prototype/until/order-of-operations.js
+++ test/built-ins/Temporal/ZonedDateTime/prototype/until/order-of-operations.js
@@ -93,3 +93,63 @@ function createOptionsObserver({ smallestUnit = "nanoseconds", largestUnit = "au
 instance.until(otherDateTimePropertyBag, createOptionsObserver());
 assert.compareArray(actual, expected, "order of operations");
 actual.splice(0); // clear
+
+const expectedOpsForPrimitiveOptions = [
+  // ToTemporalZonedDateTime
+  "get other.calendar",
+  "get other.day",
+  "get other.day.valueOf",
+  "call other.day.valueOf",
+  "get other.hour",
+  "get other.hour.valueOf",
+  "call other.hour.valueOf",
+  "get other.microsecond",
+  "get other.microsecond.valueOf",
+  "call other.microsecond.valueOf",
+  "get other.millisecond",
+  "get other.millisecond.valueOf",
+  "call other.millisecond.valueOf",
+  "get other.minute",
+  "get other.minute.valueOf",
+  "call other.minute.valueOf",
+  "get other.month",
+  "get other.month.valueOf",
+  "call other.month.valueOf",
+  "get other.monthCode",
+  "get other.monthCode.toString",
+  "call other.monthCode.toString",
+  "get other.nanosecond",
+  "get other.nanosecond.valueOf",
+  "call other.nanosecond.valueOf",
+  "get other.offset",
+  "get other.offset.toString",
+  "call other.offset.toString",
+  "get other.second",
+  "get other.second.valueOf",
+  "call other.second.valueOf",
+  "get other.timeZone",
+  "get other.year",
+  "get other.year.valueOf",
+  "call other.year.valueOf",
+];
+
+const otherDateTimePropertyBag2 = TemporalHelpers.propertyBagObserver(actual, {
+  year: 2004,
+  month: 5,
+  monthCode: "M05",
+  day: 12,
+  hour: 1,
+  minute: 46,
+  second: 40,
+  millisecond: 250,
+  microsecond: 500,
+  nanosecond: 750,
+  offset: "+00:00",
+  calendar: "iso8601",
+  timeZone: "UTC",
+}, "other", ["calendar", "timeZone"]);
+
+assert.throws(TypeError, () => instance.until(otherDateTimePropertyBag2, null));
+assert.compareArray(actual, expectedOpsForPrimitiveOptions,
+  "other zoned datetime fields are read before TypeError is thrown for primitive options");
+actual.splice(0); // clear
