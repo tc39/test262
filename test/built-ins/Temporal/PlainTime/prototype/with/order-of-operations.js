75db7d196845ffcf2e3a4823473c80230b99e542
Jesse Alama <jesse@igalia.com>
2026-03-11 11:54:11 +0100

    Temporal: Test GetOptionsObject ordering in PlainTime

--- test/built-ins/Temporal/PlainTime/prototype/with/order-of-operations.js
+++ test/built-ins/Temporal/PlainTime/prototype/with/order-of-operations.js
@@ -54,3 +54,45 @@ const options = TemporalHelpers.propertyBagObserver(actual, {
 
 const result = instance.with(fields, options);
 assert.compareArray(actual, expected, "order of operations");
+
+actual.splice(0); // clear
+
+const expectedOpsForPrimitiveOptions = [
+  // RejectObjectWithCalendarOrTimeZone
+  "get fields.calendar",
+  "get fields.timeZone",
+  // ToTemporalTimeRecord
+  "get fields.hour",
+  "get fields.hour.valueOf",
+  "call fields.hour.valueOf",
+  "get fields.microsecond",
+  "get fields.microsecond.valueOf",
+  "call fields.microsecond.valueOf",
+  "get fields.millisecond",
+  "get fields.millisecond.valueOf",
+  "call fields.millisecond.valueOf",
+  "get fields.minute",
+  "get fields.minute.valueOf",
+  "call fields.minute.valueOf",
+  "get fields.nanosecond",
+  "get fields.nanosecond.valueOf",
+  "call fields.nanosecond.valueOf",
+  "get fields.second",
+  "get fields.second.valueOf",
+  "call fields.second.valueOf",
+];
+
+const fields2 = TemporalHelpers.propertyBagObserver(actual, {
+  hour: 1.7,
+  minute: 1.7,
+  second: 1.7,
+  millisecond: 1.7,
+  microsecond: 1.7,
+  nanosecond: 1.7,
+}, "fields");
+
+assert.throws(TypeError, () => instance.with(fields2, null));
+assert.compareArray(actual, expectedOpsForPrimitiveOptions,
+  "argument fields are read before TypeError is thrown for primitive options");
+
+actual.splice(0); // clear
