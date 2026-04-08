07538bc4d70502d46882bd79084a7595f10bca66
André Bargull <andre.bargull@gmail.com>
2026-04-08 20:06:00 +0200

    Add coverage when ToNumber in set{Full,UTCFull,}Year modifies date

---
+++ test/built-ins/Date/prototype/setUTCFullYear/date-value-read-before-tonumber-time-part-correct-when-set-to-invalid.js
@@ -0,0 +1,47 @@
+// Copyright (C) 2026 André Bargull. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-date.prototype.setutcfullyear
+description: >
+  Read [[DateValue]] and in ToNumber modify date-time to an invalid value
+info: |
+  Date.prototype.setUTCFullYear ( year [ , month [ , date ] ] )
+
+  ...
+  3. Let t be dateObject.[[DateValue]].
+  4. If t is NaN, set t to +0𝔽.
+  5. Let y be ? ToNumber(year).
+  ...
+---*/
+
+var dt = new Date(0);
+dt.setUTCHours(1, 2, 3, 4);
+
+var valueOfCalled = 0;
+
+var value = {
+  valueOf() {
+    valueOfCalled++;
+
+    // This call should get ignored.
+    dt.setTime(NaN);
+
+    return 1;
+  }
+};
+
+var result = dt.setUTCFullYear(value);
+
+assert.sameValue(valueOfCalled, 1, "valueOf called exactly once");
+
+assert.notSameValue(result, NaN, "result is not NaN");
+
+assert.sameValue(result, dt.getTime(), "result is equal to getTime");
+
+assert.sameValue(dt.getUTCFullYear(), 1, "date value correctly updated");
+
+assert.sameValue(dt.getUTCHours(), 1, "hours value still intact");
+assert.sameValue(dt.getUTCMinutes(), 2, "minutes value correctly updated");
+assert.sameValue(dt.getUTCSeconds(), 3, "seconds value correctly updated");
+assert.sameValue(dt.getUTCMilliseconds(), 4, "milliseconds value correctly updated");
