a9af758c4fb6d31eee5683b3a8a6d55525ac9a77
Jack Works <5390719+Jack-Works@users.noreply.github.com>
2026-04-11 06:47:47 +0800

    Remove substr in Temporal tests

--- test/built-ins/Temporal/Instant/prototype/toString/timezone-string-datetime.js
+++ test/built-ins/Temporal/Instant/prototype/toString/timezone-string-datetime.js
@@ -43,20 +43,20 @@ assert.throws(RangeError, () => instance.toString({ timeZone }), "bare date-time
 
 timeZone = "2021-08-19T17:30Z";
 const result1 = instance.toString({ timeZone });
-assert.sameValue(result1.substr(-6), "+00:00", "date-time + Z is UTC time zone");
+assert.sameValue(result1.slice(-6), "+00:00", "date-time + Z is UTC time zone");
 
 timeZone = "2021-08-19T17:30-07:00";
 const result2 = instance.toString({ timeZone });
-assert.sameValue(result2.substr(-6), "-07:00", "date-time + offset is the offset time zone");
+assert.sameValue(result2.slice(-6), "-07:00", "date-time + offset is the offset time zone");
 
 timeZone = "2021-08-19T17:30[UTC]";
 const result3 = instance.toString({ timeZone });
-assert.sameValue(result3.substr(-6), "+00:00", "date-time + IANA annotation is the offset time zone");
+assert.sameValue(result3.slice(-6), "+00:00", "date-time + IANA annotation is the offset time zone");
 
 timeZone = "2021-08-19T17:30Z[UTC]";
 const result4 = instance.toString({ timeZone });
-assert.sameValue(result4.substr(-6), "+00:00", "date-time + Z + IANA annotation is the offset time zone");
+assert.sameValue(result4.slice(-6), "+00:00", "date-time + Z + IANA annotation is the offset time zone");
 
 timeZone = "2021-08-19T17:30-07:00[UTC]";
 const result5 = instance.toString({ timeZone });
-assert.sameValue(result5.substr(-6), "+00:00", "date-time + offset + IANA annotation is the offset time zone");
+assert.sameValue(result5.slice(-6), "+00:00", "date-time + offset + IANA annotation is the offset time zone");
--- test/built-ins/Temporal/Instant/prototype/toString/timezone-string-leap-second.js
+++ test/built-ins/Temporal/Instant/prototype/toString/timezone-string-leap-second.js
@@ -11,7 +11,7 @@ const instance = new Temporal.Instant(0n);
 let timeZone = "2016-12-31T23:59:60+00:00[UTC]";
 
 const result = instance.toString({ timeZone });
-assert.sameValue(result.substr(-6), "+00:00", "leap second is a valid ISO string for TimeZone");
+assert.sameValue(result.slice(-6), "+00:00", "leap second is a valid ISO string for TimeZone");
 
 timeZone = "2021-08-19T17:30:45.123456789+23:59[+23:59:60]";
 assert.throws(RangeError, () => instance.toString({ timeZone }), "leap second in time zone name not valid");
