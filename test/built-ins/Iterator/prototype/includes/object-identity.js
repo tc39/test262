c55db1db543cc793a1e8ff9a383216d919943bf7
Michael Ficarra <git@michael.ficarra.me>
2026-04-18 11:16:02 -0400

    make sure toString/valueOf are not called on objects

--- test/built-ins/Iterator/prototype/includes/object-identity.js
+++ test/built-ins/Iterator/prototype/includes/object-identity.js
@@ -7,9 +7,27 @@ description: >
 features: [iterator-includes]
 ---*/
 
-let o = {};
+let o = {
+  get toString() {
+    throw new Test262Error();
+  },
+  get valueOf() {
+    throw new Test262Error();
+  }
+};
 let arr = [o];
 
-assert.sameValue(arr.values().includes({}), false);
+assert.sameValue(arr.values().includes({
+  get toString() {
+    throw new Test262Error();
+  },
+  get valueOf() {
+    throw new Test262Error();
+  }
+}), false);
+
+assert.sameValue(arr.values().includes(""), false);
+
 assert.sameValue(arr.values().includes(o), true);
+
 assert.sameValue([].values().includes(o), false);
