4a50aebcca17d39107d0272e026b866a9b9aaa90
Ron Buckton <3902892+rbuckton@users.noreply.github.com>
2026-05-21 07:38:27 -0400

    better test descriptions and coverage for annex-b

--- test/annexB/built-ins/RegExp/buffer-boundaries/not-supported-outside-unicode-modes.js
+++ test/annexB/built-ins/RegExp/buffer-boundaries/not-supported-outside-unicode-modes.js
@@ -4,7 +4,7 @@
 /*---
 author: Ron Buckton
 description: >
-  `\A` and `\z` (and `\Z`) are treated as IdentityEscape in Annex B outside any unicode mode
+  `\A`, `\z`, and `\Z` are treated as IdentityEscape in Annex B outside any unicode mode
 info: |
   Patterns
   
@@ -28,17 +28,33 @@ features: [regexp-buffer-boundaries]
 ---*/
 
 // `\A` is |IdentityEscape| outside of any unicode mode
-assert(/^\A$/.test("A"), "Expected \\A to match literal 'A' outside of unicode mode.");
-assert(!/\Ax/.test("x"), "Expected \\A to not match start of buffer outside of unicode mode.");
-assert(new RegExp("^\\A$").test("A"), "Expected \\A to match literal 'A' outside of unicode mode.");
-assert(!new RegExp("\\Ax").test("x"), "Expected \\A to not match start of buffer outside of unicode mode.");
+assert(/^\A$/.test("A"), "Expected \\A to match literal 'A' outside of unicode mode. (literal)");
+assert(!/\Ax/.test("x"), "Expected \\A to not match start-of-buffer outside of unicode mode. (literal)");
+assert(new RegExp("^\\A$").test("A"), "Expected \\A to match literal 'A' outside of unicode mode. (constructed)");
+assert(!new RegExp("\\Ax").test("x"), "Expected \\A to not match at start-of-buffer outside of unicode mode. (constructed)");
 
 // `\z` is |IdentityEscape| outside of any unicode mode
-assert(/^\z$/.test("z"), "Expected \\z to match literal 'z' outside of unicode mode.");
-assert(!/x\z/.test("x"), "Expected \\z to not match end of buffer outside of unicode mode.");
-assert(new RegExp("^\\z$").test("z"), "Expected \\z to match literal 'z' outside of unicode mode.");
-assert(!new RegExp("x\\z").test("x"), "Expected \\z to not match end of buffer outside of unicode mode.");
+assert(/^\z$/.test("z"), "Expected \\z to match literal 'z' outside of unicode mode. (literal)");
+assert(!/x\z/.test("x"), "Expected \\z to not match end-of-buffer outside of unicode mode. (literal)");
+assert(new RegExp("^\\z$").test("z"), "Expected \\z to match literal 'z' outside of unicode mode. (constructed)");
+assert(!new RegExp("x\\z").test("x"), "Expected \\z to not match at end-of-buffer outside of unicode mode. (constructed)");
 
-// NOTE: ensures reserved `\Z` is |IdentityEscape| outside of any unicode mode
-assert(/^\Z$/.test("Z"), "Expected \\Z to match literal 'Z' outside of unicode mode.");
-assert(new RegExp("^\\Z$").test("Z"), "Expected \\Z to match literal 'Z' outside of unicode mode.");
+// `\Z` is |IdentityEscape| outside of any unicode mode
+assert(/^\Z$/.test("Z"), "Expected \\Z to match literal 'Z' outside of unicode mode. (literal)");
+assert(!/^x\Z$/.test("x"), "Expected \\Z to not match end-of-buffer outside of unicode mode. (literal)");
+assert(new RegExp("^\\Z$").test("Z"), "Expected \\Z to match literal 'Z' outside of unicode mode. (constructed)");
+assert(!new RegExp("^x\\Z$").test("x"), "Expected \\Z to not match at end-of-buffer outside of unicode mode. (constructed)");
+
+var terminators = [
+  ["\n", "<LF>"],
+  ["\r", "<CR>"],
+  ["\u2028", "<LS>"],
+  ["\u2029", "<PS>"],
+  ["\r\n", "<CRLF>"],
+];
+
+for (var i = 0; i < terminators.length; i++) {
+  var lts = terminators[i][0], desc = terminators[i][1];
+  assert(!/^x\Z$/.test("x" + lts), "Expected \\Z to not match at " + desc + " before end-of-buffer outside of unicode mode. (literal)");
+  assert(!new RegExp("^x\\Z$").test("x" + lts), "Expected \\Z to not match at " + desc + " before end-of-buffer outside of unicode mode. (constructed)");
+}
