07dbcbca04c5ac73eefd752eb0a67a893c159374
Philip Chimento <pchimento@igalia.com>
2026-07-24 13:40:37 -0700

    linter: Handle harness files without a `defines` key (#5094)

---
+++ tools/lint/test/fixtures/harness/noDefinesAllowUnused.js
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 Igalia, S.L.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+description: >
+  Fake harness file with empty defines, but allow_unused to avoid warnings.
+defines: []
+allow_unused: true
+---*/
