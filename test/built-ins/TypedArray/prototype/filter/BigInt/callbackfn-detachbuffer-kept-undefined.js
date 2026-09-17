846740bc2194a1cbebe38d8532321290c7443026
Kevin Gibbons <bakkot@gmail.com>
2026-09-17 11:42:12 -0700

    add tests for TA.prototype.with/filter shrinking/detaching buffer during coercion

---
+++ test/built-ins/TypedArray/prototype/filter/BigInt/callbackfn-detachbuffer-kept-undefined.js
@@ -0,0 +1,33 @@
+// Copyright (C) 2026 Kevin Gibbons. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-%typedarray%.prototype.filter
+description: >
+  Keeping undefined after the callback detaches the buffer throws a TypeError,
+  after all callbacks have run.
+includes: [detachArrayBuffer.js, testTypedArray.js]
+features: [BigInt, TypedArray]
+---*/
+
+testWithBigIntTypedArrayConstructors(function(TA, makeCtorArg) {
+  var sample = new TA(makeCtorArg([1n, 2n, 3n]));
+  var calls = 0;
+
+  assert.throws(TypeError, function() {
+    sample.filter(function(value, index, array) {
+      assert.sameValue(index, calls);
+      assert.sameValue(array, sample);
+      if (calls === 0) {
+        assert.sameValue(value, 1n);
+        $DETACHBUFFER(sample.buffer);
+      } else {
+        assert.sameValue(value, undefined, "Missing elements are passed to the callback");
+      }
+      calls++;
+      return true;
+    });
+  });
+
+  assert.sameValue(calls, 3, "All callbacks run before converting the kept values");
+}, null, null, ["immutable"]);
